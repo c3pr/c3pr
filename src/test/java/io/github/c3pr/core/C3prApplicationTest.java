aac55aad7b4dfe96f54380dd98f72d8e625da48a
Antonio Jr <acdcjunior@gmail.com>
2017-12-24 14:55:44 -0200

    test: add context loads test

---
+++ src/test/java/io/github/c3pr/core/C3prApplicationTest.java
@@ -0,0 +1,16 @@
+package io.github.c3pr.core;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class C3prApplicationTest {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
