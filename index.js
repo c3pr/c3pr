01b12080ad357af2e3c85ddf67310897f3b499e2
Antonio Jr <acdcjunior@gmail.com>
2017-12-25 19:13:22 -0200

    feat(webhook): add webhook handle example

---
+++ index.js
@@ -0,0 +1,29 @@
+const http = require('http');
+const createHandler = require('github-webhook-handler');
+
+const handler = createHandler({path: '/webhook', secret: 'myhashsecret'});
+
+http.createServer(function (req, res) {
+    handler(req, res, function (err) {
+        res.statusCode = 404;
+        res.end('no such location')
+    })
+}).listen(5000);
+
+handler.on('error', function (err) {
+    console.error('Error:', err.message)
+});
+
+handler.on('push', function (event) {
+    console.log('Received a push event for %s to %s',
+        event.payload.repository.name,
+        event.payload.ref)
+});
+
+handler.on('issues', function (event) {
+    console.log('Received an issue event for %s action=%s: #%d %s',
+        event.payload.repository.name,
+        event.payload.action,
+        event.payload.issue.number,
+        event.payload.issue.title)
+});
