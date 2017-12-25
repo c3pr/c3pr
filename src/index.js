5642a6dc83ae02bce5e5e54a43f5349af5460317
Antonio Jr <acdcjunior@gmail.com>
2017-12-25 20:06:03 -0200

    feat(webserver): switch from http to express and add some default HTML pages

--- src/index.js
+++ src/index.js
@@ -1,30 +1,35 @@
-const http = require('http');
+const express = require('express');
 const createHandler = require('github-webhook-handler');
 
 const PORT = process.env.PORT || 5000;
-const handler = createHandler({path: '/webhook', secret: 'myhashsecret'});
+const githubWebhookHandler = createHandler({path: '/webhook', secret: 'myhashsecret'});
+const app = express();
 
-http.createServer(function (req, res) {
-    handler(req, res, function (err) {
-        res.statusCode = 404;
-        res.end('no such location')
-    })
-}).listen(PORT);
+app.use(githubWebhookHandler);
+app.use(express.static('resources/public'));
 
-handler.on('error', function (err) {
+
+githubWebhookHandler.on('error', function (err) {
     console.error('Error:', err.message)
 });
 
-handler.on('push', function (event) {
+githubWebhookHandler.on('push', function (event) {
     console.log('Received a push event for %s to %s',
         event.payload.repository.name,
         event.payload.ref)
 });
 
-handler.on('issues', function (event) {
+githubWebhookHandler.on('issues', function (event) {
     console.log('Received an issue event for %s action=%s: #%d %s',
         event.payload.repository.name,
         event.payload.action,
         event.payload.issue.number,
         event.payload.issue.title)
 });
+
+// The 404 Route (ALWAYS Keep this as the last route)
+app.get('*', function(req, res){
+    res.send('No C3PR endpoint is listening at the requested location.', 404);
+});
+
+app.listen(PORT);
