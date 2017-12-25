9c95006726407038e4f460346378e86b7f144683
Antonio Jr <acdcjunior@gmail.com>
2017-12-25 20:08:37 -0200

    refactor(webhook): move github webhook handler to dedicated file

---
+++ src/githubWebhookHandler.js
@@ -0,0 +1,22 @@
+const createHandler = require('github-webhook-handler');
+
+const GITHUB_WEBHOOK_SECRET = process.env.GITHUB_WEBHOOK_SECRET || 'myhashsecret';
+const githubWebhookHandler = createHandler({path: '/webhook', secret: GITHUB_WEBHOOK_SECRET});
+
+githubWebhookHandler.on('error', function (err) {
+    console.error('Error:', err.message)
+});
+
+githubWebhookHandler.on('push', function (event) {
+    console.log('Received a push event for %s to %s',
+        event.payload.repository.name,
+        event.payload.ref)
+});
+
+githubWebhookHandler.on('issues', function (event) {
+    console.log('Received an issue event for %s action=%s: #%d %s',
+        event.payload.repository.name,
+        event.payload.action,
+        event.payload.issue.number,
+        event.payload.issue.title)
+});
--- src/index.js
+++ src/index.js
@@ -1,31 +1,12 @@
 const express = require('express');
-const createHandler = require('github-webhook-handler');
+const githubWebhookHandler = require('./githubWebhookHandler');
 
 const PORT = process.env.PORT || 5000;
-const githubWebhookHandler = createHandler({path: '/webhook', secret: 'myhashsecret'});
 const app = express();
 
 app.use(githubWebhookHandler);
+
 app.use(express.static('resources/public'));
-
-
-githubWebhookHandler.on('error', function (err) {
-    console.error('Error:', err.message)
-});
-
-githubWebhookHandler.on('push', function (event) {
-    console.log('Received a push event for %s to %s',
-        event.payload.repository.name,
-        event.payload.ref)
-});
-
-githubWebhookHandler.on('issues', function (event) {
-    console.log('Received an issue event for %s action=%s: #%d %s',
-        event.payload.repository.name,
-        event.payload.action,
-        event.payload.issue.number,
-        event.payload.issue.title)
-});
 
 // The 404 Route (ALWAYS Keep this as the last route)
 app.get('*', function(req, res){
