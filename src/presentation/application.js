ffbb46d22d42f95281e471e63c0a977c84d97300
Antonio Jr <acdcjunior@gmail.com>
2018-01-28 16:19:06 -0200

    chore: convert to JavaScript project

---
+++ src/presentation/application.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+app.use(express.static('resources/public'));
+
+// The 404 Route (ALWAYS Keep this as the last route)
+app.get('*', function(req, res){
+    res.send('No C3PR endpoint is listening at the requested location.', 404);
+});
+
+app.listen(PORT);
