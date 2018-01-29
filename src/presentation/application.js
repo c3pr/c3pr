61c4eb5ab213470a74fe6908c3711915e7b4cf76
Antonio Jr <acdcjunior@gmail.com>
2018-01-29 10:20:34 -0200

    refactor(app): remove deprecated version

--- src/presentation/application.js
+++ src/presentation/application.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 const app = express();
 
 app.use(express.static('resources/public'));
@@ -9,7 +9,7 @@ require('./pushReceivedController')(app);
 
 // The 404 Route (ALWAYS Keep this as the last route)
 app.get('*', function(req, res){
-    res.send('No C3PR endpoint is listening at the requested location.', 404);
+    res.status(404).send('No C3PR endpoint is listening at the requested location.');
 });
 
 app.listen(PORT);
