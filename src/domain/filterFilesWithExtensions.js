7e93103bba05b5766d7b7adf2f04c818cf6678a3
Antonio Jr <acdcjunior@gmail.com>
2018-01-30 11:21:23 -0200

    feat(extensions): add filterFilesWithExtensions

---
+++ src/domain/filterFilesWithExtensions.js
@@ -0,0 +1,7 @@
+const extractExtension = require("./extractExtensionsInChangeset").extractExtension;
+
+function filterFilesWithExtensions(changeset, extensions) {
+    return changeset.filter(file => extensions.includes(extractExtension(file)));
+}
+
+module.exports = filterFilesWithExtensions;
---
+++ src/domain/filterFilesWithExtensions.test.js
@@ -0,0 +1,13 @@
+const expect = require('chai').expect;
+const filterFilesWithExtensions = require('./filterFilesWithExtensions');
+
+describe('filterFilesWithExtensions', function () {
+    it('should return files with given extensions', function () {
+        const changeset = ['src/main/a/b/c/Main.java', 'src/main/a/b/c/Dockerfile', 'src/main/a/b/c/Main.js','src/abc.java'];
+        const extensions = ['java', 'js'];
+
+        expect(filterFilesWithExtensions(changeset, extensions)).to.deep.equal([
+            'src/main/a/b/c/Main.java', 'src/main/a/b/c/Main.js', 'src/abc.java'
+        ]);
+    });
+});
