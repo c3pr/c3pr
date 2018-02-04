0fd198cc474258323e9f327893efb2181dbd7378
Antonio Jr <acdcjunior@gmail.com>
2018-02-04 16:33:12 -0200

    refactor: rename webhook listener

--- src/presentation/application.js
+++ src/presentation/application.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const githubWebhookHandler = require('./githubWebhookHandler');
+const webhookListenerController = require('./webhookListenerController');
 
 const PORT = process.env.PORT || 5000;
 const app = express();
 
-app.use(githubWebhookHandler);
+app.use(webhookListenerController);
 
 app.use(express.static('resources/public'));
 
