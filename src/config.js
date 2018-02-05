c5dbda271c766e8ea4a52e149ba291d60660b510
Antonio Jr <acdcjunior@gmail.com>
2018-02-05 13:04:42 -0200

    feat(c3pr-bot): added notification of changes to c3pr bot

---
+++ src/config.js
@@ -0,0 +1,7 @@
+const C3PR_BOT_BASE_URL = 'https://c3pr.herokuapp.com';
+
+module.exports = {
+    c3pr: {
+        botChangesUrl: `${C3PR_BOT_BASE_URL}/changes`
+    }
+};
--- src/domain/c3pr-bot/notifyC3prBotOfChanges.js
+++ src/domain/c3pr-bot/notifyC3prBotOfChanges.js
@@ -1,5 +1,17 @@
-function notifyC3prBotOfChanges() {
+const request = require('request');
+const config = require('../../config');
 
+function notifyC3prBotOfChanges(changes) {
+    console.log(`>>> Notifying bot ${changes.changeset.length} of changes to ${changes.repository.url}...`);
+    request.post(config.c3pr.botChangesUrl,
+        changes,
+        function (error, response, body) {
+            console.log(`>>> Notified bot ${changes.changeset.length} of changes to ${changes.repository.url}.`);
+            if (!error && response.statusCode === 200) {
+                console.log(body)
+            }
+        }
+    );
 }
 
 module.exports = notifyC3prBotOfChanges;
