c52e257eeca1163a050c7815dd41554f47de7955
Antonio Jr <acdcjunior@gmail.com>
2018-02-06 13:06:59 -0200

    feat(changes): add correlationId and schemaName requirements

--- src/presentation/changesController.js
+++ src/presentation/changesController.js
@@ -3,12 +3,14 @@ const handleChanges = require('../application/handleChanges');
 module.exports = function (app) {
 
     app.post('/changes', function (request, response) {
-        console.log("\n\nChanges received:");
-        console.log(request.body);
-        console.log("\n\n------------------------\n\n");
-        handleChanges(request.body);
-        console.log("\n\n------------------------\n\n");
-
+        const changes = request.body;
+        if (!changes.meta.correlationId || changes.meta.schemaName !== "c3pr/c3pr::changes") {
+            const errorMessage = `Request does not contain required metadata (meta.correlationId and meta.schemaName): ${JSON.stringify(changes)}.`;
+            console.error(errorMessage);
+            response.status(400).send(errorMessage);
+        }
+        console.log(`>> changesController: Changes received. ${JSON.stringify(changes)}`);
+        handleChanges(changes);
         response.send('Ok, that would be all, thanks.');
     });
 
