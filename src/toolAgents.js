2e2f7756f57b5412d0cba498bb244437893d5770
Antonio Jr <acdcjunior@gmail.com>
2018-02-06 12:45:01 -0200

    chore: add local test tool agent

--- src/toolAgents.js
+++ src/toolAgents.js
@@ -3,7 +3,8 @@ const toolAgents = {
         {
             name: "walkmod-sonar:StringCheckOnLeft",
             extensions: ["java"],
-            agentURL: "http://some-server-running-walkmod-docker.com:5000/c3pr",
+            // agentURL: "http://some-server-running-walkmod-docker.com:5000/c3pr",
+            agentURL: "http://localhost:5002/c3pr",
             // this 'arguments' field is optional and has meaning only to the agent. it will be included in the POST to the tool
             arguments: {rule: "sonar:StringCheckOnLeft"}
         }
