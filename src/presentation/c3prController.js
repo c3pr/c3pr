c6dba0d3c306f28f5fabf31745f381400bc45a12
Antonio Jr <acdcjunior@gmail.com>
2018-02-07 10:05:03 -0200

    feat(toolInvocation): change schema prop and add example

--- src/presentation/c3prController.js
+++ src/presentation/c3prController.js
@@ -4,7 +4,10 @@ module.exports = function (app) {
 
     app.post('/c3pr', function(request, response){
         const toolInvocation = request.body;
-        if (!toolInvocation.meta.correlationId || toolInvocation.meta.schemaName !== "c3pr/c3pr-agent::toolInvocation") {
+        if (!toolInvocation.meta ||
+            !toolInvocation.meta.correlationId ||
+            !toolInvocation.meta.schemas ||
+            !toolInvocation.meta.schemas["c3pr/c3pr-agent::toolInvocation"]) {
             const errorMessage = `Request does not contain required metadata (meta.correlationId and meta.schemaName): ${JSON.stringify(toolInvocation)}.`;
             console.error(errorMessage);
             response.status(400).send(errorMessage);
