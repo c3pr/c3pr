f08d0e746cd0ee2aff217d7c3e5b9b79f928d772
Antonio Jr <acdcjunior@gmail.com>
2018-02-09 11:49:47 -0200

    feat(tools): update toolAgent schema

--- src/toolAgents.js
+++ src/toolAgents.js
@@ -6,7 +6,10 @@ const toolAgents = {
             // agentURL: "http://some-server-running-walkmod-docker.com:5000/c3pr",
             agentURL: "http://localhost:5002/c3pr",
             // this 'arguments' field is optional and has meaning only to the agent. it will be included in the POST to the tool
-            arguments: {rule: "sonar:StringCheckOnLeft"}
+            tool: {
+                command: "walkmod apply sonar:StringCheckOnLeft",
+                meta: {rule: "sonar:StringCheckOnLeft"}
+            }
         }
     ]
 };
