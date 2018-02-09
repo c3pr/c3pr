48aec6419f07c6ba5c8dc3fd776fce8ef8ec9659
Antonio Jr <acdcjunior@gmail.com>
2018-02-09 11:39:21 -0200

    feat(toolInvocation): add tool invocation at git repo

---
+++ src/domain/invokeToolAtGitRepo.js
@@ -0,0 +1,35 @@
+const cloneRepositoryLocally = require("node-git-client").cloneRepositoryLocally;
+const determineGitDiff = require("node-git-client").determineGitDiff;
+const shell = require("./util/shell").shell;
+const uuidv4 = require('uuid/v4');
+
+const config = require('../config');
+
+const CLONE_DIR = config.c3pr.agent.cloneDir;
+const CLONE_DEPTH = config.c3pr.agent.cloneDepth;
+
+async function invokeToolAtGitRepo(toolInvocation) {
+
+    const localUniqueCorrelationId = uuidv4();
+    const prefix = `[${toolInvocation.meta.correlationId}][${localUniqueCorrelationId}] [invokeToolAtGitRepo]`;
+
+    console.log(`${prefix} >>> Invoking tool at git repo: ${toolInvocation.repository.url}`);
+
+    const cloneFolder = await cloneRepositoryLocally({
+        localUniqueCorrelationId: localUniqueCorrelationId,
+        cloneBaseDir: CLONE_DIR,
+        url: toolInvocation.repository.url,
+        branch: toolInvocation.repository.branch,
+        revision: toolInvocation.repository.revision,
+        cloneDepth: CLONE_DEPTH
+    });
+
+    console.log(`${prefix} Done cloning at ${cloneFolder}`);
+
+    await shell(toolInvocation.tool.command, {cwd: cloneFolder}, {stdout: true, prefix});
+
+    return await determineGitDiff(toolInvocation.meta.correlationId, cloneFolder);
+
+}
+
+module.exports = invokeToolAtGitRepo;
---
+++ src/domain/invokeToolAtGitRepo.test.js
@@ -0,0 +1,54 @@
+const invokeToolAtGitRepo = require('./invokeToolAtGitRepo');
+
+const expect = require('chai').expect;
+const fs = require('fs');
+const path = require('path');
+
+const config = require('../config');
+
+config.c3pr.agent.cloneDir = '/tmp/c3pr/test';
+config.c3pr.agent.cloneDepth = 5;
+
+describe('invokeToolAtGitRepo', () => {
+
+    it('invokeToolAtGitRepo', async () => {
+
+        const diff = await invokeToolAtGitRepo({
+            "meta": {
+                "correlationId": "30b03c1d8aa6ee670534b80edd0dc39c12644259",
+                "schemas": {
+                    "c3pr/c3pr-agent::toolInvocation": true
+                }
+            },
+            "repository": {
+                "type": "git",
+                "url": "https://github.com/c3pr/sample-project-java-maven.git",
+                "branch": "branch-for-clone-tests",
+                "revision": "30b03c1d8aa6ee670534b80edd0dc39c12644259"
+            },
+            "files": [
+                "pom.xml"
+            ],
+            "tool": {
+                "command": "echo \"this-would-be-a-change-executed-via-tool\" >> pom.xml",
+                "meta": {
+                    "rule": "sonar:StringCheckOnLeft"
+                }
+            }
+        });
+
+        expect(diff).to.be.equal(
+`diff --git a/pom.xml b/pom.xml
+index ad8bb19..7452c7a 100644
+--- a/pom.xml
++++ b/pom.xml
+@@ -13,3 +13,4 @@
+ 	</properties>
+ 
+ </project>
++"this-would-be-a-change-executed-via-tool" 
+`);
+
+    }).timeout(10 * 1000);
+
+});
