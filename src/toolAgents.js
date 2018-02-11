647415b022feac868f5d235f4874cac9c4f11799
Antonio Jr <acdcjunior@gmail.com>
2018-02-11 16:09:10 -0200

    feat(toolAgents): add test agent

--- src/toolAgents.js
+++ src/toolAgents.js
@@ -1,10 +1,16 @@
 const toolAgents = {
     agents: [
         {
-            toolId: "walkmod-sonar:StringCheckOnLeft",
-            extensions: ["java"],
-            // agentURL: "http://some-server-running-walkmod-docker.com:5000/c3pr",
+            toolId: "test-tool",
+            extensions: ["java", "xml", "txt", "json"],
             agentURL: "http://localhost:5002/c3pr",
+            command: "echo example-test-tool>test-file.txt"
+            //, toolMeta: {}
+        },
+        {
+            toolId: "walkmod-sonar:StringCheckOnLeft",
+            extensions: ["javaREMOVE-ME"],
+            agentURL: "http://some-server-running-walkmod-docker.com:5000/c3pr",
             command: "walkmod apply sonar:StringCheckOnLeft",
             toolMeta: {rule: "sonar:StringCheckOnLeft"}
         }
