283bbce6036ec79197d44301e1bda833b1f7b9cf
Antonio Jr <acdcjunior@gmail.com>
2018-02-11 16:09:29 -0200

    fix(patches): fix scheme

--- src/presentation/patchesController.js
+++ src/presentation/patchesController.js
@@ -7,7 +7,7 @@ module.exports = function (app) {
         if (!changes.meta ||
             !changes.meta.correlationId ||
             !changes.meta.compatibleSchemas ||
-            !changes.meta.compatibleSchemas.includes("c3pr/c3pr::changes")) {
+            !changes.meta.compatibleSchemas.includes("c3pr/c3pr::patches")) {
             const errorMessage = `Request does not contain required metadata (meta.correlationId and meta.compatibleSchemas): ${JSON.stringify(patches)}.`;
             console.error(errorMessage);
             response.status(400).send(errorMessage);
