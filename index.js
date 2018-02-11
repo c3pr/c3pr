c3316b5b514b8e0d344025830db4f6ea84bd22dd
Antonio Jr <acdcjunior@gmail.com>
2018-02-11 11:49:33 -0200

    feat(shell): expose shell and shellOut as first level functions

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 module.exports = {
     cloneRepositoryLocally: require('./src/cloneRepositoryLocally'),
     determineGitDiff: require('./src/determineGitDiff'),
-    shell: require('./src/shell')
+    shell: require('./src/shell').shell,
+    shellOut: require('./src/shell').shellOut
 };
