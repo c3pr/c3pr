2b29ab326d025a257d7e1bf98e28ae1765582535
Antonio Jr <acdcjunior@gmail.com>
2018-02-12 19:47:18 -0200

    feat(patches): have agent send patches to c3pr

--- src/domain/sendPatchToBot.js
+++ src/domain/sendPatchToBot.js
@@ -1,8 +1,28 @@
+const request = require('request');
 
-function sendPatchToBot(patchesPayload) {
+function sendPatchToBot(patchesUrl, patchesPayload) {
     console.log(`[sendPatchToBot] called with ${patchesPayload}`);
 
-    return sendPatchToBot;
+    request.post(
+        {
+            url: patchesUrl,
+            json: true,
+            body: patchesPayload
+        },
+        function (error, response, body) {
+            if (error || response.statusCode !== 200) {
+                console.log(`[${patchesPayload.meta.correlationId}] [sendPatchToBot] >>>>>> Error while sending patch to bot.
+                * URL: ${patchesUrl}
+                * Status: ${response.statusCode}
+                * Error: ${error}
+                * Body:
+                -----------------------\n${body}
+                -----------------------\n\n`);
+            } else {
+                console.log(`[${patchesPayload.meta.correlationId}] [sendPatchToBot] >>> Sent patch to bot at ${patchesUrl}.`);
+            }
+        }
+    );
 }
 
 module.exports = sendPatchToBot;
