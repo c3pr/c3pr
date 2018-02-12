31450b315243bcb0bc1fbce5d42ce5270e812c26
Antonio Jr <acdcjunior@gmail.com>
2018-02-12 15:25:18 -0200

    test(toolInvocation): update expected arguments to match new style

--- src/domain/invokeTools.test.js
+++ src/domain/invokeTools.test.js
@@ -1,18 +1,22 @@
 process.env.NODE_ENV = 'test';
 
-const invokeTools = require('./invokeTools');
+const expect = require('chai').expect;
 const sinon = require('sinon');
 require('chai').should();
+
 const request = require('request');
 const RESPONSE_OK = {statusCode: 200, headers: {'content-type': 'application/json'}};
 
+const invokeTools = require('./invokeTools');
+const config = require('../config');
 
 describe('invokeTools', () => {
 
     const changes = {
         meta: {
             correlationId: "4444eedacc076e8a16ae565b535fd48edb9a044a",
-            compatibleSchemas: ["c3pr/c3pr::changes"]
+            compatibleSchemas: ["c3pr/c3pr::changes"],
+            dates: [{node: "c3pr-repo-github", date: "2018-02-12T14:05:08.893Z", schema: "changes"}]
         },
         changeset: ['src/main/a/b/c/Main.java', 'src/main/a/b/c/Main.js', 'src/boo.txt'],
         repository: {
@@ -31,7 +35,15 @@ describe('invokeTools', () => {
 
 
     let responseBody;
+    const now = new Date();
+    let sandbox, clock;
+
     beforeEach(() => {
+        sandbox = sinon.sandbox.create();
+        clock = sinon.useFakeTimers(now.getTime());
+
+        config.c3pr.changesUrl = 'http://changes-server/changes';
+
         responseBody = {
             that: 'is it!'
         };
@@ -39,6 +51,8 @@ describe('invokeTools', () => {
     });
 
     afterEach(() => {
+        sandbox.restore();
+        clock.restore();
         request.post.restore();
     });
 
@@ -49,43 +63,39 @@ describe('invokeTools', () => {
 
         sinon.assert.calledTwice(this.post);
 
-        sinon.assert.calledWith(this.post,
-            {
-                url: toolAgents.agents[0].agentURL,
-                json: true,
-                body: {
-                    meta: {
-                        compatibleSchemas: ["c3pr/c3pr-agent::toolInvocation"],
-                        correlationId: "4444eedacc076e8a16ae565b535fd48edb9a044a"
-                    },
-                    repository: changes.repository,
-                    files: [changes.changeset[0], changes.changeset[1]],
-                    tool: {
-                        command: toolAgents.agents[0].command,
-                        toolMeta: toolAgents.agents[0].toolMeta,
-                    }
-                }
+        const firstCallFirstArgument = this.post.getCall(0).args[0];
+        expect(firstCallFirstArgument).to.deep.equal({
+            url: toolAgents.agents[0].agentURL,
+            json: true,
+            body: {
+                meta: {
+                    compatibleSchemas: ["c3pr/c3pr-agent::toolInvocation"],
+                    correlationId: "4444eedacc076e8a16ae565b535fd48edb9a044a",
+                    dates: [{date: "2018-02-12T14:05:08.893Z", node: "c3pr-repo-github", schema: "changes"}, {date: now.toISOString(), node: "c3pr", schema: "toolInvocation"}]
+                },
+                c3pr: { changesUrl: "http://changes-server/changes" },
+                repository: changes.repository,
+                files: [changes.changeset[0], changes.changeset[1]],
+                tool: toolAgents.agents[0]
             }
-        );
+        });
 
-        sinon.assert.calledWith(this.post,
-            {
-                url: toolAgents.agents[1].agentURL,
-                json: true,
-                body: {
-                    meta: {
-                        compatibleSchemas: ["c3pr/c3pr-agent::toolInvocation"],
-                        correlationId: "4444eedacc076e8a16ae565b535fd48edb9a044a"
-                    },
-                    repository: changes.repository,
-                    files: [changes.changeset[1]],
-                    tool: {
-                        command: toolAgents.agents[1].command,
-                        toolMeta: toolAgents.agents[1].toolMeta,
-                    }
-                }
+        const secondCallFirstArgument = this.post.getCall(1).args[0];
+        expect(secondCallFirstArgument).to.deep.equal({
+            url: toolAgents.agents[1].agentURL,
+            json: true,
+            body: {
+                meta: {
+                    compatibleSchemas: ["c3pr/c3pr-agent::toolInvocation"],
+                    correlationId: "4444eedacc076e8a16ae565b535fd48edb9a044a",
+                    dates: [{date: "2018-02-12T14:05:08.893Z", node: "c3pr-repo-github", schema: "changes"}, {date: now.toISOString(), node: "c3pr", schema: "toolInvocation"}]
+                },
+                c3pr: { changesUrl: "http://changes-server/changes" },
+                repository: changes.repository,
+                files: [changes.changeset[1]],
+                tool: toolAgents.agents[1]
             }
-        );
+        });
     });
 
 });
