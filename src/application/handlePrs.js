e09ba7f8b446ba8355f2ffbcb741507f33eed8b4
Antonio Jr <acdcjunior@gmail.com>
2018-02-13 15:05:39 -0200

    feat(prs): create pr upon /prs request

--- src/application/handlePrs.js
+++ src/application/handlePrs.js
@@ -1,6 +1,17 @@
+const createPR = require('../domain/pr/createPR');
 
 function handlePrs(prs) {
     console.log(`[${prs.meta.correlationId}] [handlePrs] Handling pr request ${JSON.stringify(prs)}...`);
+
+    createPR({
+        mainRepoOrgRepo: prs.repository.url.replace('https://github.com/', '').replace('.git', ''),
+        mainRepoBranch: prs.repository.branch,
+        mainRepoHash: prs.repository.revision,
+        prCommitMessage: prs.patch.title,
+        prTitle: prs.patch.title,
+        prBody: prs.patch.body,
+        patchContent: prs.patch.base64diff
+    });
 }
 
 module.exports = handlePrs;
