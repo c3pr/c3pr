c9ee25f0ac3aa1c07a841645d37845739ace0fd3
Antonio Jr <acdcjunior@gmail.com>
2018-02-17 21:23:31 -0200

    config: add actual agent URL

--- src/toolAgents.js
+++ src/toolAgents.js
@@ -12,13 +12,28 @@ const toolAgents = {
         {
             toolId: "walkmod-sonar:StringCheckOnLeft",
             extensions: ["java"],
-            // agentURL: "http://some-server-running-walkmod-docker.com:5000/c3pr",
-            // agentURL: "http://localhost:5003/c3pr",
-            agentURL: "http://189.6.108.114:5003/c3pr",
+            agentURL: "https://c3pr-tool-walkmod-sonar-now-hjmsouxims.now.sh/c3pr",
             command: "walkmod apply sonar:StringCheckOnLeft",
             toolMeta: {rule: "sonar:StringCheckOnLeft"},
-            prTitle: "My PR Title",
-            prBody: "This is the `markdown` body."
+            prTitle: "Strings literals should be placed on the left side when checking for equality",
+            prBody: `
+It is preferable to place string literals on the left-hand side of an \`equals()\` or \`equalsIgnoreCase()\` method call. This prevents null pointer exceptions from being raised, as a string literal can never be \`null\` by definition.
+
+The following code:
+
+\`\`\`java
+String myString = null;
+
+System.out.println("Equal? " + myString.equals("foo"));                        // Non-Compliant - will raise a NPE
+System.out.println("Equal? " + (myString != null && myString.equals("foo")));  // Non-Compliant - null check could be removed
+\`\`\`
+
+should be refactored into:
+
+\`\`\`java
+System.out.println("Equal?" + "foo".equals(myString));                         // Compliant - properly deals with the null case
+\`\`\`
+`
         }
     ]
 };
