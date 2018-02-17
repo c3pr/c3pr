9284b01ce64f5ef367b9468c46efd0b9fbaf9f78
Antonio Jr <acdcjunior@gmail.com>
2018-02-17 18:12:33 -0200

    config: add demo agent

--- src/toolAgents.js
+++ src/toolAgents.js
@@ -1,18 +1,20 @@
 const toolAgents = {
     agents: [
-        {
-            toolId: "test-tool",
-            extensions: ["java", "xml", "txt", "json"],
-            agentURL: "http://localhost:5003/c3pr",
-            command: "echo example-test-tool>test-file.txt",
-            toolMeta: {}, // unused
-            prTitle: "My Test Tool PR Title",
-            prBody: "This is the *markdown* body."
-        },
+        // {
+        //     toolId: "test-tool",
+        //     extensions: ["java", "xml", "txt", "json"],
+        //     agentURL: "http://localhost:5003/c3pr",
+        //     command: "echo example-test-tool>test-file.txt",
+        //     toolMeta: {}, // unused
+        //     prTitle: "My Test Tool PR Title",
+        //     prBody: "This is the *markdown* body."
+        // },
         {
             toolId: "walkmod-sonar:StringCheckOnLeft",
-            extensions: ["javaREMOVE-ME"],
-            agentURL: "http://some-server-running-walkmod-docker.com:5000/c3pr",
+            extensions: ["java"],
+            // agentURL: "http://some-server-running-walkmod-docker.com:5000/c3pr",
+            // agentURL: "http://localhost:5003/c3pr",
+            agentURL: "http://189.6.108.114:5003/c3pr",
             command: "walkmod apply sonar:StringCheckOnLeft",
             toolMeta: {rule: "sonar:StringCheckOnLeft"},
             prTitle: "My PR Title",
