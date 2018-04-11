c3ad2867b883131da8fd5b2896675f0e5c7d2f5c
Antonio Jr <acdcjunior@gmail.com>
2018-04-10 21:14:04 -0300

    chore: update c3prSH usage

--- src/domain/pr/createPR.js
+++ src/domain/pr/createPR.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const uuidv4 = require('uuid/v4');
-const shell = require('node-git-client').shell;
+const c3prSH = require("node-git-client").c3prSH;
 
 const createPullRequest = require("../github/createPullRequest");
 const createForkIfNotExists = require("../github/createForkIfNotExists");
@@ -11,8 +11,7 @@ const STAGE_REPOS_FOLDER = process.env.STAGE_REPOS_FOLDER || '/tmp/';
 
 
 async function createPR({mainRepoOrgRepo, mainRepoBranch, mainRepoHash, gitHubApiToken, gitUserName, gitUserEmail, prCommitMessage, prTitle, prBody, patchContent}) {
-    const nodeName = 'c3pr-repo-github';
-    const prefix = `[${mainRepoHash}] [createPR]`;
+    const logMeta = {nodeName: 'c3pr-repo-github', correlationIds: mainRepoHash, moduleName: 'createPR'};
 
     const gitHubApiTokenReplacement = {regex: new RegExp(gitHubApiToken, "g"), replaceWith: "<GITHUB_API_TOKEN>"};
 
@@ -27,15 +26,15 @@ async function createPR({mainRepoOrgRepo, mainRepoBranch, mainRepoHash, gitHubAp
     const forkRepoCloneUrl = data.clone_url.replace(`https://github.com/`, `https://${gitHubApiToken}:${gitHubApiToken}@github.com/`);
     const forkRepoBranch = stagingFolderName;
 
-    await shell(`git init ${stagingFolder}`, {nodeName, prefix});
+    await c3prSH(`git init ${stagingFolder}`, {logMeta});
 
     // create brand new orphan branch
-    await shell(`git checkout --orphan ${forkRepoBranch}`, {cwd: stagingFolder}, {nodeName, prefix});
+    await c3prSH(`git checkout --orphan ${forkRepoBranch}`, {cwd: stagingFolder}, {logMeta});
     // add main repo, fetch it and merge into recently created branch
 
-    await shell(`git remote add main ${mainRepoCloneUrl}`, {cwd: stagingFolder}, {nodeName, prefix, replacements: [gitHubApiTokenReplacement]});
-    await shell(`git fetch main ${mainRepoBranch}`, {cwd: stagingFolder}, {nodeName, prefix});
-    await shell(`git merge main/${mainRepoBranch}`, {cwd: stagingFolder}, {nodeName, prefix});
+    await c3prSH(`git remote add main ${mainRepoCloneUrl}`, {cwd: stagingFolder}, {replacements: [gitHubApiTokenReplacement], logMeta});
+    await c3prSH(`git fetch main ${mainRepoBranch}`, {cwd: stagingFolder}, {logMeta});
+    await c3prSH(`git merge main/${mainRepoBranch}`, {cwd: stagingFolder}, {logMeta});
 
     // APPLY CHANGES
     const patchFileName = `c3pr-${uuidv4()}.patch`;
@@ -46,19 +45,19 @@ async function createPR({mainRepoOrgRepo, mainRepoBranch, mainRepoHash, gitHubAp
     // const patchContent = Buffer.from(fs.readFileSync('myPatch.patch', 'hex'), 'hex').toString('base64');
     fs.writeFileSync(patchFilePath, Buffer.from(patchContent, 'base64').toString('hex'), 'hex');
 
-    await shell(`git apply ${patchFileName}`, {cwd: stagingFolder}, {nodeName, prefix});
+    await c3prSH(`git apply ${patchFileName}`, {cwd: stagingFolder}, {logMeta});
     fs.unlinkSync(patchFilePath);
 
     // ADD and COMMIT CHANGES
-    await shell(`git add -A`, {cwd: stagingFolder}, {nodeName, prefix});
+    await c3prSH(`git add -A`, {cwd: stagingFolder}, {logMeta});
     const userNameNoQuotes = gitUserName.replace(/'/g, '');
     const userEmailNoQuotes = gitUserEmail.replace(/'/g, '');
-    await shell(`git -c user.name='${userNameNoQuotes}' -c user.email='${userEmailNoQuotes}' commit -m "${prCommitMessage.replace(/"/g, '\\"')}"`, {cwd: stagingFolder}, {nodeName, prefix});
+    await c3prSH(`git -c user.name='${userNameNoQuotes}' -c user.email='${userEmailNoQuotes}' commit -m "${prCommitMessage.replace(/"/g, '\\"')}"`, {cwd: stagingFolder}, {logMeta});
 
     // add fork repo
-    await shell(`git remote add fork ${forkRepoCloneUrl}`, {cwd: stagingFolder}, {nodeName, prefix, replacements: [gitHubApiTokenReplacement]});
+    await c3prSH(`git remote add fork ${forkRepoCloneUrl}`, {cwd: stagingFolder}, {replacements: [gitHubApiTokenReplacement], logMeta});
     // push changes
-    await shell(`git push -u fork ${forkRepoBranch}`, {cwd: stagingFolder}, {nodeName, prefix});
+    await c3prSH(`git push -u fork ${forkRepoBranch}`, {cwd: stagingFolder}, {logMeta});
 
     // CREATE PULL REQUEST VIA API
     await createPullRequest(mainRepoOrgRepo, mainRepoBranch, forkRepoOrg, forkRepoBranch, prTitle, prBody);
