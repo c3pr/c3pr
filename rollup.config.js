9cecc019f3442fb82494d7e8eedf6d0cef1c4253
Antonio Jr <acdcjunior@gmail.com>
2018-04-11 12:39:43 -0300

    feat: add rollup

---
+++ rollup.config.js
@@ -0,0 +1,19 @@
+import commonjs from 'rollup-plugin-commonjs';
+import resolve from 'rollup-plugin-node-resolve';
+import json from 'rollup-plugin-json';
+
+export default {
+    input: 'src/index.js',
+    output: {
+        file: 'dist/c3pr-agent.js',
+        format: 'cjs'
+    },
+    external: ['events', 'path', 'tty', 'util', 'fs', 'net', 'buffer', 'string_decoder', 'stream', 'zlib', 'querystring', 'url', 'http', 'crypto', 'module', 'tls',
+        'os', 'process', 'dns', 'punycode', 'https', 'dgram', 'assert', 'child_process'],
+
+    plugins: [
+        resolve(),
+        commonjs(),
+        json()
+    ],
+};
