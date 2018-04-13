d32cee8b9b8a1120c5766660cbfc1099161b42ba
Antonio Jr <acdcjunior@gmail.com>
2018-04-13 14:18:38 -0300

    fix: remove unused plugin

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,6 @@
 import commonjs from 'rollup-plugin-commonjs';
 import resolve from 'rollup-plugin-node-resolve';
 import json from 'rollup-plugin-json';
-import replace from 'rollup-plugin-replace';
-
 
 export default {
     input: 'src/index.js',
