3774db10ec45edae009ee518248ff706555bed32
Antonio Jr <acdcjunior@gmail.com>
2018-04-13 13:52:12 -0300

    refactor: replace request with axios + remove now unnecessary rollup patching

--- src/domain/sendPatchToBot.js
+++ src/domain/sendPatchToBot.js
@@ -1,30 +1,21 @@
-const request = require('request');
+const axios = require('axios');
 const c3prLOG = require("node-c3pr-logger");
 
 function sendPatchToBot(patchesUrl, patchesPayload) {
-    c3prLOG(`Called with ${patchesPayload}`, {nodeName: 'c3pr-agent', correlationId: patchesPayload.meta.correlationId, moduleName: 'sendPatchToBot'});
+    const logMeta = {nodeName: 'c3pr-agent', correlationId: patchesPayload.meta.correlationId, moduleName: 'sendPatchToBot'};
+    c3prLOG(`Called with ${patchesPayload}`, logMeta);
 
-    request.post(
-        {
-            url: patchesUrl,
-            json: true,
-            body: patchesPayload
-        },
-        function (error, response, body) {
-            if (error || response.statusCode !== 200) {
-                c3prLOG(`Error while sending patch to bot.
-                * URL: ${patchesUrl}
-                * Status: ${(response || {}).statusCode}
-                * Error: ${error}
-                * Body:
-                -----------------------\n
-                ${JSON.stringify(body, null, 2)}
-                -----------------------\n\n`, {nodeName: 'c3pr-agent', correlationId: patchesPayload.meta.correlationId, moduleName: 'sendPatchToBot'});
-            } else {
-                c3prLOG(`Sent patch to bot at ${patchesUrl}.`, {nodeName: 'c3pr-agent', correlationId: patchesPayload.meta.correlationId, moduleName: 'sendPatchToBot'});
-            }
-        }
-    );
+    axios.post(patchesUrl, patchesPayload)
+        .then(() => {
+            c3prLOG(`Sent patch to bot at ${patchesUrl}.`, logMeta);
+        }).catch(function (...error) {
+            c3prLOG(`Error while sending patch to bot.
+            * URL: ${patchesUrl}
+            * Error:
+            -----------------------\n
+            ${JSON.stringify({...error}, null, 2)}
+            -----------------------\n\n`, logMeta);
+        });
 }
 
 module.exports = sendPatchToBot;
