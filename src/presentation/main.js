86240e7e4541f79a86714fb82a739e11b33f443c
Antonio Jr <acdcjunior@gmail.com>
2018-04-14 21:32:09 -0300

    feat: display if log env var is set

--- src/presentation/main.js
+++ src/presentation/main.js
@@ -1,3 +1,4 @@
+const c3prLOG = require("node-c3pr-logger");
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -10,10 +11,12 @@ require('./c3prController')(app);
 
 // The 404 Route (ALWAYS Keep this as the last route)
 app.get('*', function(req, res){
-    res.status(404).send('This is the C3PR Agent (v. __C3PR_AGENT_EXECUTABLE_VERSION__), I am fine, thanks. Btw, no endpoint is listening at the requested location.');
+    res.status(404).send('This is the C3PR Agent (v. __C3PR_AGENT_EXECUTABLE_VERSION__). ' +
+        `c3prLOG env var is ${!c3prLOG.isEnvVarSet() ? 'not' : ''} set.` +
+        '. Btw, no endpoint is listening at the requested location.');
 });
 
 
 app.listen(PORT, () => {
-    console.log(`c3pr-agent version __C3PR_AGENT_EXECUTABLE_VERSION__ now listening at port ${PORT}.`);
+    c3prLOG(`c3pr-agent version __C3PR_AGENT_EXECUTABLE_VERSION__ now listening at port ${PORT}.`, {nodeName: 'c3pr-agent', correlationIds: 'boot', moduleName: 'main'});
 });
