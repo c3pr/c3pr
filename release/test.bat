6e7303d4d46c27fb740e5f0c52ebadbb673236fa
Antonio Jr <acdcjunior@gmail.com>
2018-04-26 14:39:09 -0300

    test: kill image if running

--- release/test.bat
+++ release/test.bat
@@ -1,5 +1,6 @@
 @echo off
 call version.bat
+docker kill c3pr-agent-test-can-delete
 docker build -f test.Dockerfile --build-arg AGENT_VERSION=%VERSION% -t c3pr-agent-test-can-delete --rm=false .
-docker run -p 1234:5003 c3pr-agent-test-can-delete
+docker run -p 1234:5003 --name c3pr-agent-test-can-delete c3pr-agent-test-can-delete
 start "" http://localhost:1234
