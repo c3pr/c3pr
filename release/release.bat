f4dd2eef59f8f6d4775d500980cb587181853148
Antonio Jr <acdcjunior@gmail.com>
2018-04-26 14:40:16 -0300

    feat: docker should have agent-executable instead now

--- release/release.bat
+++ release/release.bat
@@ -1,7 +1,8 @@
 @echo off
 call version.bat
-cd ..
-call npm run bundle
-cd release
+rem cd ..
+rem call npm run bundle
+rem cd release
 docker build -f release.Dockerfile --build-arg AGENT_VERSION=%VERSION% -t c3pr-agent-release-can-delete --rm=false .
-docker run -v %~dp0..\dist:/c3pr -v %~dp0:/release c3pr-agent-release-can-delete
+rem docker run -v %~dp0..\dist:/c3pr -v %~dp0:/release c3pr-agent-release-can-delete
+docker run -v %~dp0:/c3pr c3pr-agent-release-can-delete
