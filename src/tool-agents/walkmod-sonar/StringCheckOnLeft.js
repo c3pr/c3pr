9623344dc86b47385797e7f745ddae6ce01e394b
Antonio Jr <acdcjunior@gmail.com>
2018-05-04 12:50:21 -0300

    tool(StringCheckOnLeft): add filename argument and improve description

--- src/tool-agents/walkmod-sonar/StringCheckOnLeft.js
+++ src/tool-agents/walkmod-sonar/StringCheckOnLeft.js
@@ -2,11 +2,12 @@ module.exports = {
     toolId: "walkmod-sonar:StringCheckOnLeft",
     extensions: ["java"],
     agentURL: "https://c3pr-tool-walkmod-sonar.now.sh/c3pr",
-    command: "walkmod apply sonar:StringCheckOnLeft",
+    command: "walkmod apply sonar:StringCheckOnLeft -i #{filename}",
     toolMeta: {rule: "sonar:StringCheckOnLeft"},
     prTitle: "Strings literals should be placed on the left side when checking for equality",
     prBody: `
-It is preferable to place string literals on the left-hand side of an \`equals()\` or \`equalsIgnoreCase()\` method call. This prevents null pointer exceptions from being raised, as a string literal can never be \`null\` by definition.
+It is preferable to place string literals on the left-hand side of an \`equals()\` or \`equalsIgnoreCase()\` method call.
+ This prevents null pointer exceptions from being raised, as a string literal can never be \`null\` by definition.
 
 The following code:
 
