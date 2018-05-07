0e1d452d79a134275d2c2a9b8a42797b61879e27
Antonio Jr <acdcjunior@gmail.com>
2018-05-07 14:10:20 -0300

    feat(webhook): use externally defined handleWebhook

--- src/application/handleWebhook.js
+++ src/application/handleWebhook.js
@@ -1,16 +1,8 @@
-const c3prLOG = require("node-c3pr-logger");
-const convertWebhookToChanges = require('../domain/webhook/convertWebhookToChanges');
+const handleWebhook = require('node-c3pr-repo/webhook/handleWebhook');
 
 const config = require('../config');
-const notifyC3prBotOfChanges = require('node-c3pr-repo').changes.notifyC3prBotOfChanges;
+const convertWebhookToChanges = require('../domain/webhook/convertWebhookToChanges');
 
-function handleWebhook(webhookPayload) {
-    c3prLOG(`Handling webhook invoked for ${webhookPayload.repository.clone_url} - m: ${webhookPayload.head_commit.message}...`,
-        {webhookPayload}, {nodeName: 'c3pr-repo-gitlab', correlationId: webhookPayload.after, moduleName: 'handleWebhook'});
-
-    const changes = convertWebhookToChanges(webhookPayload);
-
-    notifyC3prBotOfChanges(config, changes);
-}
-
-module.exports = handleWebhook;
+module.exports = function (webhookPayload) {
+    handleWebhook(config, convertWebhookToChanges, webhookPayload)
+};
