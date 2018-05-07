f5a8fcda6c1735d5e3e53edb5e9c2e5215d89110
Antonio Jr <acdcjunior@gmail.com>
2018-05-07 13:07:53 -0300

    feat(changes): propagate logMetas

--- src/changes/notifyC3prBotOfChanges.ts
+++ src/changes/notifyC3prBotOfChanges.ts
@@ -1,29 +1,30 @@
 const c3prLOG = require("node-c3pr-logger");
 const axios = require('axios');
 
-async function notifyC3prBotOfChanges(config, changes) {
-    const logMeta = {nodeName: 'c3pr-repo-gitlab', correlationId: changes.meta && changes.meta.correlationId, moduleName: 'notifyC3prBotOfChanges'};
+async function notifyC3prBotOfChanges(config, changes, ...logs) {
+    const logMeta = {nodeName: 'node-c3pr-repo', correlationId: changes.meta && changes.meta.correlationId, moduleName: 'notifyC3prBotOfChanges'};
+    const logMetas = [...logs, logMeta];
 
     if (!changes.meta ||
         !changes.meta.correlationId ||
         !changes.meta.compatibleSchemas ||
         !changes.meta.compatibleSchemas.includes("c3pr/c3pr::changes")) {
         const errorMessage = `SKIPPING: Request does not contain required metadata (meta.correlationId and meta.compatibleSchemas). Meta: ${JSON.stringify(changes.meta)}.`;
-        c3prLOG(errorMessage, {changes}, logMeta);
+        c3prLOG(errorMessage, {changes}, ...logMetas);
         return;
     }
 
     try {
-        c3prLOG(`Notifying ${config.c3pr.changesUrl} of changes to ${changes.repository.url}...`, logMeta);
+        c3prLOG(`Notifying ${config.c3pr.changesUrl} of changes to ${changes.repository.url}...`, ...logMetas);
         await axios.post(config.c3pr.changesUrl, changes);
-        c3prLOG(`Notified ${config.c3pr.changesUrl} of changes to ${changes.repository.url}.`, {changes}, logMeta);
+        c3prLOG(`Notified ${config.c3pr.changesUrl} of changes to ${changes.repository.url}.`, {changes}, ...logMetas);
     } catch (e) {
         c3prLOG(`Error while notifying bot.
             * URL: ${config.c3pr.changesUrl}
             * Error:
             -----------------------\n
             ${require('util').inspect(e)}
-            -----------------------\n\n`, logMeta);
+            -----------------------\n\n`, ...logMetas);
     }
 }
 
