e1a163b62a0f92f0b881e09361550b236d262fdf
Antonio Jr <acdcjunior@gmail.com>
2018-05-08 22:08:00 -0300

    feat(createPR): implement createMergeRequest

---
+++ src/domain/gitlab/createMergeRequest.js
@@ -0,0 +1,32 @@
+const axios = require('axios');
+const config = require('../../config');
+
+const getGitLabProject = require('./getGitLabProject');
+
+async function createMergeRequest(mainRepoOrgRepo, mainRepoBranch, forkRepoOrg, forkRepoProject, forkRepoBranch, prTitle, prBodyMarkdown) {
+
+    let {id: mainRepoId} = await getGitLabProject(mainRepoOrgRepo);
+    let forkRepoId = encodeURIComponent(forkRepoOrg+"/"+forkRepoProject);
+
+    let {data: mergeRequestCreation} = await axios.post(
+        `${config.c3pr.gitLabUrl}/api/v4/projects/${forkRepoId}/merge_requests`,
+        {
+            //"id": forkRepoId,               // integer/string yes	The ID or URL-encoded path of the project owned by the authenticated user
+            "source_branch": forkRepoBranch,  // string	       yes	The source branch
+            "target_branch": mainRepoBranch,  // string	       yes	The target branch
+            "title": prTitle,                 // string	       yes	Title of MR
+            //"assignee_id": "",              // integer       no	Assignee user ID
+            "description": prBodyMarkdown,    // string	       no	Description of MR
+            "target_project_id": mainRepoId,  // integer       no	The target project (numeric id)
+            //"labels": "",                   // string	       no	Labels for MR as a comma-separated list
+            //"milestone_id": "",             // integer       no	The ID of a milestone
+            //"remove_source_branch": "",     // boolean       no	Flag indicating if a merge request should remove the source branch when merging
+            "allow_maintainer_to_push": true, // boolean       no	Whether or not a maintainer of the target project can push to the source branch
+        },
+        {headers: {"PRIVATE-TOKEN": config.c3pr.gitLabApiToken}}
+    );
+
+    return mergeRequestCreation;
+}
+
+module.exports = createMergeRequest;
---
+++ src/domain/gitlab/createMergeRequest.test.demo.js
@@ -0,0 +1,27 @@
+const createMergeRequest = require('./createMergeRequest');
+
+(async () => {
+
+    try {
+
+        /*
+        NOTE: this requires the existence of the c3pr-bot/sample_user__sample-project-java-maven project and a commit in the prCreationTestBranch.
+         */
+
+        let r = await createMergeRequest(
+            'sample_user/sample-project-java-maven',
+            'master',
+            'c3pr-bot',
+            'sample_user__sample-project-java-maven',
+            'prCreationTestBranch',
+            'my pr title',
+            'my pr `desc`'
+        );
+        console.log(JSON.stringify(r));
+
+
+    } catch (e) {
+        console.error(e.response.data);
+    }
+
+})();
