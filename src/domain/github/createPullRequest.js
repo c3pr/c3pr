5170e963d8deadad998e6803b3947206eb6585b1
Antonio Jr <acdcjunior@gmail.com>
2018-05-08 21:47:58 -0300

    feat(createPR): handle forkRepoProject

--- src/domain/github/createPullRequest.js
+++ src/domain/github/createPullRequest.js
@@ -3,7 +3,7 @@ const githubClient = require('octonode').client(config.c3pr.gitHubApiToken);
 
 // https://github.com/pksunkara/octonode#create-a-pull-request-post-repospksunkarahubpulls
 // https://developer.github.com/v3/pulls/#create-a-pull-request
-function createPullRequest(mainRepoOrgRepo, mainRepoBranch, forkRepoOrg, forkRepoBranch, prTitle, prBodyMarkdown) {
+function createPullRequest(mainRepoOrgRepo, mainRepoBranch, forkRepoOrg, forkRepoProject, forkRepoBranch, prTitle, prBodyMarkdown) {
     const ghrepo = githubClient.repo(mainRepoOrgRepo);
     return new Promise((resolve, reject) => {
         // noinspection JSUnusedLocalSymbols
