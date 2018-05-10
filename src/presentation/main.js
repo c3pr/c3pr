4f4a828135a8a68d2191c399acb8ac75f7de31d7
Antonio Jr <acdcjunior@gmail.com>
2018-05-10 12:32:43 -0300

    chore: remove line break

--- src/presentation/main.js
+++ src/presentation/main.js
@@ -20,8 +20,7 @@ app.get('*', function(req, res){
 app.listen(config.c3pr.agent.port, () => {
     const logMeta = {nodeName: 'c3pr-agent', correlationIds: 'boot', moduleName: 'main'};
 
-    c3prLOG(`c3pr-agent version __C3PR_AGENT_EXECUTABLE_VERSION__ now listening at ${config.c3pr.agent.agentUrl}.
-Agent ID: ${config.c3pr.agent.agentId}`, logMeta);
+    c3prLOG(`c3pr-agent version __C3PR_AGENT_EXECUTABLE_VERSION__ now listening at ${config.c3pr.agent.agentUrl}. Agent ID: ${config.c3pr.agent.agentId}`, logMeta);
     console.log();
 
     c3prLOG(`Now broadcasting to C-3PR registry API: ${config.c3pr.registryUrl}.`, logMeta);
