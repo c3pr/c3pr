4fd13bfa554f8e9f09cea228aa26ec06d8c5a921
Antonio Jr <acdcjunior@gmail.com>
2018-05-10 10:31:56 -0300

    feat(config): infer port from agent URL

--- src/config.js
+++ src/config.js
@@ -1,11 +1,11 @@
-const port = process.env.PORT || 5003;
+const agentUrl = process.env.C3PR_AGENT_URL || `http://${os.hostname()}:${port}`;
 
 module.exports = {
     c3pr: {
         registryUrl: process.env.C3PR_DASHBOARD_URL || 'http://host.docker.internal:5000',
         agent: {
-            port,
-            agentUrl: process.env.C3PR_AGENT_URL || `http://${os.hostname()}:${port}`,
+            agentUrl: agentUrl,
+            port: require('url').parse(agentUrl).port || 80,
             agentId: process.env.C3PR_AGENT_ID,
             cloneDir: process.env.C3PR_CLONE_DIR || '/tmp/c3pr/clones',
             cloneDepth: process.env.C3PR_CLONE_DEPTH || 50
