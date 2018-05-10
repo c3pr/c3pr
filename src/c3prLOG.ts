20f7e61625cf7a3d01e221dc3256386c4b4dce32
Antonio Jr <acdcjunior@gmail.com>
2018-05-10 12:32:25 -0300

    feat(c3prLOG): improve logged msg

--- src/c3prLOG.ts
+++ src/c3prLOG.ts
@@ -12,7 +12,7 @@ function arrayfy<T>(o: T | T[]): T[] {
 let warningShown = false;
 function showWarningIfDatabaseNotDefined() {
     if (!config.c3pr.mongoLogsUri && !warningShown) {
-        console.log('Logs: MONGO_LOGS_URI env var is not defined. Printing to STDOUT only. (This message will be only printed once every 5mins.)');
+        console.log('Logs: MONGO_LOGS_URI env var is not defined. Printing to STDOUT only. (This message will be printed only once every 5 minutes.)');
         warningShown = true;
         setTimeout(() => warningShown = false, 5 * 60 * 1000).unref();
     }
