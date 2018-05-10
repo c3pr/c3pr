73759de78231234db3f785dff8911a842def60aa
Antonio Jr <acdcjunior@gmail.com>
2018-05-10 10:21:46 -0300

    feat(registry): add URL broadcasting to c3pr registry

--- src/config.js
+++ src/config.js
@@ -1,7 +1,12 @@
+const port = process.env.PORT || 5003;
 
 module.exports = {
     c3pr: {
+        registryUrl: process.env.C3PR_DASHBOARD_URL || 'http://host.docker.internal:5000',
         agent: {
+            port,
+            agentUrl: process.env.C3PR_AGENT_URL || `http://${os.hostname()}:${port}`,
+            toolId: process.env.C3PR_AGENT_URL || `http://${os.hostname()}:${port}`,
             cloneDir: process.env.C3PR_CLONE_DIR || '/tmp/c3pr/clones',
             cloneDepth: process.env.C3PR_CLONE_DEPTH || 50
         }
--- src/presentation/main.js
+++ src/presentation/main.js
@@ -1,8 +1,9 @@
 const c3prLOG = require("node-c3pr-logger");
 const express = require('express');
 const bodyParser = require('body-parser');
+const config = require('../config');
+const axios = require('axios');
 
-const PORT = process.env.PORT || 5003;
 const app = express();
 
 app.use(bodyParser.json());
@@ -16,7 +17,16 @@ app.get('*', function(req, res){
         `Btw, no endpoint is listening at ${req.url}.`);
 });
 
+app.listen(config.c3pr.agent.port, () => {
+    const logMeta = {nodeName: 'c3pr-agent', correlationIds: 'boot', moduleName: 'main'};
 
-app.listen(PORT, () => {
-    c3prLOG(`c3pr-agent version __C3PR_AGENT_EXECUTABLE_VERSION__ now listening at port ${PORT}.`, {nodeName: 'c3pr-agent', correlationIds: 'boot', moduleName: 'main'});
+    c3prLOG(`c3pr-agent version __C3PR_AGENT_EXECUTABLE_VERSION__ now listening at ${config.c3pr.agent.agentUrl}.
+Tool ID: ${config.c3pr.agent.toolId}`, logMeta);
+    console.log();
+
+    c3prLOG(`Now broadcasting to C-3PR registry.`, logMeta);
+    setInterval(() => {
+        axios.patch(config.c3pr.registryUrl, {key: `agent//${config.c3pr.agent.toolId}`, value: config.c3pr.agent.agentUrl, timeout: 13 * 1000})
+    }, 10 * 1000);
+
 });
