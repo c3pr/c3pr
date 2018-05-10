737d9742b932f9d611c0a0fcf4ec7d37e0a9f995
Antonio Jr <acdcjunior@gmail.com>
2018-05-10 11:33:02 -0300

    fix(config): fix registry default url

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ const agentUrl = process.env.C3PR_AGENT_URL || `http://${os.hostname()}:5003`;
 
 module.exports = {
     c3pr: {
-        registryUrl: process.env.C3PR_REGISTRY_URL || 'http://host.docker.internal:5000',
+        registryUrl: process.env.C3PR_REGISTRY_URL || 'http://host.docker.internal:5000/api/v1/registry',
         agent: {
             agentUrl: agentUrl,
             port: require('url').parse(agentUrl).port || 80,
