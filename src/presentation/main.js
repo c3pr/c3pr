61526a689efdd3b015fe3665c5a47071d781037f
Antonio Jr <acdcjunior@gmail.com>
2018-05-10 11:18:56 -0300

    feat(main): add broadcasting to registry

--- src/presentation/main.js
+++ src/presentation/main.js
@@ -2,6 +2,7 @@ const c3prLOG = require("node-c3pr-logger");
 const express = require('express');
 const bodyParser = require('body-parser');
 const config = require('../config');
+const axios = require('axios');
 
 const app = express();
 
@@ -18,5 +19,15 @@ app.get('*', function(req, res){
 });
 
 app.listen(config.c3pr.port, () => {
-    c3prLOG(`C-3PR BRAIN is up at port ${config.c3pr.port}.`, {nodeName: 'c3pr-brain', correlationIds: 'boot', moduleName: 'main'});
+    const logMeta = {nodeName: 'c3pr-brain', correlationIds: 'boot', moduleName: 'main'};
+
+    c3prLOG(`C-3PR BRAIN is up at port ${config.c3pr.port}.`, logMeta);
+
+    c3prLOG(`Now broadcasting to C-3PR registry.`, logMeta);
+    setInterval(() => {
+        axios.patch(config.c3pr.registryUrl, [
+            {key: `changesUrl`, value: config.c3pr.changesUrl, timeout: 13 * 1000},
+            {key: `patchesUrl`, value: config.c3pr.patchesUrl, timeout: 13 * 1000}
+        ]);
+    }, 10 * 1000);
 });
