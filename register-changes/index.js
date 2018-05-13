115740ba9e70279bfa25b9a967ccede9c442393a
Antonio Jr <acdcjunior@gmail.com>
2018-05-13 12:05:36 -0300

    [2.2.0] feat(changes): create reusable registerChanges() function

---
+++ register-changes/index.js
@@ -0,0 +1,32 @@
+const c3prLOG2 = require("node-c3pr-logger/c3prLOG2").c3pr.c3prLOG2;
+
+const axios = require('axios');
+
+async function registerChanges(changes, {changesUrl, jwt, logMetas: outerLogMetas}) {
+    const logMeta = {nodeName: 'node-c3pr-repo', correlationId: changes.meta && changes.meta.correlationId, moduleName: 'notifyC3prBotOfChanges'};
+    const logMetas = [...(outerLogMetas || []), logMeta];
+
+    try {
+        c3prLOG2({msg: `Notifying HUB (${changesUrl}) of changes to ${changes.repository.url}...`, logMetas});
+
+        await axios.post(changesUrl, {headers: {Authentication: `Bearer ${jwt}`}}, changes);
+
+        c3prLOG2({msg: `Notified HUB (${changesUrl}) of changes to ${changes.repository.url}.`, logMetas});
+    } catch (e) {
+        c3prLOG2({
+            msg: `Error while notifying bot.
+            * URL: ${changesUrl}
+            * Error:
+            -----------------------\n
+            ${require('util').inspect(e)}
+            -----------------------\n\n`,
+            logMetas
+        });
+    }
+}
+
+module.exports = {
+    c3pr: {
+        registerChanges
+    }
+};
