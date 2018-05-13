9487c5168b034c60d0d8c11c00e3e37f6fe2f586
Antonio Jr <acdcjunior@gmail.com>
2018-05-13 11:49:37 -0300

    [8.0.0] feat(c3prLOG2): export type definitions and move c3prLOG2 to another folder

---
+++ c3prLOG2/index.d.ts
@@ -0,0 +1,7 @@
+export declare const c3pr: {
+    c3prLOG2({ msg, logMetas, meta }: {
+        msg: any;
+        logMetas: any;
+        meta: any;
+    }): void;
+};
---
+++ c3prLOG2/index.ts
@@ -0,0 +1,7 @@
+const c3prLOG_original = require('../src/c3prLOG');
+
+export const c3pr = {
+    c3prLOG2({msg, logMetas, meta}) {
+        c3prLOG_original(msg, meta || {}, ...logMetas);
+    }
+};
