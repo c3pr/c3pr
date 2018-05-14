a77d949878cd6ce24ce093de0dc556d79c37b082
Antonio Jr <acdcjunior@gmail.com>
2018-05-14 12:57:52 -0300

    feat(login): create login function with auto-retry

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-c3pr-hub-client
+
+Client for c3pr-hub in node.
---
+++ login/index.js
@@ -0,0 +1,34 @@
+const axios = require('axios');
+const c3prLOG2 = require("node-c3pr-logger/c3prLOG2").c3pr.c3prLOG2;
+
+const LOGIN_RETRY_TIME = 5 * 1000;
+
+async function login({loginUrl, subscriptions, logMetas: outerLogMetas}) {
+    const logMetas = [...(outerLogMetas || []), {nodeName: 'node-c3pr-hub-client', correlationId: 'login', moduleName: 'login'}];
+
+    try {
+        const {data: jwt} = await axios.post(loginUrl, subscriptions);
+        c3prLOG2({
+            msg: `Successfully logged in at ${loginUrl}.`,
+            logMetas,
+        });
+        return jwt;
+    } catch (e) {
+        c3prLOG2({
+            msg: `Error while logging in at ${loginUrl}. Reason: '${e}'.`,
+            logMetas,
+            meta: {error: require('util').inspect(e)}
+        });
+        return new Promise(resolve => {
+            setTimeout(() => {
+                resolve(login({loginUrl, subscriptions, logMetas: outerLogMetas}));
+            }, LOGIN_RETRY_TIME);
+        })
+    }
+}
+
+module.exports = {
+    c3prHubClient: {
+        login
+    }
+};
