719fed8f58d6cc9f8babe7932f2cd0e626171d65
Antonio Jr <acdcjunior@gmail.com>
2018-05-14 21:57:01 -0300

    feat(markEventAsProcessing): add markEventAsProcessing function

---
+++ events/markEventAsProcessing.js
@@ -0,0 +1,25 @@
+const axios = require('axios');
+const c3prLOG2 = require("node-c3pr-logger/c3prLOG2").c3pr.c3prLOG2;
+
+const logMeta = {nodeName: 'node-c3pr-hub-client', moduleName: 'markEventAsProcessing'};
+
+async function markEventAsProcessing({eventType, jwt, logMetas: outerLogMetas}) {
+    const headers = {Authorization: `Bearer ${jwt}`};
+
+    /** @namespace event.payload */
+    let {data: event} = await axios.get(`/api/v1/events/${eventType}/peek/unprocessed`, {headers});
+
+    try {
+        await axios.patch(`/api/v1/events/${eventType}/${event.uuid}/meta/processing`, {}, {headers});
+        return event.payload;
+    } catch (e) {
+        c3prLOG2({msg: `Error while marking event ${event.uuid} of type ${eventType} as processing.`, logMetas: [...(outerLogMetas || []), logMeta], meta: e});
+        throw e;
+    }
+}
+
+module.exports = {
+    c3prHubClient: {
+        markEventAsProcessing
+    }
+};
