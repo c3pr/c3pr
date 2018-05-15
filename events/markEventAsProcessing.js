5fc7ea016b99543e992b74ac9126da768af08b5a
Antonio Jr <acdcjunior@gmail.com>
2018-05-15 13:17:28 -0300

    fix(events): add c3prHubUrl argument

--- events/markEventAsProcessing.js
+++ events/markEventAsProcessing.js
@@ -3,14 +3,14 @@ const c3prLOG2 = require("node-c3pr-logger/c3prLOG2").c3pr.c3prLOG2;
 
 const logMeta = {nodeName: 'node-c3pr-hub-client', moduleName: 'markEventAsProcessing'};
 
-async function markEventAsProcessing({eventType, jwt, logMetas: outerLogMetas}) {
+async function markEventAsProcessing({eventType, c3prHubUrl, jwt, logMetas: outerLogMetas}) {
     const headers = {Authorization: `Bearer ${jwt}`};
 
     /** @namespace event.payload */
-    let {data: event} = await axios.get(`/api/v1/events/${eventType}/peek/unprocessed`, {headers});
+    let {data: event} = await axios.get(`${c3prHubUrl}/api/v1/events/${eventType}/peek/unprocessed`, {headers});
 
     try {
-        await axios.patch(`/api/v1/events/${eventType}/${event.uuid}/meta/processing`, {}, {headers});
+        await axios.patch(`${c3prHubUrl}/api/v1/events/${eventType}/${event.uuid}/meta/processing`, {}, {headers});
         return event.payload;
     } catch (e) {
         c3prLOG2({
--- events/registerNewEvent.js
+++ events/registerNewEvent.js
@@ -3,10 +3,10 @@ const c3prLOG2 = require("node-c3pr-logger/c3prLOG2").c3pr.c3prLOG2;
 
 const logMeta = {nodeName: 'node-c3pr-hub-client', moduleName: 'registerNewEvent'};
 
-async function registerNewEvent({eventType, payload, jwt, logMetas: outerLogMetas}) {
+async function registerNewEvent({eventType, payload, c3prHubUrl, jwt, logMetas: outerLogMetas}) {
     try {
         const headers = {Authorization: `Bearer ${jwt}`};
-        await axios.post(`/api/v1/events/${eventType}`, payload, {headers});
+        await axios.post(`${c3prHubUrl}/api/v1/events/${eventType}`, payload, {headers});
     } catch (e) {
         c3prLOG2({
             msg: `Error while registering new event of type ${eventType}. Reason: '${e}'. Data: ${e.response && e.response.data}`,
