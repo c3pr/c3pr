98ec92bfad6a1a4fd0ca46a07250c854799aeb60
Antonio Jr <acdcjunior@gmail.com>
2018-05-15 12:12:36 -0300

    feat(hubRegistryBroadcast): pick tools summary from ENV var + add some checks

--- src/application/hubRegistryBroadcast.js
+++ src/application/hubRegistryBroadcast.js
@@ -2,16 +2,22 @@ const axios = require('axios');
 
 const config = require('../config');
 
+const fs = require('fs');
+if (!fs.existsSync(config.c3pr.agent.agentToolsPath)) {
+    throw new Error(`Could not find tools file at 'C3PR_AGENT_TOOLS_PATH': ${config.c3pr.agent.agentToolsPath}`);
+}
+
+const tools = require(config.c3pr.agent.agentToolsPath);
+if (!tools.summary) {
+    throw new Error(`The object at 'C3PR_AGENT_TOOLS_PATH' (${config.c3pr.agent.agentToolsPath}) does not have a 'summary' property.`);
+}
+
 c3prLOG(`Now broadcasting to C-3PR registry API: ${config.c3pr.hub.registryUrl}.`, logMeta);
 setInterval(() => {
     axios.patch(config.c3pr.hub.registryUrl,
         {
             key: `agent://${config.c3pr.agent.agentId}`,
-            value: {
-                tool_id: "walkmod-sonar:AddCurlyBrackets",
-                extensions: ["java"],
-                tags: ["java"]
-            },
+            value: tools.summary,
             timeout: config.c3pr.hub.broadcastTimeoutInMs
         }
     ).catch((e) => {
--- src/config.js
+++ src/config.js
@@ -1,12 +1,15 @@
 const os = require("os");
-const isDocker = require("is-docker");
 
-const C3PR_HUB_URL = process.env.C3PR_HUB_URL || (isDocker() ? `http://host.docker.internal:5000` : `http://${os.hostname()}:5000`);
+const C3PR_HUB_URL = process.env.C3PR_HUB_URL || (require("is-docker")() ? `http://host.docker.internal:5000` : `http://${os.hostname()}:5000`);
 
 const agentUrl = process.env.C3PR_AGENT_URL || `http://${os.hostname()}:5003`;
+const agentToolsPath = process.env.C3PR_AGENT_TOOLS_PATH || `/c3pr/agent/tools`;
 
 module.exports = {
     c3pr: {
+        auth: {
+            jwt: null
+        },
         hub:{
             registryUrl: `${C3PR_HUB_URL}/api/v1/registry`,
 
@@ -14,7 +17,8 @@ module.exports = {
             broadcastIntervalInMs: 8 * 60 * 60 * 1000, // 8 hours
         },
         agent: {
-            agentUrl: agentUrl,
+            agentToolsPath,
+            agentUrl,
             port: require('url').parse(agentUrl).port || 80,
             agentId: process.env.C3PR_AGENT_ID,
             cloneDir: process.env.C3PR_CLONE_DIR || '/tmp/c3pr/clones',
