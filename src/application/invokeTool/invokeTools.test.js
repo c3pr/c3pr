ad109f0e6b67eeb4f5b7a5d5f4704d2c22bc0370
Antonio Jr <acdcjunior@gmail.com>
2018-05-14 22:37:20 -0300

    test(invokeTool): update tests

--- src/application/invokeTool/invokeTools.test.js
+++ src/application/invokeTool/invokeTools.test.js
@@ -19,20 +19,13 @@ config.c3pr.patchesUrl = 'http://changes-server/patches';
 
 require("node-c3pr-logger").testMode();
 
-const invokeTools = require('./invokeTools');
+const invokeTools = require('./invokeTools').c3prBrain.invokeTools;
 
 describe('invokeTools', () => {
 
     const changes = {
-        meta: {
-            correlationId: "4444eedacc076e8a16ae565b535fd48edb9a044a",
-            compatibleSchemas: ["c3pr/c3pr::changes"],
-            dates: [{node: "c3pr-repo-github", date: "2018-02-12T14:05:08.893Z", schema: "changes"}]
-        },
-        c3pr: {prsUrl: "http://c3pr-github.herokuapp.com/prs"},
         changeset: ['src/main/a/b/c/Main.java', 'src/main/a/b/c/Main.js', 'src/boo.txt'],
         repository: {
-            type: "git",
             url: "https://github.com/org/repo.git",
             branch: "my-branch-name",
             revision: "4444eedacc076e8a16ae565b535fd48edb9a044a"
@@ -57,31 +50,22 @@ describe('invokeTools', () => {
             {toolId: "two", extensions: ["js"], agentURL: "http://two", command: "two command", toolMeta: {rule: "two"}, prTitle: "prTitle two", prBody: `prBody two`},
         ];
 
-        const meta = {
-            compatibleSchemas: ["c3pr/c3pr-agent::toolInvocation"],
-            correlationId: "4444eedacc076e8a16ae565b535fd48edb9a044a",
-            dates: [{date: "2018-02-12T14:05:08.893Z", node: "c3pr-repo-github", schema: "changes"}, {date: now.toISOString(), node: "c3pr", schema: "toolInvocation"}]
-        };
-        const c3pr = {
-            prsUrl: "http://c3pr-github.herokuapp.com/prs",
-            patchesUrl: "http://changes-server/patches"
-        };
         const repository = changes.repository;
 
         axiosMock.onPost(toolAgents.agents[0].agentURL, {
-            meta, c3pr, repository,
+            repository,
             files: [changes.changeset[0], changes.changeset[1]],
             tool: toolAgents.agents[0]
         }).reply(204, {}, {'content-type': 'application/json'}); // calls on tool 'one' never produce diff
 
         axiosMock.onPost(toolAgents.agents[1].agentURL, {
-            meta, c3pr, repository,
+            repository,
             files: [changes.changeset[1]],
             tool: toolAgents.agents[1]
         }).reply(200, {files: [changes.changeset[1]]}, {'content-type': 'application/json'}); // calls on tool 'two' produces diff
 
         // execute
-        let toolsApplied = await invokeTools(changes);
+        let toolsApplied = await invokeTools({repository: changes.repository, files: changes.changeset});
 
         // verify
         expect(toolsApplied[0]).to.deep.equal({ toolId: 'one', diff: false, files: []});
@@ -96,31 +80,22 @@ describe('invokeTools', () => {
             {toolId: "two", extensions: ["java", "js"], agentURL: "http://two", command: "two command", toolMeta: {rule: "two"}, prTitle: "prTitle two", prBody: `prBody two`},
         ];
 
-        const meta = {
-            compatibleSchemas: ["c3pr/c3pr-agent::toolInvocation"],
-            correlationId: "4444eedacc076e8a16ae565b535fd48edb9a044a",
-            dates: [{date: "2018-02-12T14:05:08.893Z", node: "c3pr-repo-github", schema: "changes"}, {date: now.toISOString(), node: "c3pr", schema: "toolInvocation"}]
-        };
-        const c3pr = {
-            prsUrl: "http://c3pr-github.herokuapp.com/prs",
-            patchesUrl: "http://changes-server/patches"
-        };
         const repository = changes.repository;
 
         axiosMock.onPost(toolAgents.agents[0].agentURL, {
-            meta, c3pr, repository,
+            repository,
             files: [changes.changeset[0], changes.changeset[1]],
             tool: toolAgents.agents[0]
         }).reply(200, {files: [changes.changeset[0]]}, {'content-type': 'application/json'}); // calls on tool 'one' changes only one file, so the other tool can be invoked in the remaining one
 
         axiosMock.onPost(toolAgents.agents[1].agentURL, {
-            meta, c3pr, repository,
+            repository,
             files: [changes.changeset[1]],
             tool: toolAgents.agents[1]
         }).reply(200, {files: [changes.changeset[1]]}, {'content-type': 'application/json'}); // calls on tool 'two' changes the other file
 
         // execute
-        let toolsApplied = await invokeTools(changes);
+        let toolsApplied = await invokeTools({repository: changes.repository, files: changes.changeset});
 
         // verify
         expect(toolsApplied[0]).to.deep.equal({ toolId: 'one', diff: true, files: [changes.changeset[0]]});
