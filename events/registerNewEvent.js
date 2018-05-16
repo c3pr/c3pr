cbf08a89a99d38098ff7fba920b49c7317a76081
Antonio Jr <acdcjunior@gmail.com>
2018-05-16 13:31:52 -0300

    feat(registerNewEvent): add automatic retry

--- events/registerNewEvent.js
+++ events/registerNewEvent.js
@@ -1,12 +1,20 @@
 const axios = require('axios');
+const axiosRetry = require('axios-retry');
 const c3prLOG2 = require("node-c3pr-logger/c3prLOG2").c3pr.c3prLOG2;
 
 const logMeta = {nodeName: 'node-c3pr-hub-client', moduleName: 'registerNewEvent'};
 
-async function registerNewEvent({eventType, payload, c3prHubUrl, jwt, logMetas: outerLogMetas}) {
+
+
+async function registerNewEvent({eventType, payload, c3prHubUrl, jwt, logMetas: outerLogMetas, retryWait = 2000}) {
     try {
+        const client = axios.create({ baseURL: c3prHubUrl });
+        // noinspection JSUnusedGlobalSymbols
+        axiosRetry(client, { retries: 3, retryDelay: retryCount => retryCount * retryWait, retryCondition() { return true; } });
+
         const headers = {Authorization: `Bearer ${jwt}`};
-        await axios.post(`${c3prHubUrl}/api/v1/events/${eventType}`, payload, {headers});
+
+        await client.post(`/api/v1/events/${eventType}`, payload, {headers});
     } catch (e) {
         c3prLOG2({
             msg: `Error while registering new event of type ${eventType}. Reason: '${e}'. Data: ${e.response && e.response.data}`,
---
+++ events/registerNewEvent.test.js
@@ -0,0 +1,40 @@
+require("node-c3pr-logger").testMode();
+process.env.NODE_ENV = 'test';
+
+const expect = require('chai').expect;
+require('chai').should();
+
+const registerNewEvent = require('./registerNewEvent').c3prRNE.registerNewEvent;
+
+const axios = require('axios');
+const MockAdapter = require('axios-mock-adapter');
+const axiosMock = new MockAdapter(axios);
+
+describe('registerNewEvent', () => {
+
+    it('registerNewEvent automatically retries 3 times', async () => {
+
+        const eventType = "someEvent";
+        const c3prHubUrl = 'http://localhost:5004';
+        const jwt = "jwt";
+        const payload = {a: 123};
+
+        axiosMock
+            .onPost(`${c3prHubUrl}/api/v1/events/${eventType}`, payload).replyOnce(500)
+            .onPost(`${c3prHubUrl}/api/v1/events/${eventType}`, payload).replyOnce(500)
+            .onPost(`${c3prHubUrl}/api/v1/events/${eventType}`, payload).replyOnce(500)
+            .onPost(`${c3prHubUrl}/api/v1/events/${eventType}`, payload).replyOnce(200);
+
+        await registerNewEvent({
+            eventType,
+            c3prHubUrl,
+            jwt,
+            payload,
+            retryWait: 100
+        });
+
+        expect(2).to.equal(2);
+    });
+
+});
+
