c32d560e7d4cccf2e35e7e2f0067f91b88a352ed
Antonio Jr <acdcjunior@gmail.com>
2018-05-17 14:17:40 -0300

    feat(ChangesCommitted): use new collectEventAndMarkAsProcessing

--- src/application/ChangesCommitted/handleChangesCommitted.js
+++ src/application/ChangesCommitted/handleChangesCommitted.js
@@ -1,5 +1,5 @@
 const c3prLOG2 = require("node-c3pr-logger/c3prLOG2").c3pr.c3prLOG2;
-const c3prHubClient = require('node-c3pr-hub-client/events/markEventAsProcessing').c3prHubClient;
+const c3prCEAMAP = require('node-c3pr-hub-client/events/collectEventAndMarkAsProcessing').c3prCEAMAP;
 
 const c3prRTI = require('../invokeTool/invokeTools').c3prBrain;
 
@@ -10,14 +10,18 @@ const logMeta = {nodeName: 'c3pr-brain', moduleName: 'handleChangesCommitted'};
 function handleChangesCommitted() {
     c3prLOG2({msg: `Handling ChangesCommitted.`, logMetas: [logMeta]});
 
-    c3prHubClient.markEventAsProcessing(
+    c3prCEAMAP.collectEventAndMarkAsProcessing(
         {eventType: `ChangesCommitted`, c3prHubUrl: config.c3pr.hub.c3prHubUrl, jwt: config.c3pr.jwt, logMetas: [logMeta]}
     ).catch(() => {
         c3prLOG2({msg: `Couldn't collect ChangesCommitted. Skipping.`, logMetas: [logMeta]});
     }).then((changesCommitted) => {
+        const parent = {
+            event_type: `ChangesCommitted`,
+            uuid: changesCommitted.uuid
+        };
         // noinspection JSIgnoredPromiseFromCall
         /** @namespace changesCommitted.changed_files */
-        c3prRTI.invokeTools({repository: changesCommitted.repository, files: changesCommitted.changed_files});
+        c3prRTI.invokeTools({parent, repository: changesCommitted.repository, files: changesCommitted.changed_files});
     });
 }
 
