1fe18fa492e0e9335c305df800cb2c8058ee8723
Antonio Jr <acdcjunior@gmail.com>
2018-05-18 12:34:16 -0300

    fix(c3prLOG2): allow undefined logMetas

--- c3prLOG2/index.js
+++ c3prLOG2/index.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const c3prLOG_original = require('../src/c3prLOG');
 exports.c3pr = {
     c3prLOG2({ msg, logMetas, meta }) {
-        c3prLOG_original(msg, meta || {}, ...logMetas);
+        c3prLOG_original(msg, meta || {}, ...(logMetas || []));
     }
 };
 //# sourceMappingURL=index.js.map
---
+++ src/c3prLOG.d.ts
@@ -0,0 +1,30 @@
+declare const mongodb: any;
+declare const config: any;
+declare function wrap(arr: any, prefix?: string, suffix?: string): any;
+declare function arrayfy<T>(o: T | T[]): T[];
+declare let warningShown: boolean;
+declare function showWarningIfDatabaseNotDefined(): void;
+declare const emptyLogMeta: {
+    nodeName: string;
+    correlationIds: string[];
+    moduleNames: string[];
+}[];
+declare const emptyNodeName: {
+    nodeName: string;
+};
+declare function logWithMeta(message: string, metadata: any, logMetasArg: LogMeta[]): Promise<void>;
+declare function log(nodeName: string, correlationIds: string[], moduleNames: string[], message: string, metadata: any): Promise<void>;
+interface C3prLOG {
+    (): Promise<any>;
+    testMode(): void;
+    isEnvVarSet(): boolean;
+}
+interface LogMeta {
+    nodeName: string;
+    correlationId?: string;
+    moduleName?: string;
+    correlationIds?: string | string[];
+    moduleNames?: string | string[];
+}
+declare function isLogMeta(o: any): boolean;
+declare const c3prLOG: C3prLOG;
