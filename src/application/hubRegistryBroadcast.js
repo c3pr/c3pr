ece28fc1753794837b25c775101fec9ba75ac6ac
Antonio Jr <acdcjunior@gmail.com>
2018-05-18 12:13:04 -0300

    refactor(hubRegistryBroadcast): turn hubRegistryBroadcast into a function

--- src/application/hubRegistryBroadcast.js
+++ src/application/hubRegistryBroadcast.js
@@ -4,21 +4,25 @@ const c3prLOG2 = require("node-c3pr-logger/c3prLOG2").c3pr.c3prLOG2;
 const config = require('../config');
 const loadToolsSummary = require('./loadToolsSummary');
 
-const summary = loadToolsSummary();
+function hubRegistryBroadcast() {
+    const summary = loadToolsSummary();
 
-c3prLOG2({
-    msg: `Now broadcasting to C-3PR registry API: ${config.c3pr.hub.registryUrl}.`,
-    logMetas: [{nodeName: 'c3pr-agent', correlationIds: 'boot', moduleName: 'hubRegistryBroadcast'}],
-    meta: {summary}
-});
-setInterval(() => {
-    axios.patch(config.c3pr.hub.registryUrl,
-        {
-            key: `agent://${config.c3pr.agent.agentId}`,
-            value: summary,
-            timeout: config.c3pr.hub.broadcastTimeoutInMs
-        }
-    ).catch((e) => {
-        console.log(`Error while broadcasting to registry. URL: ${config.c3pr.hub.registryUrl}. - ${e}`);
+    c3prLOG2({
+        msg: `Now broadcasting to C-3PR registry API: ${config.c3pr.hub.registryUrl}.`,
+        logMetas: [{nodeName: 'c3pr-agent', correlationIds: 'boot', moduleName: 'hubRegistryBroadcast'}],
+        meta: {summary}
     });
-}, config.c3pr.hub.broadcastIntervalInMs);
+    setInterval(() => {
+        axios.patch(config.c3pr.hub.registryUrl,
+            {
+                key: `agent://${config.c3pr.agent.agentId}`,
+                value: summary,
+                timeout: config.c3pr.hub.broadcastTimeoutInMs
+            }
+        ).catch((e) => {
+            console.log(`Error while broadcasting to registry. URL: ${config.c3pr.hub.registryUrl}. - ${e}`);
+        });
+    }, config.c3pr.hub.broadcastIntervalInMs);
+}
+
+module.exports = hubRegistryBroadcast;
