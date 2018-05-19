facc4a1bcbc463cda3e91da42cb2090548f643a3
Antonio Jr <acdcjunior@gmail.com>
2018-05-19 20:24:44 -0300

    fix(C3PR_MONGO_URL): remove default value

--- src/config.js
+++ src/config.js
@@ -1,7 +1,9 @@
+const C3PR_MONGO_URL = process.env.C3PR_MONGO_URL;
+
 module.exports = {
     c3pr: {
         logger: {
-            mongoUrl: process.env.C3PR_MONGO_URL || 'mongodb://127.0.0.1:27017',
+            mongoUrl: C3PR_MONGO_URL,
             database: 'c3pr',
             collection: 'logs'
         }
