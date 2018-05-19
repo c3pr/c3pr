564ee4ef6653ce40910df33195cb0ab60cd74318
Antonio Jr <acdcjunior@gmail.com>
2018-05-19 16:25:36 -0300

    refactor(config): clean up env vars

--- src/config.js
+++ src/config.js
@@ -1,9 +1,13 @@
 const os = require("os");
 
 const C3PR_HUB_URL = process.env.C3PR_HUB_URL || (require("is-docker")() ? `http://host.docker.internal:5000` : `http://${os.hostname()}:5000`);
+const C3PR_AGENT_URL = process.env.C3PR_AGENT_URL || `http://${os.hostname()}:5003`;
 
-const agentUrl = process.env.C3PR_AGENT_URL || `http://${os.hostname()}:5003`;
-const agentToolsPath = process.env.C3PR_AGENT_TOOLS_PATH || `/c3pr/agent/tools`;
+// The env variables below are typically set at build time in the Dockerfile
+const C3PR_AGENT_TOOLS_PATH = process.env.C3PR_AGENT_TOOLS_PATH || `/c3pr/agent/tools`;
+const C3PR_AGENT_ID = process.env.C3PR_AGENT_ID;
+const C3PR_CLONE_DIR = process.env.C3PR_CLONE_DIR || '/tmp/c3pr/clones';
+const C3PR_CLONE_DEPTH = process.env.C3PR_CLONE_DEPTH || 50;
 
 module.exports = {
     c3pr: {
@@ -17,12 +21,12 @@ module.exports = {
             broadcastIntervalInMs: 8 * 60 * 60 * 1000, // 8 hours
         },
         agent: {
-            agentToolsPath,
-            agentUrl,
-            port: require('url').parse(agentUrl).port || 80,
-            agentId: process.env.C3PR_AGENT_ID,
-            cloneDir: process.env.C3PR_CLONE_DIR || '/tmp/c3pr/clones',
-            cloneDepth: process.env.C3PR_CLONE_DEPTH || 50
+            agentToolsPath: C3PR_AGENT_TOOLS_PATH,
+            agentUrl: C3PR_AGENT_URL,
+            port: require('url').parse(C3PR_AGENT_URL).port || 80,
+            agentId: C3PR_AGENT_ID,
+            cloneDir: C3PR_CLONE_DIR,
+            cloneDepth: C3PR_CLONE_DEPTH
         }
     }
 };
