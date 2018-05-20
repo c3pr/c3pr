fd881434c4bdb3f32fe318fb8ac95cd686f48b86
Antonio Jr <acdcjunior@gmail.com>
2018-05-20 16:36:01 -0300

    feat: add logMetas

--- src/web/c3prHubListenerController.js
+++ src/web/c3prHubListenerController.js
@@ -4,22 +4,24 @@ const c3prLOG2 = require("node-c3pr-logger/c3prLOG2").c3pr.c3prLOG2;
 const c3prHCC = require('../application/ChangesCommitted/handleChangesCommitted').c3pr;
 const c3prHTIC = require('../application/ToolInvocationCompleted/handleToolInvocationCompleted').c3pr;
 
+const logMetas = [{nodeName: 'c3pr-brain', moduleName: 'c3prHubListenerController'}];
+
 module.exports = function (app) {
 
     app.post(config.c3pr.brain.ChangesCommittedCallbackUrl, function (request, response) {
-        c3prLOG2({msg: `'ChangesCommitted' received.`});
+        c3prLOG2({msg: `'ChangesCommitted' received.`, logMetas});
         c3prHCC.handleChangesCommitted();
         response.send();
     });
 
     app.post(config.c3pr.brain.ToolInvocationCompletedCallbackUrl, function (request, response) {
-        c3prLOG2({msg: `'ToolInvocationCompleted' received.`});
+        c3prLOG2({msg: `'ToolInvocationCompleted' received.`, logMetas});
         c3prHTIC.handleToolInvocationCompleted();
         response.send();
     });
 
     app.post(config.c3pr.brain.PullRequestUpdatedCallbackUrl, function (request, response) {
-        c3prLOG2({msg: `'PullRequestUpdated' received.`});
+        c3prLOG2({msg: `'PullRequestUpdated' received.`, logMetas});
         response.send();
     });
 
