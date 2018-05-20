f53537d4a8eba8da1582547c7347e1e9209386ba
Antonio acdc Jr <acdcjunior@gmail.com>
2018-05-19 21:14:36 -0300

    feat: add script to run npm install in all modules

---
+++ node/npm-install-all.sh
@@ -0,0 +1,12 @@
+cd ../..
+cd c3pr-agent              && npm i && cd ..
+cd c3pr-brain              && npm i && cd ..
+cd c3pr-dashboard/frontend && npm i && cd ../..
+cd c3pr-dashboard          && npm i && cd ..
+cd c3pr-hub                && npm i && cd ..
+cd c3pr-repo-github        && npm i && cd ..
+cd c3pr-repo-gitlab        && npm i && cd ..
+cd node-c3pr-hub-client    && npm i && cd ..
+cd node-c3pr-logger        && npm i && cd ..
+cd node-c3pr-repo          && npm i && cd ..
+cd node-git-client         && npm i && cd ..
