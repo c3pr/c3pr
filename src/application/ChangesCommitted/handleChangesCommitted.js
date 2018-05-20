f4aa4381fe28104611832ceb1c3554fc0dca240b
Antonio Jr <acdcjunior@gmail.com>
2018-05-20 16:43:41 -0300

    feat(ChangesCommitted): add error handling

--- src/application/ChangesCommitted/handleChangesCommitted.js
+++ src/application/ChangesCommitted/handleChangesCommitted.js
@@ -5,23 +5,29 @@ const c3prRTI = require('../invokeTool/invokeTools').c3prBrain;
 
 const config = require('../../config');
 
-const logMeta = {nodeName: 'c3pr-brain', moduleName: 'handleChangesCommitted'};
+const logMetas = [{nodeName: 'c3pr-brain', moduleName: 'handleChangesCommitted'}];
 
 function handleChangesCommitted() {
-    c3prLOG2({msg: `Handling ChangesCommitted.`, logMetas: [logMeta]});
+    c3prLOG2({msg: `Handling ChangesCommitted.`, logMetas});
 
     c3prCEAMAP.collectEventAndMarkAsProcessing(
-        {eventType: `ChangesCommitted`, c3prHubUrl: config.c3pr.hub.c3prHubUrl, jwt: config.c3pr.jwt, logMetas: [logMeta]}
+        {eventType: `ChangesCommitted`, c3prHubUrl: config.c3pr.hub.c3prHubUrl, jwt: config.c3pr.jwt, logMetas}
     ).catch(() => {
-        c3prLOG2({msg: `Couldn't collect ChangesCommitted. Skipping.`, logMetas: [logMeta]});
+        c3prLOG2({msg: `Couldn't collect ChangesCommitted. Skipping.`, logMetas});
     }).then((changesCommitted) => {
         const parent = {
             event_type: `ChangesCommitted`,
             uuid: changesCommitted.uuid
         };
-        // noinspection JSIgnoredPromiseFromCall
         /** @namespace changesCommitted.changed_files */
-        c3prRTI.invokeTools({parent, repository: changesCommitted.repository, files: changesCommitted.changed_files});
+        c3prRTI.invokeTools({parent, repository: changesCommitted.repository, files: changesCommitted.changed_files})
+            .catch(e => {
+                c3prLOG2({
+                    msg: `Error while invoking tools. Reason: '${e}'. Data: ${e.response && e.response.data}.`,
+                    logMetas,
+                    meta: {args: {parent, repository: changesCommitted.repository, files: changesCommitted.changed_files}, error: require('util').inspect(e)}
+                });
+            })
     });
 }
 
