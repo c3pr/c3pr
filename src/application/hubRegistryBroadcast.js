063d05ad863c8f2d0209ee389eec0ebaa345e1e9
Antonio Jr <acdcjunior@gmail.com>
2018-05-20 16:00:53 -0300

    feat(hub): add initial broadcasting + improve logging

--- src/application/hubRegistryBroadcast.js
+++ src/application/hubRegistryBroadcast.js
@@ -4,24 +4,40 @@ const c3prLOG2 = require("node-c3pr-logger/c3prLOG2").c3pr.c3prLOG2;
 const config = require('../config');
 const loadToolsSummary = require('./loadToolsSummary');
 
+const logMeta = {nodeName: 'c3pr-agent', correlationIds: 'boot', moduleName: 'hubRegistryBroadcast'};
+
+function broadcast(summary) {
+    axios.patch(config.c3pr.hub.registryUrl,
+        {
+            key: `agent://${config.c3pr.agent.agentId}`,
+            value: summary,
+            timeout: config.c3pr.hub.broadcastTimeoutInMs
+        }
+    ).then(({data}) => {
+        c3prLOG2({
+            msg: `Successfully broadcasted to registry. URL: ${config.c3pr.hub.registryUrl}.`,
+            logMetas: [logMeta],
+            meta: {data, summary}
+        });
+    }).catch((e) => {
+        c3prLOG2({
+            msg: `Error while broadcasting to registry. URL: ${config.c3pr.hub.registryUrl}. Reason: '${e}'. Data: ${e.response && e.response.data}`,
+            logMetas: [logMeta],
+            meta: {error: require('util').inspect(e)}
+        });
+    });
+}
+
 function hubRegistryBroadcast() {
     const summary = loadToolsSummary();
-
     c3prLOG2({
         msg: `Now broadcasting to C-3PR registry API: ${config.c3pr.hub.registryUrl}.`,
-        logMetas: [{nodeName: 'c3pr-agent', correlationIds: 'boot', moduleName: 'hubRegistryBroadcast'}],
+        logMetas: [logMeta],
         meta: {summary}
     });
+    broadcast(summary);
     setInterval(() => {
-        axios.patch(config.c3pr.hub.registryUrl,
-            {
-                key: `agent://${config.c3pr.agent.agentId}`,
-                value: summary,
-                timeout: config.c3pr.hub.broadcastTimeoutInMs
-            }
-        ).catch((e) => {
-            console.log(`Error while broadcasting to registry. URL: ${config.c3pr.hub.registryUrl}. - ${e}`);
-        });
+        broadcast(summary);
     }, config.c3pr.hub.broadcastIntervalInMs);
 }
 
