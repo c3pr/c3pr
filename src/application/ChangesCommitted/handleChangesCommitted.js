bfca7eb8a92ca99ba26209d657f39aa8129d6f24
Antonio Jr <acdcjunior@gmail.com>
2018-05-20 21:06:14 -0300

    feat: handle events using helper functions

--- src/application/ChangesCommitted/handleChangesCommitted.js
+++ src/application/ChangesCommitted/handleChangesCommitted.js
@@ -1,5 +1,5 @@
 const c3prLOG2 = require("node-c3pr-logger/c3prLOG2").c3pr.c3prLOG2;
-const c3prCEAMAP = require('node-c3pr-hub-client/events/collectEventAndMarkAsProcessing').c3prCEAMAP;
+const handleFirstCollectedEvent = require('node-c3pr-hub-client/events/handleFirstCollectedEvent').handleFirstCollectedEvent.handleFirstCollectedEvent;
 
 const c3prRTI = require('../invokeTool/invokeTools').c3prBrain;
 
@@ -8,31 +8,30 @@ const config = require('../../config');
 const logMetas = [{nodeName: 'c3pr-brain', moduleName: 'handleChangesCommitted'}];
 
 function handleChangesCommitted() {
-    c3prLOG2({msg: `Handling ChangesCommitted.`, logMetas});
-
-    c3prCEAMAP.collectEventAndMarkAsProcessing(
-        {eventType: `ChangesCommitted`, c3prHubUrl: config.c3pr.hub.c3prHubUrl, jwt: config.c3pr.jwt, logMetas}
-    ).catch((e) => {
-        c3prLOG2({msg: `Couldn't collect ChangesCommitted. Skipping.`, logMetas, meta: {error: require('util').inspect(e)}});
-    }).then((changesCommitted) => {
-        if (!changesCommitted) {
-            c3prLOG2({msg: `No ChangesCommitted collected (possibly due to concurrent attempts to collect the same event). Skipping.`, logMetas, meta: {changesCommitted}});
-            return;
-        }
-        const parent = {
-            event_type: `ChangesCommitted`,
-            uuid: changesCommitted.uuid
-        };
-        /** @namespace changesCommitted.changed_files */
-        c3prRTI.invokeTools({parent, repository: changesCommitted.repository, files: changesCommitted.changed_files})
-            .catch(e => {
-                c3prLOG2({
-                    msg: `Error while invoking tools. Reason: '${e}'. Data: ${e.response && e.response.data}.`,
-                    logMetas,
-                    meta: {args: {parent, repository: changesCommitted.repository, files: changesCommitted.changed_files}, error: require('util').inspect(e)}
-                });
-            })
+    return handleFirstCollectedEvent({
+        event_type: `ChangesCommitted`,
+        handlerFunction,
+        c3prHubUrl: config.c3pr.hub.c3prHubUrl,
+        jwt: config.c3pr.auth.jwt,
+        logMetas
     });
+}
+
+async function handlerFunction(changesCommittedEvent) {
+    const parent = {
+        event_type: changesCommittedEvent.event_type,
+        uuid: changesCommittedEvent.uuid
+    };
+    try {
+        await c3prRTI.invokeTools({parent, repository: changesCommittedEvent.payload.repository, files: changesCommittedEvent.payload.changed_files});
+    } catch (e) {
+        c3prLOG2({
+            msg: `Error while invoking tools. Reason: '${e}'. Data: ${e.response && e.response.data}.`,
+            logMetas,
+            meta: {args: {parent, repository: changesCommittedEvent.repository, files: changesCommittedEvent.changed_files}, error: require('util').inspect(e)}
+        });
+        throw e;
+    }
 }
 
 module.exports = {
--- src/application/ToolInvocationCompleted/handleToolInvocationCompleted.js
+++ src/application/ToolInvocationCompleted/handleToolInvocationCompleted.js
@@ -1,29 +1,20 @@
-const c3prLOG2 = require("node-c3pr-logger/c3prLOG2").c3pr.c3prLOG2;
-const c3prCEAMAP = require('node-c3pr-hub-client/events/collectEventAndMarkAsProcessing').c3prCEAMAP;
+const handleFirstCollectedEvent = require('node-c3pr-hub-client/events/handleFirstCollectedEvent').handleFirstCollectedEvent.handleFirstCollectedEvent;
 
 const createAndEmitPullRequestRequested = require('../PullRequestRequested/createAndEmitPullRequestRequested');
 
 const config = require('../../config');
 
-const logMeta = {nodeName: 'c3pr-brain', moduleName: 'handleToolInvocationCompleted'};
+const logMetas = [{nodeName: 'c3pr-brain', moduleName: 'handleToolInvocationCompleted'}];
 
 function handleToolInvocationCompleted() {
-    c3prLOG2({msg: `Handling ToolInvocationCompleted.`, logMetas: [logMeta]});
-
-    c3prCEAMAP.collectEventAndMarkAsProcessing(
-        {eventType: `ToolInvocationCompleted`, c3prHubUrl: config.c3pr.hub.c3prHubUrl, jwt: config.c3pr.auth.jwt, logMetas: [logMeta]}
-    ).catch((e) => {
-        c3prLOG2({msg: `Couldn't collect ToolInvocationCompleted. Skipping.`, logMetas: [logMeta], meta: {error: require('util').inspect(e)}});
-    }).then((toolInvocationCompleted) => {
-        if (!toolInvocationCompleted) {
-            c3prLOG2({msg: `No ToolInvocationCompleted collected (possibly due to concurrent attempts to collect the same event). Skipping.`, logMetas, meta: {toolInvocationCompleted}});
-            return;
-        }
-        createAndEmitPullRequestRequested(toolInvocationCompleted);
+    return handleFirstCollectedEvent({
+        event_type: `ToolInvocationCompleted`,
+        handlerFunction: createAndEmitPullRequestRequested,
+        c3prHubUrl: config.c3pr.hub.c3prHubUrl,
+        jwt: config.c3pr.auth.jwt,
+        logMetas
     });
 }
-
-
 
 module.exports = {
     c3pr: {
