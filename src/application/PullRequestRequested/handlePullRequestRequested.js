b21bc2c7e5082fa8587d2c3c7e3304aa3427f22e
Antonio Jr <acdcjunior@gmail.com>
2018-05-20 21:06:14 -0300

    feat: handle events using helper functions

--- src/application/PullRequestRequested/handlePullRequestRequested.js
+++ src/application/PullRequestRequested/handlePullRequestRequested.js
@@ -1,56 +1,57 @@
 const c3prLOG2 = require("node-c3pr-logger/c3prLOG2").c3pr.c3prLOG2;
-const c3prCEAMAP = require('node-c3pr-hub-client/events/collectEventAndMarkAsProcessing').c3prCEAMAP;
+const handleFirstCollectedEvent = require('node-c3pr-hub-client/events/handleFirstCollectedEvent').handleFirstCollectedEvent.handleFirstCollectedEvent;
 
 const createGitLabMR = require('../pr/createGitLabMR');
 
 const config = require('../../config');
 
-const logMeta = {nodeName: 'c3pr-repo-gitlab', moduleName: 'handlePullRequestRequested'};
+const logMetas = [{nodeName: 'c3pr-repo-gitlab', moduleName: 'handlePullRequestRequested'}];
 
 function handlePullRequestRequested() {
-    c3prLOG2({msg: `Handling PullRequestRequested.`, logMetas: [logMeta]});
-
-    c3prCEAMAP.collectEventAndMarkAsProcessing(
-        {eventType: `PullRequestRequested`, c3prHubUrl: config.c3pr.hub.c3prHubUrl, jwt: config.c3pr.jwt, logMetas: [logMeta]}
-    ).catch((e) => {
-        c3prLOG2({msg: `Couldn't collect PullRequestRequested. Skipping.`, logMetas: [logMeta], meta: {error: require('util').inspect(e)}});
-    }).then(async (pullRequestRequested) => {
-        if (!pullRequestRequested) {
-            c3prLOG2({msg: `No PullRequestRequested collected (possibly due to concurrent attempts to collect the same event). Skipping.`, logMetas, meta: {pullRequestRequested}});
-            return;
-        }
-        const prr = pullRequestRequested.payload;
-        const repository = prr.repository;
-
-        c3prLOG2({
-            msg: `Handling MR request title '${prr.pr_title}' rev '${repository.revision}'`,
-            logMetas: [logMeta],
-            meta: {toolInvocationCompleted: pullRequestRequested}
-        });
-
-        /** @namespace prr.pr_body */
-        /** @namespace prr.diff_base64 */
-        /** @namespace prr.pr_title */
-        await createGitLabMR({
-            mainRepoOrgRepo: repository.fullpath,
-            mainRepoBranch: repository.branch,
-            mainRepoHash: repository.revision,
-            gitLabUrl: config.c3pr.repoGitlab.gitlab.url,
-            gitLabApiToken: config.c3pr.repoGitlab.gitlab.apiToken,
-            gitUserName: config.c3pr.repoGitlab.gitlab.botUserName,
-            gitUserEmail: config.c3pr.repoGitlab.gitlab.botUserEmail,
-            prCommitMessage: prr.pr_title,
-            prTitle: prr.pr_title,
-            prBody: prr.pr_body,
-            patchContent: prr.diff_base64
-        });
-
-        c3prLOG2({
-            msg: `MR created successfully.`,
-            logMetas: [logMeta],
-            meta: {toolInvocationCompleted: pullRequestRequested}
-        });
+    return handleFirstCollectedEvent({
+        event_type: `PullRequestRequested`,
+        handlerFunction,
+        c3prHubUrl: config.c3pr.hub.c3prHubUrl,
+        jwt: config.c3pr.jwt,
+        logMetas
     });
+}
+
+async function handlerFunction(pullRequestRequestedEvent) {
+
+    const prr = pullRequestRequestedEvent.payload;
+    const repository = prr.repository;
+
+    c3prLOG2({
+        msg: `Handling MR request title '${prr.pr_title}' rev '${repository.revision}'`,
+        logMetas,
+        meta: {pullRequestRequestedEvent}
+    });
+
+    /** @namespace repository.fullpath */
+    /** @namespace prr.pr_body */
+    /** @namespace prr.diff_base64 */
+    /** @namespace prr.pr_title */
+    await createGitLabMR({
+        mainRepoOrgRepo: repository.fullpath,
+        mainRepoBranch: repository.branch,
+        mainRepoHash: repository.revision,
+        gitLabUrl: config.c3pr.repoGitlab.gitlab.url,
+        gitLabApiToken: config.c3pr.repoGitlab.gitlab.apiToken,
+        gitUserName: config.c3pr.repoGitlab.gitlab.botUserName,
+        gitUserEmail: config.c3pr.repoGitlab.gitlab.botUserEmail,
+        prCommitMessage: prr.pr_title,
+        prTitle: prr.pr_title,
+        prBody: prr.pr_body,
+        patchContent: prr.diff_base64
+    });
+
+    c3prLOG2({
+        msg: `MR created successfully.`,
+        logMetas,
+        meta: {pullRequestRequestedEvent}
+    });
+
 }
 
 module.exports = handlePullRequestRequested;
