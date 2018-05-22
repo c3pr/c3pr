3cd9d8216e020ca2791c5cedb1116cf665534ead
Antonio acdc Jr <acdcjunior@gmail.com>
2018-05-22 08:17:29 -0300

    feat: add git clone script

---
+++ git-clone-all.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+cd ..
+git clone http://github.com/c3pr/c3pr-agent
+git clone http://github.com/c3pr/c3pr-brain
+git clone http://github.com/c3pr/c3pr-dashboard
+git clone http://github.com/c3pr/c3pr-hub
+git clone http://github.com/c3pr/c3pr-repo-github
+git clone http://github.com/c3pr/c3pr-repo-gitlab
+git clone http://github.com/c3pr/c3pr-tool-walkmod-sonar
+git clone http://github.com/c3pr/node-c3pr-hub-client
+git clone http://github.com/c3pr/node-c3pr-logger
+git clone http://github.com/c3pr/node-c3pr-repo
+git clone http://github.com/c3pr/node-git-client
