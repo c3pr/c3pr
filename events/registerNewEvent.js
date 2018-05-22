4666bb50360eecc283fd7af93ec14d146da61da1
Antonio Jr <acdcjunior@gmail.com>
2018-05-22 11:46:10 -0300

    feat(registerNewEvent): change arg from eventType to event_type and add logging

--- events/registerNewEvent.js
+++ events/registerNewEvent.js
@@ -4,21 +4,26 @@ const c3prLOG2 = require("node-c3pr-logger/c3prLOG2").c3pr.c3prLOG2;
 
 const logMeta = {nodeName: 'node-c3pr-hub-client', moduleName: 'registerNewEvent'};
 
-
-
-async function registerNewEvent({eventType, payload, c3prHubUrl, jwt, logMetas: outerLogMetas, retryWait = 2000}) {
+async function registerNewEvent({event_type, payload, c3prHubUrl, jwt, logMetas: outerLogMetas, retryWait = 2000}) {
+    const logMetas = [...(outerLogMetas || []), logMeta];
     try {
+
+        c3prLOG2({
+            msg: `Registering new event of type '${event_type}'.`,
+            logMetas,
+            meta: {event_type, payload}
+        });
         const client = axios.create({ baseURL: c3prHubUrl });
+
         // noinspection JSUnusedGlobalSymbols
         axiosRetry(client, { retries: 3, retryDelay: retryCount => retryCount * retryWait, retryCondition() { return true; } });
 
         const headers = {Authorization: `Bearer ${jwt}`};
-
-        await client.post(`/api/v1/events/${eventType}`, payload, {headers});
+        await client.post(`/api/v1/events/${event_type}`, payload, {headers});
     } catch (e) {
         c3prLOG2({
-            msg: `Error while registering new event of type ${eventType}. Reason: '${e}'. Data: ${e.response && e.response.data}`,
-            logMetas: [...(outerLogMetas || []), logMeta],
+            msg: `Error while registering new event of type '${event_type}'. Reason: '${e}'. Data: ${e.response && e.response.data}.`,
+            logMetas,
             meta: {payload, error: require('util').inspect(e)}
         });
         throw e;
--- events/registerNewEvent.test.js
+++ events/registerNewEvent.test.js
@@ -11,19 +11,19 @@ describe('registerNewEvent', () => {
 
     it('registerNewEvent automatically retries 3 times', async () => {
 
-        const eventType = "someEvent";
+        const event_type = "someEvent";
         const c3prHubUrl = 'http://localhost:5004';
         const jwt = "jwt";
         const payload = {a: 123};
 
         axiosMock
-            .onPost(`${c3prHubUrl}/api/v1/events/${eventType}`, payload).replyOnce(500)
-            .onPost(`${c3prHubUrl}/api/v1/events/${eventType}`, payload).replyOnce(500)
-            .onPost(`${c3prHubUrl}/api/v1/events/${eventType}`, payload).replyOnce(500)
-            .onPost(`${c3prHubUrl}/api/v1/events/${eventType}`, payload).replyOnce(200);
+            .onPost(`${c3prHubUrl}/api/v1/events/${event_type}`, payload).replyOnce(500)
+            .onPost(`${c3prHubUrl}/api/v1/events/${event_type}`, payload).replyOnce(500)
+            .onPost(`${c3prHubUrl}/api/v1/events/${event_type}`, payload).replyOnce(500)
+            .onPost(`${c3prHubUrl}/api/v1/events/${event_type}`, payload).replyOnce(200);
 
         await registerNewEvent({
-            eventType,
+            event_type,
             c3prHubUrl,
             jwt,
             payload,
