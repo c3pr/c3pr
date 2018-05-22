b5e67dd1f76f8161c7a1cbc273a7b08892a80e12
Antonio Jr <acdcjunior@gmail.com>
2018-05-22 13:41:19 -0300

    feat: add logmeta

--- src/web/c3prHubListenerController.js
+++ src/web/c3prHubListenerController.js
@@ -3,10 +3,12 @@ const c3prLOG2 = require("node-c3pr-logger/c3prLOG2").c3pr.c3prLOG2;
 
 const handlePullRequestRequested = require('../application/PullRequestRequested/handlePullRequestRequested');
 
+const logMetas = [{nodeName: 'c3pr-repo-gitlab', moduleName: 'c3prHubListenerController'}];
+
 module.exports = function (app) {
 
     app.post(config.c3pr.repoGitlab.PullRequestRequestedCallbackUrl, function (request, response) {
-        c3prLOG2({msg: `'PullRequestRequested' received.`});
+        c3prLOG2({msg: `'PullRequestRequested' received.`, logMetas});
         handlePullRequestRequested();
         response.send();
     });
