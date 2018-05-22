0e3a34b24a36aa40a306e294b34d61b7f496daf8
Antonio Jr <acdcjunior@gmail.com>
2018-05-22 14:11:56 -0300

    fix: remove some old properties

--- src/domain/webhook/convertWebhookToChanges.js
+++ src/domain/webhook/convertWebhookToChanges.js
@@ -22,19 +22,11 @@ function convertWebhookToChanges(webhookPayload) {
     const changeset = extractChangedFiles(webhookPayload.commits);
     const gitSHA = webhookPayload.after;
     return {
-        meta: {
-            correlationId: gitSHA,
-            compatibleSchemas: ["c3pr/c3pr::changes"],
-            dates: [{node: 'c3pr-repo-github', date: new Date().toISOString()}]
-        },
-        c3pr: {
-            prsUrl: config.c3pr.prsUrl
-        },
         changeset,
         repository: {
-            type: "git",
-            fullpath: webhookPayload.repository.full_name,
-            url: webhookPayload.repository.clone_url,
+            full_path: webhookPayload.repository.full_name,
+            get author() { throw new Error() },
+            clone_url_http: webhookPayload.repository.clone_url,
             // TODO maybe it would be more secure to send down the refs and git fetch the refs instead of the branch... This seems rather sketchy
             branch: webhookPayload.ref.replace(/refs\/heads\//, ''),
             revision: gitSHA
