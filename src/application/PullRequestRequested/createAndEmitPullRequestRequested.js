a7d3b47b770ba9c7df445fd37c3ef801ea63fa95
Antonio Jr <acdcjunior@gmail.com>
2018-05-22 22:05:38 -0300

    feat(ToolInvocationCompleted): add new tool invocations after tool completions with unmodified files

--- src/application/PullRequestRequested/createAndEmitPullRequestRequested.js
+++ src/application/PullRequestRequested/createAndEmitPullRequestRequested.js
@@ -1,7 +1,34 @@
+const c3prLOG2 = require("node-c3pr-logger/c3prLOG2").c3pr.c3prLOG2;
 const createPullRequestRequested = require('../PullRequestRequested/createPullRequestRequested').c3pr.createPullRequestRequested;
 const emitPullRequestRequested = require('../PullRequestRequested/emitPullRequestRequested');
 
+const c3prRTI = require('../invokeTool/invokeTools').c3prBrain;
+
+const logMetaz = (correlationId) => [{nodeName: 'c3pr-brain', correlationId, moduleName: 'createAndEmitPullRequestRequested'}];
+
+function invokeToolsForRemainingFiles(toolInvocationCompleted) {
+    c3prRTI.invokeTools({
+        parent: {
+            event_type: toolInvocationCompleted.event_type,
+            uuid: toolInvocationCompleted.uuid
+        },
+        changes_committed_root: toolInvocationCompleted.changes_committed_root,
+        repository: toolInvocationCompleted.repository,
+        files: toolInvocationCompleted.unmodified_files
+    }).catch(e => {
+        c3prLOG2({
+            msg: `Error while invoking tools. Reason: '${e}'. Data: ${e.response && e.response.data}.`,
+            logMetas: logMetaz(toolInvocationCompleted.repository.revision),
+            meta: {toolInvocationCompleted, error: require('util').inspect(e)}
+        });
+    });
+}
+
 function createAndEmitPullRequestRequested(toolInvocationCompleted, logMetas) {
+    if (toolInvocationCompleted.unmodified_files.length) {
+        invokeToolsForRemainingFiles(toolInvocationCompleted);
+    }
+
     const pullRequestRequested = createPullRequestRequested(toolInvocationCompleted);
     emitPullRequestRequested(pullRequestRequested, logMetas);
 }
