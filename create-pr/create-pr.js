c1749af77cc62548442c218ca2a5b39823d05307
Antonio Jr <acdcjunior@gmail.com>
2018-05-23 11:36:57 -0300

    fix(create-pr): patchContent is a string

--- create-pr/create-pr.js
+++ create-pr/create-pr.js
@@ -51,7 +51,7 @@ async function createPR({
     // The base64 used here assume a specific format, a patch file generated (somewhere else) as:
     // git diff > 'myPatch.patch'
     // const patchContent = Buffer.from(fs.readFileSync('myPatch.patch', 'hex'), 'hex').toString('base64');
-    fs.writeFileSync(patchFilePath, Buffer.from(patchContent.diff, 'base64').toString('hex'), 'hex');
+    fs.writeFileSync(patchFilePath, Buffer.from(patchContent, 'base64').toString('hex'), 'hex');
 
     await c3prSH(`git apply ${patchFileName}`, {cwd: stagingFolder}, {logMeta});
     fs.unlinkSync(patchFilePath);
