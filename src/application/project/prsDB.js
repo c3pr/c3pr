cb60e67797a185a57f3f8830b428932c2a85ea7e
Antonio Jr <acdcjunior@gmail.com>
2018-05-28 21:05:29 -0300

    feat(prs): add findFilesWithOpenPR

--- src/application/project/prsDB.js
+++ src/application/project/prsDB.js
@@ -1,13 +1,13 @@
 const client = require('./db');
 
-const prs = client.then(cli => cli.db(config.c3pr.brain.c3prBrainMongoDatabase).collection(config.c3pr.brain.c3prBrainMongoCollectionPRs));
+const prsDB = client.then(cli => cli.db(config.c3pr.brain.c3prBrainMongoDatabase).collection(config.c3pr.brain.c3prBrainMongoCollectionPRs));
 
 async function insert(data) {
-    return (await prs).insertOne(data);
+    return (await prsDB).insertOne(data);
 }
 
 async function findBy(query) {
-    return (await prs).find(query).toArray();
+    return (await prsDB).find(query).toArray();
 }
 
 const PR_STATUS = {
@@ -17,28 +17,34 @@ const PR_STATUS = {
 };
 
 // before creating a TIR, brain must fetch all PRs with OPEN status of that project and remove the changed_files of these PRs from the eligible files
-async function newPR({project, url, ToolInvocationRequested, changed_files}) {
+async function newPR({projectUUID, url, ToolInvocationCompleted, changed_files}) {
     return insert(    {
-        project,
+        projectUUID,
         url,
         status: PR_STATUS.OPEN,
-        ToolInvocationRequested,
+        ToolInvocationCompleted,
         changed_files,
         comments_count: {}
     });
 }
 
+async function findFilesWithOpenPR(projectUUID) {
+    const openPRs = await findBy({projectUUID, status: PR_STATUS.OPEN});
+    return openPRs.reduce((changedFiles, openPR) => changedFiles.push(...openPR.changed_files), [])
+}
+
 module.exports = {
-    newPR
+    newPR,
+    findFilesWithOpenPR
 };
 
 // noinspection JSUnusedLocalSymbols
 const examplePR = [
     {
-        project: "project-uuid-1234-1234",
+        projectUUID: "project-uuid-1234-1234",
         url: "https://github.com/c3pr/sample-project-java-maven/pull/23",
         status: "open", //["open", "merged", "closed"]
-        ToolInvocationRequested: "tir-uuid-123-123",
+        ToolInvocationCompleted: "tir-uuid-123-123",
         changed_files: ["..."],
         comments_count: {
             'userone': 12,
