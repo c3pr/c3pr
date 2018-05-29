f27f2b5a355baa2595625c3763c0c264ed5ed6bd
Antonio acdc Jr <acdcjunior@gmail.com>
2018-05-28 21:48:24 -0300

    feat(prs): add prs

---
+++ src/application/project/prsDB.js
@@ -0,0 +1,71 @@
+const client = require('./db');
+const config = require('../../config');
+
+const prsDB = client.then(cli => cli.db(config.c3pr.hub.mongoC3prDatabase).collection(config.c3pr.hub.mongoPRsCollection));
+
+async function insert(data) {
+    return (await prsDB).insertOne(data);
+}
+
+async function findBy(query) {
+    return (await prsDB).find(query).toArray();
+}
+
+async function findAllOfProject(projectUUID) {
+    return (await prsDB).find({projectUUID}).toArray();
+}
+
+const PR_STATUS = {
+    OPEN: 'OPEN',
+    MERGED: 'MERGED',
+    CLOSED: 'CLOSED'
+};
+
+// before creating a TIR, brain must fetch all PRs with OPEN status of that project and remove the changed_files of these PRs from the eligible files
+async function newPR({projectUUID, url, ToolInvocationCompleted, changed_files}) {
+    return insert(    {
+        projectUUID,
+        url,
+        status: PR_STATUS.OPEN,
+        ToolInvocationCompleted,
+        changed_files,
+        comments_count: {}
+    });
+}
+
+async function findFilesWithOpenPR(projectUUID) {
+    const openPRs = await findBy({projectUUID, status: PR_STATUS.OPEN});
+    return openPRs.reduce((changedFiles, openPR) => [...changedFiles, ...openPR.changed_files], [])
+}
+
+module.exports = {
+    newPR,
+    findAllOfProject,
+    findFilesWithOpenPR
+};
+
+
+// noinspection BadExpressionStatementJS
+(() => {
+    // noinspection JSUnusedLocalSymbols
+    const examplePR = [
+        {
+            projectUUID: "project-uuid-1234-1234",
+            url: "https://github.com/c3pr/sample-project-java-maven/pull/23",
+            status: 'OPEN', //['OPEN', 'MERGED', 'CLOSED']
+            ToolInvocationCompleted: "tir-uuid-123-123",
+            changed_files: ["..."],
+            comments_count: {
+                'userone': 12,
+                'usertwo': 3
+            }
+        }
+    ];
+
+    // noinspection JSIgnoredPromiseFromCall
+    newPR({projectUUID: "7ac33816-c794-4cf3-a5b4-5f7e2ff06543", url: "http://example.com/starwars/c-3po-project/pull/1", ToolInvocationCompleted: "...", changed_files: ["a1.txt"]});
+    // noinspection JSIgnoredPromiseFromCall
+    newPR({projectUUID: "7ac33816-c794-4cf3-a5b4-5f7e2ff06543", url: "http://example.com/starwars/c-3po-project/pull/2", ToolInvocationCompleted: "...", changed_files: ["a2.txt"]}); //later status:merged
+    // noinspection JSIgnoredPromiseFromCall
+    newPR({projectUUID: "7ac33816-c794-4cf3-a5b4-5f7e2ff06543", url: "http://example.com/starwars/c-3po-project/pull/3", ToolInvocationCompleted: "...", changed_files: ["a3.txt"]}); //later status:closed
+});
--- src/web/projectsController.js
+++ src/web/projectsController.js
@@ -1,14 +1,33 @@
-const projectsDB = require('../application/project/projectsDB');
 const authExpressMiddleware = require("../application/auth/authExpressMiddleware");
 
+const projectsDB = require('../application/project/projectsDB');
+const prsDB = require('../application/project/prsDB');
 
 module.exports = function (app) {
 
     app.use('/api/v1/projects', authExpressMiddleware);
 
-    app.get('/api/v1/projects/', function ({params: {eventType, uuid}}, response) {
+    app.get('/api/v1/projects/', function (req, response) {
         projectsDB.findAll().then((projects) => {
             response.status(200).send(projects);
+        }).catch((e) => {
+            console.error(e);
+            response.status(500).send(e);
+        });
+    });
+
+    app.get('/api/v1/projects/:projectUUID/prs', function ({params: {projectUUID}}, response) {
+        prsDB.findAllOfProject(projectUUID).then((prs) => {
+            response.status(200).send(prs);
+        }).catch((e) => {
+            console.error(e);
+            response.status(500).send(e);
+        });
+    });
+
+    app.get('/api/v1/projects/:projectUUID/prs/open/changed_files', function ({params: {projectUUID}}, response) {
+        prsDB.findFilesWithOpenPR(projectUUID).then((prs) => {
+            response.status(200).send(prs);
         }).catch((e) => {
             console.error(e);
             response.status(500).send(e);
