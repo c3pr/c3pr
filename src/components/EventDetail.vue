b5818c2c7fb2f921a156ae97a584463425cb01f8
Antonio acdc Jr <acdcjunior@gmail.com>
2018-05-30 23:14:34 -0300

    feat(hub): improve function application

--- src/components/EventDetail.vue
+++ src/components/EventDetail.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <table class="event-table" v-for="contractType of Object.keys($data)" v-if="event_type === contractType">
-      <tbody><tr v-for="prop of $data[contractType]"><td class="grayed">{{ Array.isArray(prop) ? prop[0] : prop }}</td><td>{{ display(prop, payload) }}</td></tr></tbody>
+      <tbody><tr v-for="prop of $data[contractType]"><td class="grayed">{{ typeof prop === 'object' ? Object.values(prop)[0] : prop }}</td><td>{{ display(prop, payload) }}</td></tr></tbody>
     </table>
   </div>
 </template>
@@ -12,22 +12,24 @@ export default {
   props: ['event_type', 'payload'],
   data() {
       return {
-          ChangesCommitted: ['changed_files', 'repository.clone_url_http'],
-          ToolInvocationRequested: ['parent.event_type', ['parent.uuid', 'id'], ['changes_committed_root', 'id'], 'tool_id', 'files', 'repository.clone_url_http'],
-          ToolInvocationCompleted: ['parent.event_type', ['parent.uuid', 'id'], ['changes_committed_root', 'id'], 'changed_files', 'unmodified_files', 'pr_title', 'diff_base64.length', 'repository.clone_url_http'],
-          PullRequestRequested: ['parent.event_type', ['parent.uuid', 'id'], 'assignee', 'pr_title', 'repository.clone_url_http'],
+          ChangesCommitted: [{id: 'project_uuid'}, 'changed_files', {date: 'date'}, 'repository.clone_url_http'],
+          ToolInvocationRequested: ['parent.event_type', {id: 'parent.uuid'}, {id: 'changes_committed_root'}, 'tool_id', 'files', 'repository.clone_url_http'],
+          ToolInvocationCompleted: ['parent.event_type', {id: 'parent.uuid'}, {id: 'changes_committed_root'}, 'changed_files', 'unmodified_files', 'pr_title', 'diff_base64.length', 'repository.clone_url_http'],
+          PullRequestRequested: ['parent.event_type', {id: 'parent.uuid'}, 'assignee', 'pr_title', 'repository.clone_url_http'],
           PullRequestUpdated: [],
 
           functions: {
-            id(uuid) { return uuid.split("-")[0]; }
+            id(uuid) { return uuid.split("-")[0]; },
+            date(d) { return (d || "").replace("T", " "); }
           }
       }
   },
   methods: {
     display(path, obj) {
-      if (Array.isArray(path)) {
-        let value = this.resolve(path[0], obj);
-        return this.functions[path[1]](value);
+      if (typeof path === 'object') {
+        let [[functionToBeApplied, propertyPath]] = Object.entries(path);
+        let value = this.resolve(propertyPath, obj);
+        return this.functions[functionToBeApplied](value);
       }
       return this.resolve(path, obj);
     },
