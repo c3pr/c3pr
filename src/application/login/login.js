598b4717c71745afa98d19e94b8c68de2026911c
Antonio acdc Jr <acdcjunior@gmail.com>
2018-05-31 16:12:23 -0300

    feat(login): add username and password

--- src/application/login/login.js
+++ src/application/login/login.js
@@ -4,6 +4,8 @@ const c3prHubClient = require('node-c3pr-hub-client/login').c3prHubClient;
 
 c3prHubClient.login({
     loginUrl: config.c3pr.hub.loginUrl,
+    username: 'c3pr-dashboard',
+    password: 'not-used',
     subscriptions: [],
     logMetas: [{nodeName: 'c3pr-dashboard', moduleName: 'login'}]
 }).then(jwt => {
