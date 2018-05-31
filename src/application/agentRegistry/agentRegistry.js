6203da2c652e45140b1efcdb3ea9062db3737f1a
Antonio acdc Jr <acdcjunior@gmail.com>
2018-05-31 15:31:13 -0300

    feat(agentRegistry): allow tool_ids for multiple tool registration at once

--- src/application/agentRegistry/agentRegistry.js
+++ src/application/agentRegistry/agentRegistry.js
@@ -1,18 +1,27 @@
 const agentRegistryDB = require('./agentRegistryDB');
 const config = require('../../config');
 
-function putAgentsOrAgent(auth, agentOrAgents) {
-    let entries = Array.isArray(agentOrAgents) ? agentOrAgents : [agentOrAgents];
+/**
+ * Agent may have a single `tool_id` or a `tool_ids` - an array of `tool_id`s.
+ */
+async function putAgent(auth, agent) {
+    if (!agent || (!agent.tool_id && !agent.tool_ids) || !agent.expiration_time) {
+        throw new Error("Agent must have a (tool_id or tool_ids) and expiration_time. Received: " + JSON.stringify(agent));
+    }
+    if (agent.tool_ids) {
+        const agents = agent.tool_ids.map(tid => ({...agent, tool_id: tid}));
 
-    return Promise.all(entries.map(agent => putAgent(auth, agent)));
+        return Promise.all(agents.map(a => putAgentToolId(auth, a)));
+    }
+    return putAgentToolId(auth, agent);
 }
 
-function putAgent(auth, agent) {
-    if (!agent || !agent.tool_id || !agent.expiration_time) {
-        throw new Error("Agent must have a tool_id and expiration_time. Received: " + JSON.stringify(agent));
-    }
-    const agentProps = {tool_id: agent.tool_id, agent_id: auth.sub};
-    return agentRegistryDB.replaceOne(agentProps, {...agentProps, expiration_time: new Date(agent.expiration_time)}, {upsert: true});
+function putAgentToolId({sub: agent_id}, {tool_id, expiration_time}) {
+    return agentRegistryDB.replaceOne(
+        {tool_id, agent_id},
+        {tool_id, agent_id, expiration_time: new Date(expiration_time)},
+        {upsert: true}
+    );
 }
 
 function removeExpiredAgents() {
@@ -24,6 +33,6 @@ removeExpiredAgents(); // initial run
 setInterval(removeExpiredAgents, config.c3pr.hub.agentRegistry.cleanRegistryStepInMs).unref();
 
 module.exports = {
-    putAgentsOrAgent,
+    putAgent,
     findAll: agentRegistryDB.findAll
 };
--- src/web/agentRegistryController.js
+++ src/web/agentRegistryController.js
@@ -6,17 +6,15 @@ module.exports = function (app) {
     app.get('/api/v1/agents', function (request, response) {
         agentRegistry.findAll().then(agents => {
             response.send(agents);
-        })
+        });
     });
 
     app.patch('/api/v1/agents', function (request, response) {
-        try {
-            const agentOrArrayOfAgents = request.body;
-            agentRegistry.putAgentsOrAgent({sub: 'anyone'}, agentOrArrayOfAgents);
+        agentRegistry.putAgent({sub: 'anyone'}, request.body).then(() => {
             response.status(200).send('Agent successfully registered!');
-        } catch (e) {
-            response.status(400).send(e);
-        }
+        }).catch((e) => {
+            response.status(400).send(e.toString());
+        });
     });
 
 };
