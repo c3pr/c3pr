a18f0dc4dd850ec121fd270cff74bb10e5ff5e45
Antonio Jr <acdcjunior@gmail.com>
2018-06-01 12:56:41 -0300

    feat(log): add error arg

--- c3prLOG2/index.d.ts
+++ c3prLOG2/index.d.ts
@@ -1,12 +1,14 @@
 export declare const c3pr: {
-    c3prLOG2({ msg, logMetas, meta }: {
+    c3prLOG2({ msg, logMetas, meta, error: e }: {
         msg: any;
         logMetas: any;
         meta: any;
+        error: any;
     }): void;
 };
-export declare const c3prLOG2: ({ msg, logMetas, meta }: {
+export declare const c3prLOG2: ({ msg, logMetas, meta, error: e }: {
     msg: any;
     logMetas: any;
     meta: any;
+    error: any;
 }) => void;
--- c3prLOG2/index.js
+++ c3prLOG2/index.js
@@ -1,16 +1,23 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const util = require('util');
 const c3prLOG_original = require('../src/c3prLOG');
 exports.c3pr = {
-    c3prLOG2({ msg, logMetas, meta }) {
+    c3prLOG2({ msg, logMetas, meta, error: e }) {
         if (arguments.length !== 1) {
             throw new Error(`c3prLOG2() called with different number or arguments. Wanted: 1. Passed: ${arguments.length} - ${JSON.stringify(arguments)}`);
         }
-        const extraKeys = Object.keys(arguments[0]).filter(key => !["msg", "logMetas", "meta"].includes(key));
+        const extraKeys = Object.keys(arguments[0]).filter(key => !["msg", "logMetas", "meta", "error"].includes(key));
         if (extraKeys.length) {
             throw new Error(`c3prLOG2() argument must be of format {msg, logMetas, meta}. Additional keys passed: ${JSON.stringify(extraKeys)}. Full arg: ${JSON.stringify(arguments[0])}`);
         }
-        c3prLOG_original(msg, meta || {}, ...(logMetas || []));
+        let msgMsg = msg || '';
+        let metaMeta = meta || {};
+        if (e) {
+            msgMsg += `Error reason: '${e}'. Data: ${e.response && JSON.stringify(e.response.data) || 'no data'}.`;
+            metaMeta.error = util.inspect(e);
+        }
+        c3prLOG_original(msgMsg, metaMeta, ...(logMetas || []));
     }
 };
 exports.c3prLOG2 = exports.c3pr.c3prLOG2;
