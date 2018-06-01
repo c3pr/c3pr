623663f9ab957cecebac3602bb3dbb194b33ca64
Antonio Jr <acdcjunior@gmail.com>
2018-06-01 13:21:56 -0300

    feat(events): handlerFunction now should return an object of format {new_status, result}

--- events/handleFirstCollectedEvent.js
+++ events/handleFirstCollectedEvent.js
@@ -1,6 +1,7 @@
 const c3prLOG2 = require("node-c3pr-logger/c3prLOG2").c3pr.c3prLOG2;
-const collectEventAndMarkAsProcessing = require('node-c3pr-hub-client/events/collectEventAndMarkAsProcessing').collectEventAndMarkAsProcessing.collectEventAndMarkAsProcessing;
-const markAsProcessed = require('node-c3pr-hub-client/events/markAsProcessed').markAsProcessed.markAsProcessed;
+const collectEventAndMarkAsProcessing = require('./collectEventAndMarkAsProcessing').collectEventAndMarkAsProcessing.collectEventAndMarkAsProcessing;
+const markAsProcessed = require('./markAs').markAs.markAsProcessed;
+const markAsUnprocessed = require('./markAs').markAs.markAsUnprocessed;
 
 const logMeta = {nodeName: 'node-c3pr-hub-client', moduleName: 'handleFirstCollectedEvent'};
 
@@ -12,15 +13,12 @@ async function handleFirstCollectedEvent({event_type, handlerFunction, c3prHubUr
     let event;
     try {
         event = await collectEventAndMarkAsProcessing({event_type, c3prHubUrl, jwt, logMetas})
-    } catch (e) {
-        c3prLOG2({
-            msg: `Couldn't collect ${event_type}. Skipping. Error reason: '${e}'. Data: ${e.response && JSON.stringify(e.response.data) || 'no data'}.`,
-            logMetas,
-            meta: {error: require('util').inspect(e)}
-        });
+    } catch (error) {
+        c3prLOG2({msg: `Couldn't collect ${event_type}. Skipping.`, logMetas, error});
         return;
     }
 
+    let handlerFunctionResult;
     try {
         if (!event) {
             c3prLOG2({
@@ -31,29 +29,46 @@ async function handleFirstCollectedEvent({event_type, handlerFunction, c3prHubUr
             return;
         }
 
-        await handlerFunction(event);
+        handlerFunctionResult = await handlerFunction(event);
 
-    } catch (e) {
-        c3prLOG2({
-            msg: `Error while executing handlerFunction() for event handling. Reason: '${e}'. Data: ${e.response && JSON.stringify(e.response.data) || 'no data'}.`,
-            logMetas,
-            meta: {event, error: require('util').inspect(e)}
-        });
-        // TODO UNCOLLECT HERE! If not uncollected, the PROCESSING status will timeout anyway, though.
+    } catch (error) {
+        c3prLOG2({msg: `Error while executing handlerFunction() for event handling.`, logMetas, error});
+
+        try {
+            await markAsUnprocessed({event_type, uuid: event.uuid, c3prHubUrl, jwt, logMetas});
+        } catch (error) {
+            c3prLOG2({msg: `Couldn't mark ${event_type}/${event.uuid} as UNPROCESSED. You must do it **MANUALLY**. If you don't, you'll have to wait until the PROCESSING `+
+                    `status times out.`, logMetas, error});
+        }
         return;
     }
 
-    try {
-        await markAsProcessed({event_type, uuid: event.uuid, c3prHubUrl, jwt, logMetas});
-    } catch (e) {
-        c3prLOG2({
-            msg: `Couldn't mark ${event_type}/${event.uuid} as COMPLETED. You must do it **MANUALLY**. If you don't, the PROCESSING status will `+
-                `timeout and the event will be reprocessed, possibly generating duplicated effects. Error reason: '${e}'. Data: ${e.response && JSON.stringify(e.response.data) || 'no data'}.`,
-            logMetas,
-            meta: {event, error: require('util').inspect(e)}
-        });
+    if (!handlerFunctionResult || !handlerFunctionResult.new_status) {
+        throw new Error('Handler function should return an object of format {new_status, result}, being new_status mandatory. Received: ' + JSON.stringify(handlerFunctionResult));
     }
 
+    switch (handlerFunctionResult.new_status.toUpperCase()) {
+        case 'PROCESSED':
+            try {
+                await markAsProcessed({event_type, uuid: event.uuid, c3prHubUrl, jwt, logMetas});
+            } catch (error) {
+                c3prLOG2({msg: `Couldn't mark ${event_type}/${event.uuid} as PROCESSED. You must do it **MANUALLY**. If you don't, the PROCESSING status will `+
+                        `timeout and the event will be reprocessed, possibly generating duplicated effects.`, logMetas, error});
+            }
+            break;
+        case 'UNPROCESSED':
+            try {
+                await markAsUnprocessed({event_type, uuid: event.uuid, c3prHubUrl, jwt, logMetas});
+            } catch (error) {
+                c3prLOG2({msg: `Couldn't mark ${event_type}/${event.uuid} as UNPROCESSED. You must do it **MANUALLY**. If you don't, you'll have to wait until the PROCESSING `+
+                        `status times out.`, logMetas, error});
+            }
+            break;
+        default:
+            throw new Error('Handler function returned a new_status of unsupported value. handlerFunctionResult received: ' + JSON.stringify(handlerFunctionResult));
+    }
+
+    return handlerFunctionResult.result;
 }
 
 module.exports = {
