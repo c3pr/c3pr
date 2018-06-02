203acfdd415165fbac0f705b721d807ba03ea389
Antonio acdc Jr <acdcjunior@gmail.com>
2018-06-02 15:22:31 -0300

    feat(prs): update sample

--- src/application/project/prsDB.js
+++ src/application/project/prsDB.js
@@ -22,12 +22,12 @@ const PR_STATUS = {
 };
 
 // before creating a TIR, brain must fetch all PRs with OPEN status of that project and remove the changed_files of these PRs from the eligible files
-async function newPR({projectUUID, url, ToolInvocationCompleted, changed_files}) {
+async function newPR({projectUUID, url, PullRequestRequested, changed_files}) {
     return insert(    {
         projectUUID,
         url,
         status: PR_STATUS.OPEN,
-        ToolInvocationCompleted,
+        PullRequestRequested,
         changed_files,
         comments_count: {}
     });
@@ -47,13 +47,27 @@ module.exports = {
 
 // noinspection BadExpressionStatementJS
 (() => {
+    const examplePRCreated = {
+        // project_uuid
+        parent: {
+            event_type: "PullRequestRequested",
+            uuid: pullRequestRequestedEvent.uuid
+        },
+        repository: pullRequestRequestedEvent.payload.repository,
+        pr_id: 123,
+        pr_url: createMrResult.web_url, // "https://github.com/c3pr/sample-project-java-maven/pull/23",
+        // changed_files,
+        //status: 'OPEN',
+        //PullRequestRequested: ,
+        //comments_count: {}
+    };
     // noinspection JSUnusedLocalSymbols
     const examplePR = [
         {
             projectUUID: "project-uuid-1234-1234",
             url: "https://github.com/c3pr/sample-project-java-maven/pull/23",
             status: 'OPEN', //['OPEN', 'MERGED', 'CLOSED']
-            ToolInvocationCompleted: "tir-uuid-123-123",
+            PullRequestRequested: "tir-uuid-123-123",
             changed_files: ["..."],
             comments_count: {
                 'userone': 12,
@@ -63,9 +77,9 @@ module.exports = {
     ];
 
     // noinspection JSIgnoredPromiseFromCall
-    newPR({projectUUID: "7ac33816-c794-4cf3-a5b4-5f7e2ff06543", url: "http://example.com/starwars/c-3po-project/pull/1", ToolInvocationCompleted: "...", changed_files: ["a1.txt"]});
+    newPR({projectUUID: "7ac33816-c794-4cf3-a5b4-5f7e2ff06543", url: "http://example.com/starwars/c-3po-project/pull/1", PullRequestRequested: "...", changed_files: ["a1.txt"]});
     // noinspection JSIgnoredPromiseFromCall
-    newPR({projectUUID: "7ac33816-c794-4cf3-a5b4-5f7e2ff06543", url: "http://example.com/starwars/c-3po-project/pull/2", ToolInvocationCompleted: "...", changed_files: ["a2.txt"]}); //later status:merged
+    newPR({projectUUID: "7ac33816-c794-4cf3-a5b4-5f7e2ff06543", url: "http://example.com/starwars/c-3po-project/pull/2", PullRequestRequested: "...", changed_files: ["a2.txt"]}); //later status:merged
     // noinspection JSIgnoredPromiseFromCall
-    newPR({projectUUID: "7ac33816-c794-4cf3-a5b4-5f7e2ff06543", url: "http://example.com/starwars/c-3po-project/pull/3", ToolInvocationCompleted: "...", changed_files: ["a3.txt"]}); //later status:closed
+    newPR({projectUUID: "7ac33816-c794-4cf3-a5b4-5f7e2ff06543", url: "http://example.com/starwars/c-3po-project/pull/3", PullRequestRequested: "...", changed_files: ["a3.txt"]}); //later status:closed
 });
