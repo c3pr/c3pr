33ad379064530f305d06447834972f32ac5942fe
Antonio Jr <acdcjunior@gmail.com>
2018-06-02 19:51:14 -0300

    feat(hubClient): add hubClientConfig to index

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,3 +1,16 @@
-import { HubClient } from "./types/HubClient";
+import { HubClient } from './types/HubClient';
 declare const index: HubClient;
 export default index;
+export declare const hubClientConfig: {
+    c3pr: {
+        hub: {
+            auth: {
+                jwt: string;
+            };
+            c3prHubUrl: string;
+            loginUrl: string;
+            projectsByCloneUrlHttp: (ignore: any) => string;
+        };
+    };
+    init(C3PR_HUB_URL: any, jwt: any): void;
+};
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
-import {HubClient} from "./types/HubClient";
+import {HubClient} from './types/HubClient';
+import _hubClientConfig from './hubClientConfig'
 import {_fetchProjectUuidForCloneUrl as fetchProjectUuidForCloneUrl} from "./adapters/fetchProjectUuidForCloneUrl";
 
 const index: HubClient = {
     fetchProjectUuidForCloneUrl
 };
 export default index;
+
+export const hubClientConfig = _hubClientConfig;
