9098e49ba00bb81404b5702296f1b6e3406e20f0
Antonio Jr <acdcjunior@gmail.com>
2018-06-03 17:32:35 -0300

    fix(updatePrOfProject): use PATCH, not POST

--- projects/updatePrOfProject.js
+++ projects/updatePrOfProject.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const axios_1 = require("axios");
 const __1 = require("../");
 async function updatePrOfProject(project_uuid, pr_id, status, assignee) {
-    let { data } = await axios_1.default.post(__1.hubClientConfig.c3pr.hub.prOfProjectUrl(project_uuid, pr_id), { status, assignee }, { headers: __1.hubClientConfig.headers() });
+    let { data } = await axios_1.default.patch(__1.hubClientConfig.c3pr.hub.prOfProjectUrl(project_uuid, pr_id), { status, assignee }, { headers: __1.hubClientConfig.headers() });
     return data;
 }
 exports.updatePrOfProject = updatePrOfProject;
--- projects/updatePrOfProject.ts
+++ projects/updatePrOfProject.ts
@@ -3,7 +3,7 @@ import {hubClientConfig, Event, PR} from "../";
 import {PRStatus} from "../types/PR";
 
 async function updatePrOfProject(project_uuid: string, pr_id: string, status: PRStatus, assignee?: {id: number, username: string}): Promise<any> {
-    let {data} = await axios.post(hubClientConfig.c3pr.hub.prOfProjectUrl(project_uuid, pr_id), {status, assignee}, {headers: hubClientConfig.headers()});
+    let {data} = await axios.patch(hubClientConfig.c3pr.hub.prOfProjectUrl(project_uuid, pr_id), {status, assignee}, {headers: hubClientConfig.headers()});
     return data;
 }
 
