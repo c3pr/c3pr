a19b5e9c9d13416df021f4ef8495493d56445e2a
Antonio Jr <acdcjunior@gmail.com>
2018-06-03 21:32:44 -0300

    feat(PullRequestCreated): add changes_committed_root

--- src/application/PullRequestCreated/createPullRequestCreated.ts
+++ src/application/PullRequestCreated/createPullRequestCreated.ts
@@ -5,10 +5,13 @@ import {GitLabMergeRequestCreated} from "../../ports/types/GitLabMergeRequestCre
 export function createPullRequestCreated(pullRequestRequestedEvent: Event<any>, createMrResult: GitLabMergeRequestCreated) {
     return {
         parent: {
-            event_type: "PullRequestRequested",
+            event_type: pullRequestRequestedEvent.event_type,
             uuid: pullRequestRequestedEvent.uuid
         },
+        changes_committed_root: pullRequestRequestedEvent.payload.changes_committed_root,
+
         repository: pullRequestRequestedEvent.payload.repository,
+
         pr_id: createMrResult.iid,
         pr_url: createMrResult.web_url,
         assignee: createMrResult.assignee && {id: createMrResult.assignee.id, username: createMrResult.assignee.username}
