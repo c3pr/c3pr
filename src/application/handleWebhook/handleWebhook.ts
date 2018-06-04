79198bd1ebf67768d19c619f89f2dd1858dae22b
Antonio Jr <acdcjunior@gmail.com>
2018-06-03 22:17:18 -0300

    refactor(handleWebhook): cleanup a bit

--- src/application/handleWebhook/handleWebhook.ts
+++ src/application/handleWebhook/handleWebhook.ts
@@ -7,20 +7,22 @@ function logMetaz(correlationId?: string) {
     return [{nodeName: 'c3pr-repo-gitlab', correlationId: correlationId, moduleName: 'handleWebhook'}];
 }
 
-export default function handleWebhook(webhookPayload) {
+function handleWebhook(webhookPayload) {
     if (webhookPayload.object_kind === "push") {
         c3prLOG2({
             msg: `Received webhook for PUSH from ${webhookPayload.repository.git_http_url}. Message: '${webhookPayload.commits && webhookPayload.commits[0].message.trim()}'.`,
             logMetas: logMetaz(webhookPayload.after)
         });
-        handlePush(webhookPayload);
-    } else if (webhookPayload.object_kind === "merge_request") {
-        handleMergeRequest(webhookPayload);
-    } else {
-        c3prLOG2({
-            msg: `Received webhook. Unknown type: ${webhookPayload.object_kind}.`,
-            logMetas: logMetaz(webhookPayload.after),
-            meta: {webhookPayload},
-        });
+        return handlePush(webhookPayload);
     }
+    if (webhookPayload.object_kind === "merge_request") {
+        return handleMergeRequest(webhookPayload);
+    }
+    c3prLOG2({
+        msg: `Received webhook. Unknown type: ${webhookPayload.object_kind}.`,
+        logMetas: logMetaz(webhookPayload.after),
+        meta: {webhookPayload},
+    });
 }
+
+export default handleWebhook;
