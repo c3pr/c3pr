7ac6e39f9ae4a325824f5da20674ad70e75bc955
Antonio Jr <acdcjunior@gmail.com>
2018-06-05 22:51:51 -0300

    feat(c3prSH3): add simpler sh function

---
+++ src/c3prSH3.js
@@ -0,0 +1,50 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const child_process_1 = require("child_process");
+const c3prLOG3_1 = require("node-c3pr-logger/c3prLOG3");
+function sh(command, options) {
+    return new Promise((resolve => {
+        child_process_1.exec(command, options, function (error, stdout, stderr) {
+            resolve({ error, stdout, stderr });
+        });
+    }));
+}
+function replaceTokens(input, replacements) {
+    let inputAfterReplacements = input;
+    replacements.forEach(({ regex, replaceWith }) => {
+        inputAfterReplacements = inputAfterReplacements.replace(regex, replaceWith);
+    });
+    return inputAfterReplacements;
+}
+async function c3prSH3(shCommand, shOptions = {}, { ids = [], stdout: shouldStdOut = false, replacements } = {}) {
+    const hideTokens = s => replaceTokens(s, replacements || []);
+    c3prLOG3_1.default({ msg: `\$ ${hideTokens(shCommand)}`, ids });
+    let { error, stdout, stderr } = await sh(shCommand, shOptions);
+    if (shouldStdOut) {
+        if (stdout.trim() === "")
+            stdout = '<empty output>';
+        c3prLOG3_1.default({ msg: hideTokens(stdout), ids });
+    }
+    if (error) {
+        c3prLOG3_1.default({
+            msg: `
+            [ERROR] >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+            COMMAND: ${hideTokens(shCommand)}
+            OPTIONS: ${hideTokens(JSON.stringify(shOptions))}
+            There as an error: ${hideTokens(error)}
+            ------------------------------
+            STDOUT:
+            ${hideTokens(stdout)}
+            ------------------------------
+            STDERR:
+            ${hideTokens(stderr)}
+            [/ERROR] <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<`,
+            ids
+        });
+        throw new Error(hideTokens(error));
+    }
+    return (hideTokens(stdout) || '').trim();
+}
+// noinspection JSUnusedGlobalSymbols
+exports.default = c3prSH3;
+//# sourceMappingURL=c3prSH3.js.map
---
+++ src/c3prSH3.ts
@@ -0,0 +1,56 @@
+import {exec, ExecOptions} from 'child_process';
+import c3prLOG3 from "node-c3pr-logger/c3prLOG3";
+
+function sh(command, options: { encoding: "buffer" | null } & ExecOptions | {}): Promise<{error, stdout, stderr}> {
+    return new Promise((resolve => {
+        exec(command, options, function (error, stdout, stderr) {
+            resolve({error, stdout, stderr});
+        });
+    }))
+}
+
+function replaceTokens(input, replacements: {regex:RegExp, replaceWith:string}[]) {
+    let inputAfterReplacements = input;
+    replacements.forEach(({regex, replaceWith}) => {
+        inputAfterReplacements = inputAfterReplacements.replace(regex, replaceWith)
+    });
+    return inputAfterReplacements;
+}
+
+async function c3prSH3(shCommand: string,
+                       shOptions: { encoding: "buffer" | null } & ExecOptions | {} = {},
+                       {ids = [], stdout: shouldStdOut = false, replacements}: {ids?: (string|number)[], stdout?: boolean, replacements?: {regex:RegExp, replaceWith:string}[]} = {}
+                       ) {
+    const hideTokens = s => replaceTokens(s, replacements || []);
+
+    c3prLOG3({msg: `\$ ${hideTokens(shCommand)}`, ids});
+
+    let {error, stdout, stderr} = await sh(shCommand, shOptions);
+    if (shouldStdOut) {
+        if (stdout.trim() === "")
+            stdout = '<empty output>';
+        c3prLOG3({msg: hideTokens(stdout), ids});
+    }
+    if (error) {
+        c3prLOG3({
+            msg: `
+            [ERROR] >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+            COMMAND: ${hideTokens(shCommand)}
+            OPTIONS: ${hideTokens(JSON.stringify(shOptions))}
+            There as an error: ${hideTokens(error)}
+            ------------------------------
+            STDOUT:
+            ${hideTokens(stdout)}
+            ------------------------------
+            STDERR:
+            ${hideTokens(stderr)}
+            [/ERROR] <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<`,
+            ids
+        });
+        throw new Error(hideTokens(error));
+    }
+    return (hideTokens(stdout) || '').trim();
+}
+
+// noinspection JSUnusedGlobalSymbols
+export default c3prSH3;
