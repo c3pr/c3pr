631db831f8faa5210d91c4eba4cf939c5ca5cfe3
Antonio Jr <acdcjunior@gmail.com>
2018-06-23 12:45:01 -0300

    feat(projects): improve error message

--- projects/fetchFirstProjectForCloneUrl.js
+++ projects/fetchFirstProjectForCloneUrl.js
@@ -5,7 +5,7 @@ const __1 = require("../");
 async function fetchFirstProjectForCloneUrl(clone_url_http) {
     const { data } = await axios_1.default.get(__1.hubClientConfig.c3pr.hub.projectsByCloneUrlHttp(clone_url_http), { headers: __1.hubClientConfig.headers() });
     if (!data.length) {
-        throw new Error('Project with URL ' + clone_url_http + ' not found.');
+        throw new Error('No project at C-3PR Hub was found for URL ' + clone_url_http + ' not found. Did you create a project there?');
     }
     const [{ uuid: project_uuid }] = data;
     return project_uuid;
--- projects/fetchFirstProjectForCloneUrl.ts
+++ projects/fetchFirstProjectForCloneUrl.ts
@@ -4,7 +4,7 @@ import {hubClientConfig} from "../";
 async function fetchFirstProjectForCloneUrl(clone_url_http: string): Promise<string> {
     const {data} = await axios.get(hubClientConfig.c3pr.hub.projectsByCloneUrlHttp(clone_url_http), {headers: hubClientConfig.headers()});
     if (!data.length) {
-        throw new Error('Project with URL ' + clone_url_http + ' not found.');
+        throw new Error('No project at C-3PR Hub was found for URL ' + clone_url_http + ' not found. Did you create a project there?');
     }
     const [{uuid: project_uuid}] = data;
     return project_uuid;
