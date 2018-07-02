e5277065fb462b4119291be3ecb3b48f6a890e4e
Antonio acdc Jr <acdcjunior@gmail.com>
2018-07-02 20:20:00 -0300

    feat: add different projects

--- git-clone-all.sh
+++ git-clone-all.sh
@@ -4,6 +4,7 @@ cd ..
 git clone http://github.com/c3pr/c3pr-agent
 git clone http://github.com/c3pr/c3pr-brain
 git clone http://github.com/c3pr/c3pr-dashboard
+git clone http://github.com/c3pr/c3pr-dashboard-ui
 git clone http://github.com/c3pr/c3pr-hub
 git clone http://github.com/c3pr/c3pr-repo-github
 git clone http://github.com/c3pr/c3pr-repo-gitlab
@@ -12,3 +13,4 @@ git clone http://github.com/c3pr/node-c3pr-hub-client
 git clone http://github.com/c3pr/node-c3pr-logger
 git clone http://github.com/c3pr/node-c3pr-repo
 git clone http://github.com/c3pr/node-git-client
+git clone http://github.com/c3pr/sample-project-java-maven
