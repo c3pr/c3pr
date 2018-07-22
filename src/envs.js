eea9df5d3abd6fdb61d99c38d585c133154cfad4
Antonio Jr <acdcjunior@gmail.com>
2018-07-22 20:25:16 -0300

    feat(envs): handle non-array values returned

--- src/envs.js
+++ src/envs.js
@@ -5,4 +5,17 @@ export const BACKEND = `http://${document.location.hostname}:7305`;
 export const BACKEND_HUB = BACKEND + '/api/hub';
 //export const BACKEND_HUB = `http://127.0.0.1:7300`;
 
-export default axios.create({baseURL: BACKEND_HUB});
+
+const c3prAxios = axios.create({baseURL: BACKEND_HUB});
+
+c3prAxios.interceptors.response.use(function (response) {
+  if (!Array.isArray(response.data)) {
+    console.error(`Empty array returned because response obtained from '${response.config.url}' is not an array. Got ${JSON.stringify(response.data)}.`);
+    return {...response, data: []};
+  }
+  return response;
+}, function (error) {
+  return Promise.reject(error);
+});
+
+export default c3prAxios;
