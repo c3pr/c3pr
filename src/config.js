599e58c56de9f05c987350da299ec419cb897d3f
Antonio Jr <acdcjunior@gmail.com>
2018-07-22 16:36:15 -0300

    feat(config): pick up logs database and collection from env vars

--- src/config.js
+++ src/config.js
@@ -1,11 +1,13 @@
-const C3PR_MONGO_URL = process.env.C3PR_MONGO_URL;
+const C3PR_MONGO_URL = process.env.C3PR_MONGO_URL || 'mongodb://127.0.0.1:27017';
+const C3PR_MONGO_DATABASE = process.env.C3PR_MONGO_DATABASE || 'c3pr';
+const C3PR_MONGO_LOGS_COLLECTION = process.env.C3PR_MONGO_LOGS_COLLECTION || 'logs';
 
 module.exports = {
     c3pr: {
         logger: {
             mongoUrl: C3PR_MONGO_URL,
-            database: 'c3pr',
-            collection: 'logs'
+            database: C3PR_MONGO_DATABASE,
+            collection: C3PR_MONGO_LOGS_COLLECTION
         }
     }
 };
