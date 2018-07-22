fddc3079cda7ba994bb6c2659d9174555447aa88
Antonio Jr <acdcjunior@gmail.com>
2018-07-22 16:43:19 -0300

    feat(config): pick up logs database and collection from env vars

--- src/config.js
+++ src/config.js
@@ -1,5 +1,7 @@
 const PORT = process.env.PORT;
 const C3PR_MONGO_URL = process.env.C3PR_MONGO_URL || 'mongodb://127.0.0.1:27017';
+const C3PR_MONGO_DATABASE = process.env.C3PR_MONGO_DATABASE || 'c3pr';
+const C3PR_MONGO_LOGS_COLLECTION = process.env.C3PR_MONGO_LOGS_COLLECTION || 'logs';
 
 module.exports = {
     c3pr: {
@@ -7,7 +9,7 @@ module.exports = {
             port: PORT || 7300,
 
             mongoC3prUrl: C3PR_MONGO_URL,
-            mongoC3prDatabase: 'c3pr',
+            mongoC3prDatabase: C3PR_MONGO_DATABASE,
             mongoEventsCollection: 'events',
             uncollectPollingInMs: 5 * 60 * 1000, // five minutes
             uncollectTimeoutInMs: 60 * 60 * 1000, // one hour
@@ -16,7 +18,7 @@ module.exports = {
             mongoPRsCollection: 'prs',
             mongoAgentRegistryCollection: 'agents',
 
-            mongoLogsCollection: 'logs',
+            mongoLogsCollection: C3PR_MONGO_LOGS_COLLECTION,
 
             bus: {
                 maxRetries: 2,
