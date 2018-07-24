b912856e73400390dbc54f8e4633be368ce6e013
Antonio Jr <acdcjunior@gmail.com>
2018-07-24 17:24:23 -0300

    fix(hub): properly forward requests, including query params

--- src/web/hubController.js
+++ src/web/hubController.js
@@ -1,11 +1,14 @@
-const axios = require('axios');
+const axios = require('axios').default;
 const config = require('../config');
 
 module.exports = function (app) {
 
     app.get('/api/hub*', function (request, response) {
         const headers = {Authorization: `Bearer ${config.c3pr.auth.jwt}`};
-        axios.get(config.c3pr.hub.c3prHubUrl + request.params[0], {headers}).then(({data}) => {
+
+        const targetUrl = request.originalUrl.replace(/^\/api\/hub/, '');
+
+        axios.get(config.c3pr.hub.c3prHubUrl + targetUrl, {headers}).then(({data}) => {
             response.send(data);
         }).catch(e => {
             response.send(e.toString());
