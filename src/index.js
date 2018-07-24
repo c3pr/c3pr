40d77a7a1f1b303d7bc75468fe0d0017c74ced46
Antonio Jr <acdcjunior@gmail.com>
2018-07-24 19:36:47 -0300

    feat(error): improve error handling

--- src/index.js
+++ src/index.js
@@ -3,3 +3,19 @@ require('./application/events/events').init.then(() => {
 }).then(() => {
     require('./web/express');
 });
+
+process.on('uncaughtException', function (err) {
+    console.log('uncaughtException >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>');
+    const stack = new Error().stack;
+    console.log(stack);
+    console.log(err);
+    console.log('uncaughtException <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<');
+});
+
+process.on('UnhandledPromiseRejectionWarning', function (err) {
+    console.log('UnhandledPromiseRejectionWarning >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>');
+    const stack = new Error().stack;
+    console.log(stack);
+    console.log(err);
+    console.log('UnhandledPromiseRejectionWarning <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<');
+});
