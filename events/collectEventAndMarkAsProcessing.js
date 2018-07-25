1172286d9488814f64816141e8ef6e5033a4c125
Antonio Jr <acdcjunior@gmail.com>
2018-07-24 22:20:01 -0300

    refactor(events): migrate to log3

--- events/collectEventAndMarkAsProcessing.js
+++ events/collectEventAndMarkAsProcessing.js
@@ -1,12 +1,10 @@
-const axios = require('axios');
-const c3prLOG2 = require("node-c3pr-logger/c3prLOG2").c3pr.c3prLOG2;
-
-const logMeta = {nodeName: 'node-c3pr-hub-client', moduleName: 'collectEventAndMarkAsProcessing'};
+const axios = require('axios').default;
+const c3prLOG3 = require("node-c3pr-logger/c3prLOG3").default;
 
 /**
  * When getting the return from this function, make sure you handle when it returns null, because it will be a common result.
  */
-async function collectEventAndMarkAsProcessing({event_type, c3prHubUrl, jwt, logMetas: outerLogMetas}) {
+async function collectEventAndMarkAsProcessing({event_type, c3prHubUrl, jwt}) {
     const headers = {Authorization: `Bearer ${jwt}`};
 
     /** @namespace event.payload */
@@ -18,13 +16,9 @@ async function collectEventAndMarkAsProcessing({event_type, c3prHubUrl, jwt, log
     try {
         await axios.patch(`${c3prHubUrl}/api/v1/events/${event_type}/${event.uuid}/meta/processing`, {}, {headers});
         return {uuid: event.uuid, event_type, payload: event.payload};
-    } catch (e) {
-        c3prLOG2({
-            msg: `Error while marking event ${event.uuid} of type ${event_type} as processing. Reason: '${e}'. Data: ${e.response && JSON.stringify(e.response.data) || 'no data'}`,
-            logMetas: [...(outerLogMetas || []), logMeta],
-            meta: {error: require('util').inspect(e)}
-        });
-        throw e;
+    } catch (error) {
+        c3prLOG3(`Error while marking event ${event.uuid} of type ${event_type} as processing.`, {ids: ['pre-event'], error});
+        throw error;
     }
 }
 
