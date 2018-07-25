1702cb8e7875c36783e2978d52ee1fceec5fc950
Antonio Jr <acdcjunior@gmail.com>
2018-07-24 22:22:56 -0300

    test(events): update tools and log db

--- events/collectEventAndMarkAsProcessing.demo.js
+++ events/collectEventAndMarkAsProcessing.demo.js
@@ -1,13 +1,14 @@
+require("node-c3pr-logger").testMode();
 const login = require('../login').c3prHubClient.login;
 const collectEventAndMarkAsProcessing = require('./collectEventAndMarkAsProcessing').collectEventAndMarkAsProcessing.collectEventAndMarkAsProcessing;
 
 (async () => {
 
-    let jwt = await login({loginUrl: 'http://127.0.0.1:5000/api/v1/login'});
+    let jwt = await login({loginUrl: 'http://127.0.0.1:7300/api/v1/login', username: 'u', password: 'p'});
     console.log(jwt);
 
     collectEventAndMarkAsProcessing({
-        c3prHubUrl: 'http://localhost:5000',
+        c3prHubUrl: 'http://localhost:7300',
         jwt,
         event_type: 'ChangesCommitted'
     })
