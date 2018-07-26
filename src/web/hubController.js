3ed51d1c825af12baa820e221e9914187f58c01b
Antonio Jr <acdcjunior@gmail.com>
2018-07-25 23:35:43 -0300

    feat(hub): attempt to forward other HTTP methods

--- src/web/hubController.js
+++ src/web/hubController.js
@@ -15,4 +15,28 @@ module.exports = function (app) {
         })
     });
 
+    app.patch('/api/hub*', function (request, response) {
+        const headers = {Authorization: `Bearer ${config.c3pr.auth.jwt}`};
+
+        const targetUrl = request.originalUrl.replace(/^\/api\/hub/, '');
+
+        axios.patch(config.c3pr.hub.c3prHubUrl + targetUrl, {headers}, request.body).then(({data}) => {
+            response.send(data);
+        }).catch(e => {
+            response.send(e.toString());
+        })
+    });
+
+    app.post('/api/hub*', function (request, response) {
+        const headers = {Authorization: `Bearer ${config.c3pr.auth.jwt}`};
+
+        const targetUrl = request.originalUrl.replace(/^\/api\/hub/, '');
+
+        axios.post(config.c3pr.hub.c3prHubUrl + targetUrl, {headers}, request.body).then(({data}) => {
+            response.send(data);
+        }).catch(e => {
+            response.send(e.toString());
+        })
+    });
+
 };
