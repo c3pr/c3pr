f7a64c9f55277d29b50d36203b329c7323f6358d
Antonio Jr <acdcjunior@gmail.com>
2018-07-30 17:00:05 -0300

    feat: handle {lcid, euuid}

--- src/application/PullRequestCreated/savePR.js
+++ src/application/PullRequestCreated/savePR.js
@@ -1,7 +1,7 @@
-const c3prLOG2 = require("node-c3pr-logger/c3prLOG2").c3prLOG2;
+const c3prLOG4 = require("node-c3pr-logger/c3prLOG4").default;
 const ports = require('../ports');
 
-async function savePR(pullRequestCreatedEvent) {
+async function savePR(pullRequestCreatedEvent, {lcid, euuid}) {
     const project_uuid = await ports.fetchFirstProjectForCloneUrl(pullRequestCreatedEvent.payload.repository.clone_url_http);
     const changed_files = await ports.fetchChangedFilesForPullRequestCreatedEvent(pullRequestCreatedEvent);
 
@@ -15,11 +15,7 @@ async function savePR(pullRequestCreatedEvent) {
     };
 
     let result = await ports.postNewPrForProject(project_uuid, pr);
-    c3prLOG2({
-        msg: `Created PR in database for ${pullRequestCreatedEvent.payload.pr_url}`,
-        logMetas: [{nodeName: 'c3pr-brain', moduleName: 'savePR'}],
-        meta: {pullRequestCreatedEvent, project_uuid, changed_files}
-    });
+    c3prLOG4(`Created PR in database for ${pullRequestCreatedEvent.payload.pr_url}`, {lcid, euuid, meta: {pullRequestCreatedEvent, project_uuid, changed_files}});
     return {new_status: 'PROCESSED', result};
 }
 
