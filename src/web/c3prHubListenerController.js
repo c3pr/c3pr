b7e2bda97f02715a72f3de036d5e2e8c4eb67cd6
Antonio Jr <acdcjunior@gmail.com>
2018-07-30 16:46:59 -0300

    feat(web): generate and pass down {lcid, euuid}

--- src/web/c3prHubListenerController.js
+++ src/web/c3prHubListenerController.js
@@ -1,36 +1,40 @@
 const config = require('../config');
-const c3prLOG2 = require("node-c3pr-logger/c3prLOG2").c3pr.c3prLOG2;
 
 const c3prHCC = require('../application/ChangesCommitted/handleChangesCommitted').c3pr;
 const c3prHTIC = require('../application/ToolInvocationCompleted/handleToolInvocationCompleted').c3pr;
 const handlePullRequestCreated = require('../application/PullRequestCreated/handlePullRequestCreated');
 const handlePullRequestUpdated = require('../application/PullRequestUpdated/handlePullRequestUpdated');
 
-const logMetas = [{nodeName: 'c3pr-brain', moduleName: 'c3prHubListenerController'}];
+const c3prLOG4 = require("node-c3pr-logger/c3prLOG4").default;
+const euuid = 'express-hub';
 
 module.exports = function (app) {
 
     app.post(config.c3pr.brain.ChangesCommittedCallbackUrl, function (request, response) {
-        c3prLOG2({msg: `'ChangesCommitted' received.`, logMetas});
-        c3prHCC.handleChangesCommitted();
+        const lcid = c3prLOG4.lcid();
+        c3prLOG4(`'ChangesCommitted' received.`, {lcid, euuid});
+        c3prHCC.handleChangesCommitted({lcid, euuid});
         response.send();
     });
 
     app.post(config.c3pr.brain.ToolInvocationCompletedCallbackUrl, function (request, response) {
-        c3prLOG2({msg: `'ToolInvocationCompleted' received.`, logMetas});
-        c3prHTIC.handleToolInvocationCompleted();
+        const lcid = c3prLOG4.lcid();
+        c3prLOG4(`'ToolInvocationCompleted' received.`, {lcid, euuid});
+        c3prHTIC.handleToolInvocationCompleted({lcid, euuid});
         response.send();
     });
 
     app.post(config.c3pr.brain.PullRequestCreatedCallbackUrl, function (request, response) {
-        c3prLOG2({msg: `'PullRequestCreated' received.`, logMetas});
-        handlePullRequestCreated();
+        const lcid = c3prLOG4.lcid();
+        c3prLOG4(`'PullRequestCreated' received.`, {lcid, euuid});
+        handlePullRequestCreated({lcid, euuid});
         response.send();
     });
 
     app.post(config.c3pr.brain.PullRequestUpdatedCallbackUrl, function (request, response) {
-        c3prLOG2({msg: `'PullRequestUpdated' received.`, logMetas});
-        handlePullRequestUpdated();
+        const lcid = c3prLOG4.lcid();
+        c3prLOG4(`'PullRequestUpdated' received.`, {lcid, euuid});
+        handlePullRequestUpdated({lcid, euuid});
         response.send();
     });
 
