c672037cee7530be7f6543abeb518e2429e41327
Antonio Jr <acdcjunior@gmail.com>
2018-07-30 16:29:09 -0300

    chore(events): add missing files

---
+++ events/handleFirstCollectedEvent.d.ts
@@ -0,0 +1,8 @@
+export default function handleFirstCollectedEvent({ event_type, handlerFunction, c3prHubUrl, jwt, lcid: outerLCID, euuid: outerEUUID }: {
+    event_type: any;
+    handlerFunction: any;
+    c3prHubUrl: any;
+    jwt: any;
+    lcid: any;
+    euuid: any;
+}): Promise<any>;
