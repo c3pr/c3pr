86966ad84b8b1a0351eb5427a1515f3f4332cd1a
Antonio Jr <acdcjunior@gmail.com>
2018-07-30 16:45:15 -0300

    feat: pass down {lcid, euuid}

--- src/application/ToolInvocationCompleted/handleToolInvocationCompleted.js
+++ src/application/ToolInvocationCompleted/handleToolInvocationCompleted.js
@@ -1,18 +1,16 @@
-const handleFirstCollectedEvent = require('node-c3pr-hub-client/events/handleFirstCollectedEvent').handleFirstCollectedEvent.handleFirstCollectedEvent;
-
+const handleFirstCollectedEvent = require('node-c3pr-hub-client/events/handleFirstCollectedEvent').default;
 const createAndEmitPullRequestRequested = require('./createAndEmitPullRequestRequested');
-
 const config = require('../../config');
 
-const logMetas = [{nodeName: 'c3pr-brain', moduleName: 'handleToolInvocationCompleted'}];
 
-function handleToolInvocationCompleted() {
+function handleToolInvocationCompleted({lcid, euuid}) {
     return handleFirstCollectedEvent({
         event_type: `ToolInvocationCompleted`,
         handlerFunction: createAndEmitPullRequestRequested,
         c3prHubUrl: config.c3pr.hub.c3prHubUrl,
         jwt: config.c3pr.auth.jwt,
-        logMetas
+        lcid,
+        euuid
     });
 }
 
