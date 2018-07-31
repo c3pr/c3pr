ee895e944d83a7715c78fafd1b56797e77c9f163
Antonio Jr <acdcjunior@gmail.com>
2018-07-31 16:33:50 -0300

    fix(log4): remove logmetas

--- login/index.demo.js
+++ login/index.demo.js
@@ -2,10 +2,11 @@ const login = require('./index').c3prHubClient.login;
 
 login({
     loginUrl: 'http://127.0.0.1:5000/api/v1/login',
-    logMetas: [],
     username: 'username',
     password: 'password',
-    subscriptions: []
+    subscriptions: [],
+    lcid: 'lcid',
+    euuid: 'euuid'
 })
     .then(s => console.log(s))
     .catch(e => console.log('Error', e));
