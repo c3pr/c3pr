1e94c0289958b35a3be438934ec208ac7ed374d2
Antonio Jr <acdcjunior@gmail.com>
2018-07-31 17:13:04 -0300

    fix: remove reference to deleted scripts

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
 module.exports = {
-    cloneRepositoryLocally: require('./src/cloneRepositoryLocally'),
-    determineGitDiffBase64: require('./src/determineGitDiffBase64'),
-    c3prSH: require('./src/c3prSH')
+    cloneRepositoryLocally: require('./src/cloneRepositoryLocally')
 };
