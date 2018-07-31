c76b0193af3a7dd3544c0f6ba022e2e52b881ccf
Antonio Jr <acdcjunior@gmail.com>
2018-07-31 16:09:20 -0300

    fix(login): fix login and update deps

--- src/application/login/login.js
+++ src/application/login/login.js
@@ -2,11 +2,17 @@ const config = require('../../config');
 
 const c3prHubClient = require('node-c3pr-hub-client/login').c3prHubClient;
 
+const c3prLOG4 = require("node-c3pr-logger/c3prLOG4").default;
+const lcid = c3prLOG4.lcid();
+const euuid = 'dashboard-login';
+
 c3prHubClient.login({
     loginUrl: config.c3pr.hub.loginUrl,
     username: 'c3pr-dashboard',
     password: 'not-used',
-    subscriptions: []
+    subscriptions: [],
+    lcid,
+    euuid
 }).then(jwt => {
     config.c3pr.auth.jwt = jwt;
 }).catch(e => {
