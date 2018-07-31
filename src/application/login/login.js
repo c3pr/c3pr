338b7f5a686213ba0b15fd0d8d6698150ffb0ae4
Antonio Jr <acdcjunior@gmail.com>
2018-07-31 16:09:22 -0300

    fix(login): fix login and update deps

--- src/application/login/login.js
+++ src/application/login/login.js
@@ -2,6 +2,10 @@ const config = require('../../config');
 
 const c3prHubClient = require('node-c3pr-hub-client/login').c3prHubClient;
 
+const c3prLOG4 = require("node-c3pr-logger/c3prLOG4").default;
+const lcid = c3prLOG4.lcid();
+const euuid = 'brain-login';
+
 c3prHubClient.login({
     loginUrl: config.c3pr.hub.loginUrl,
     username: 'c3pr-brain',
@@ -11,7 +15,9 @@ c3prHubClient.login({
         {eventType: "ToolInvocationCompleted", callbackUrl: config.c3pr.brain.c3prBrainUrl + config.c3pr.brain.ToolInvocationCompletedCallbackUrl},
         {eventType: "PullRequestCreated",      callbackUrl: config.c3pr.brain.c3prBrainUrl + config.c3pr.brain.PullRequestCreatedCallbackUrl},
         {eventType: "PullRequestUpdated",      callbackUrl: config.c3pr.brain.c3prBrainUrl + config.c3pr.brain.PullRequestUpdatedCallbackUrl}
-    ]
+    ],
+    lcid,
+    euuid
 }).then(jwt => {
     config.c3pr.auth.jwt = jwt;
 }).catch(e => {
