09a692b59116fa79816712afdc203d533d6f4d7d
Antonio Jr <acdcjunior@gmail.com>
2018-07-31 16:06:26 -0300

    fix(login): fix login and update deps

--- src/application/login/login.ts
+++ src/application/login/login.ts
@@ -2,6 +2,10 @@ import config from '../../config';
 
 import { c3prHubClient } from 'node-c3pr-hub-client/login';
 
+const c3prLOG4 = require("node-c3pr-logger/c3prLOG4").default;
+const lcid = c3prLOG4.lcid();
+const euuid = 'gitlab-login';
+
 function c3prRepoGitLabLogin(): Promise<void> {
     return c3prHubClient.login({
         loginUrl: config.c3pr.hub.loginUrl,
@@ -9,7 +13,9 @@ function c3prRepoGitLabLogin(): Promise<void> {
         password: 'unused',
         subscriptions: [
             {eventType: "PullRequestRequested", callbackUrl: config.c3pr.repoGitlab.c3prRepoGitlabUrl + config.c3pr.repoGitlab.PullRequestRequestedCallbackUrl}
-        ]
+        ],
+        lcid,
+        euuid
     }).then(jwt => {
         config.c3pr.hub.auth.jwt = jwt;
     }).catch(e => {
