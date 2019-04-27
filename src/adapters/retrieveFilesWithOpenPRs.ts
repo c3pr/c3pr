5e063e9ab9ebf68154d32eff0d4ac54bdb5f1418
Antonio Jr <acdcjunior@gmail.com>
2019-04-27 19:24:09 -0300

    refactor(adapters): curry function and export partial application

--- src/adapters/retrieveFilesWithOpenPRs.ts
+++ src/adapters/retrieveFilesWithOpenPRs.ts
@@ -4,11 +4,12 @@ import config from '../config';
 /**
  * Returns all files with an open PR that was created by the bot.
  */
-async function retrieveFilesWithOpenPRs(changes_committed_root) {
+const retrieveFilesWithOpenPRs = config => axios =>
+async function (changes_committed_root) {
     const headers = {Authorization: `Bearer ${config.c3pr.auth.jwt}`};
     let {data: {payload: {project_uuid}}} = await axios.get(config.c3pr.hub.changesCommittedOfUuidUrl.replace(/:uuid/, changes_committed_root), {headers});
     let {data: filesWithOpenPRs} = await axios.get(config.c3pr.hub.filesWithOpenPRsForProjectUrl.replace(/:project_uuid/, project_uuid), {headers});
     return filesWithOpenPRs;
-}
+};
 
-export default retrieveFilesWithOpenPRs;
+export default retrieveFilesWithOpenPRs(config)(axios);
