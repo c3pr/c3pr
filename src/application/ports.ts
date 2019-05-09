d9549e0907a760f0b9b955faa88638bc8f7b36ed
Antonio Jr <acdcjunior@gmail.com>
2019-05-09 03:19:29 -0300

    refactor(ports): remove unused fields

--- src/application/ports.ts
+++ src/application/ports.ts
@@ -1,26 +1,17 @@
-import fetchAllToolAgents from "../adapters/fetchAllToolAgents";
 import shuffleArray from "../adapters/shuffleArray";
-import fetchProjectFiles from "../adapters/fetchProjectFiles";
-import retrieveFilesWithOpenPRs from "../adapters/retrieveFilesWithOpenPRs";
 import fetchChangedFilesForPullRequestCreatedEvent from "../adapters/fetchChangedFilesForPullRequestCreatedEvent";
 import {fetchFirstProjectForCloneUrl} from "node-c3pr-hub-client/projects/fetchFirstProjectForCloneUrl";
 import {postNewPrForProject} from "node-c3pr-hub-client/projects/postNewPrForProject";
 import {updatePrOfProject} from "node-c3pr-hub-client/projects/updatePrOfProject";
 
 const ports: {
-    fetchAllToolAgents;
     shuffleArray;
-    fetchProjectFiles;
-    retrieveFilesWithOpenPRs;
     fetchChangedFilesForPullRequestCreatedEvent;
     fetchFirstProjectForCloneUrl;
     postNewPrForProject;
     updatePrOfProject;
 } = {
-    fetchAllToolAgents,
     shuffleArray,
-    fetchProjectFiles,
-    retrieveFilesWithOpenPRs,
     fetchChangedFilesForPullRequestCreatedEvent,
     fetchFirstProjectForCloneUrl,
     postNewPrForProject,
