16d1a4543886c513d1e2d67a695ae2cc71565297
Antonio Jr <acdcjunior@gmail.com>
2019-05-12 05:07:23 -0300

    refactor: update hub-client usage

--- src/application/login/login.ts
+++ src/application/login/login.ts
@@ -1,20 +1,22 @@
 import config from '../../config';
 
-import { c3prHubClient } from 'node-c3pr-hub-client/login';
+import c3prHubLogin from 'node-c3pr-hub-client/login';
 import c3prLOG5 from "node-c3pr-logger/c3prLOG5";
 
 const _c3prLOG5 = c3prLOG5({sha: '!repo-gitlab-login'});
 
 function c3prRepoGitLabLogin(): Promise<void> {
-    return c3prHubClient.login({
+    return c3prHubLogin(
+        {
         loginUrl: config.c3pr.hub.loginUrl,
         username: 'c3pr-repo-gitlab',
         password: 'unused',
         subscriptions: [
             {eventType: "PullRequestRequested", callbackUrl: config.c3pr.repoGitlab.c3prRepoGitlabUrl + config.c3pr.repoGitlab.PullRequestRequestedCallbackUrl}
-        ],
-        ..._c3prLOG5
-    }).then(jwt => {
+        ]
+        },
+        _c3prLOG5
+    ).then(jwt => {
         config.c3pr.hub.auth.jwt = jwt;
     }).catch(e => {
         throw e;
