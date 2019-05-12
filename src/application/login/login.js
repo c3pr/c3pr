bd6321de1e4458b015dff51087c05205a6826956
Antonio Jr <acdcjunior@gmail.com>
2019-05-12 05:10:25 -0300

    refactor: update dep and hub-client usage

--- src/application/login/login.js
+++ src/application/login/login.js
@@ -1,18 +1,20 @@
 const config = require('../../config');
 
-const c3prHubClient = require('node-c3pr-hub-client/login').c3prHubClient;
+const c3prHubLogin = require('node-c3pr-hub-client/login').default;
 
 const c3prLOG5 = require("node-c3pr-logger/c3prLOG5").default;
 
 const _c3prLOG5 = c3prLOG5({sha: '!dashboard-login'});
 
-c3prHubClient.login({
+c3prHubLogin(
+    {
     loginUrl: config.c3pr.hub.loginUrl,
     username: 'c3pr-dashboard',
     password: 'not-used',
-    subscriptions: [],
-    ..._c3prLOG5
-}).then(jwt => {
+    subscriptions: []
+    },
+    _c3prLOG5
+).then(jwt => {
     config.c3pr.auth.jwt = jwt;
 }).catch(e => {
     throw e;
