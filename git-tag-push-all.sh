8230b26d1826c46ecdca7ab214ff36cb674f9fca
Antonio Jr <acdcjunior@gmail.com>
2019-05-16 04:35:00 -0300

    feat: add which repo is being tagged (so we know when there's an error)

--- git-tag-push-all.sh
+++ git-tag-push-all.sh
@@ -1,26 +1,32 @@
 #!/usr/bin/env bash
 
 cd ../c3pr-agent
+echo tagging c3pr-agent...
 git tag $(sed -nE 's/^\s*"version": "(.*?)",$/\1/p' package.json)
 # git push --tags
 
 cd ../c3pr-brain
+echo tagging c3pr-brain...
 git tag $(sed -nE 's/^\s*"version": "(.*?)",$/\1/p' package.json)
 # git push --tags
 
 cd ../c3pr-dashboard
+echo tagging c3pr-dashboard...
 git tag $(sed -nE 's/^\s*"version": "(.*?)",$/\1/p' package.json)
 # git push --tags
 
 cd ../c3pr-dashboard-ui
+echo tagging c3pr-dashboard-ui...
 git tag $(sed -nE 's/^\s*"version": "(.*?)",$/\1/p' package.json)
 # git push --tags
 
 cd ../c3pr-hub
+echo tagging c3pr-hub...
 git tag $(sed -nE 's/^\s*"version": "(.*?)",$/\1/p' package.json)
 # git push --tags
 
 cd ../c3pr-repo-gitlab
+echo tagging c3pr-repo-gitlab...
 git tag $(sed -nE 's/^\s*"version": "(.*?)",$/\1/p' package.json)
 # git push --tags
 
