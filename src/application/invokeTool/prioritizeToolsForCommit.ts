f0a3f21a5f6b5cfcd691aeed9b73a4901be7be63
Antonio Jr <acdcjunior@gmail.com>
2019-06-23 07:23:08 -0300

    feat(tools): shuffle tools before sorting by weight

--- src/application/invokeTool/prioritizeToolsForCommit.ts
+++ src/application/invokeTool/prioritizeToolsForCommit.ts
@@ -1,5 +1,6 @@
+import shuffleArray from "../../adapters/shuffleArray";
 
 
 export async function prioritizeToolsForCommit(changesCommittedRootEuuid, tools) {
-    return tools.sort((t1, t2) => t1.weight||0 - t2.weight||0)
+    return shuffleArray(tools).sort((t1, t2) => t1.weight||0 - t2.weight||0)
 }
