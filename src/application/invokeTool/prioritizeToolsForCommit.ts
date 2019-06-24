6df40a0dd067b661e9df934412a6a94808105592
Antonio Jr <acdcjunior@gmail.com>
2019-06-24 06:46:19 -0300

    feat: add weight for agent

--- src/application/invokeTool/prioritizeToolsForCommit.ts
+++ src/application/invokeTool/prioritizeToolsForCommit.ts
@@ -2,5 +2,5 @@ import shuffleArray from "../../adapters/shuffleArray";
 
 
 export async function prioritizeToolsForCommit(changesCommittedRootEuuid, tools) {
-    return shuffleArray(tools).sort((t1, t2) => t1.weight||0 - t2.weight||0)
+    return shuffleArray(tools).sort((t1, t2) => t1.weight - t2.weight)
 }
