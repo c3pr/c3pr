40971b22e10ddec4fae19e5e7f503f690ddb4f6d
Antonio 'acdc' Jr <acdcjunior@gmail.com>
2019-07-03 20:06:32 -0300

    feat: add improved error handling

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,13 @@
+const Sentry = require('@sentry/node');
+Sentry.init({ dsn: 'https://ca35d507f8184162a6f3d3151f3de475@sentry.io/1494586' });
+
+process.on('unhandledRejection', function (err) {
+    console.log('unhandledRejection >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>');
+    const stack = new Error().stack;
+    console.log(stack);
+    console.log(err);
+    console.log('unhandledRejection <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<');
+});
+
 require('./application/login/login');
 require('./web/express');
