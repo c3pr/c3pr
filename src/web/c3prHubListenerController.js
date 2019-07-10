c6e1458af8d2e9707ccb966bd3ece49a9aa9a664
Antonio Jr <acdcjunior@gmail.com>
2019-07-10 01:50:51 -0300

    fix(web): remove unused variable

--- src/web/c3prHubListenerController.js
+++ src/web/c3prHubListenerController.js
@@ -3,7 +3,7 @@ const config = require('../config');
 const handleToolInvocationRequested = require('../application/ToolInvocationRequested/handleToolInvocationRequested');
 
 let c3prLOG5 = require("node-c3pr-logger/c3prLOG5").default;
-c3prLOG5 = c3prLOG5({service_name: 'c3pr-agent#'+config.c3pr.agent.agentId, caller_name: 'c3prHubListenerController', euuid: request.body.event && request.body.event.uuid});
+c3prLOG5 = c3prLOG5({service_name: 'c3pr-agent#'+config.c3pr.agent.agentId, caller_name: 'c3prHubListenerController'});
 
 function euuid(request) {
     return request.body && request.body.uuid;
