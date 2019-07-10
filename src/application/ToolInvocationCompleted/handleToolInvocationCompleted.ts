9c114bbe6e835bedf14d1c9e7c467251c85194f2
Antonio 'acdc' Jr <acdcjunior@gmail.com>
2019-07-10 00:13:21 -0300

    feat: update typings

--- src/application/ToolInvocationCompleted/handleToolInvocationCompleted.ts
+++ src/application/ToolInvocationCompleted/handleToolInvocationCompleted.ts
@@ -6,7 +6,7 @@ import createAndEmitPullRequestRequested from "./createAndEmitPullRequestRequest
 export default function handleToolInvocationCompleted(c3prLOG5) {
     return handleFirstCollectedEvent({
         event_type: `ToolInvocationCompleted`,
-        handlerFunction: createAndEmitPullRequestRequested,
+        handlerFunction: createAndEmitPullRequestRequested as any,
         c3prHubUrl: config.c3pr.hub.c3prHubUrl,
         jwt: config.c3pr.auth.jwt
     }, c3prLOG5);
