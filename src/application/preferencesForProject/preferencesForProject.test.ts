3d1db5f74aafc1bf2e50f43e7690cef41f3786ba
Antonio Jr <acdcjunior@gmail.com>
2019-07-12 07:15:10 -0300

    feat(ProjectPreferencesUpdated): update ProjectPreferencesUpdated calculation

--- src/application/preferencesForProject/preferencesForProject.test.ts
+++ src/application/preferencesForProject/preferencesForProject.test.ts
@@ -3,6 +3,56 @@ import {expect} from 'chai';
 import preferencesForProject from "./preferencesForProject";
 import * as eventsDBModule from "../events/eventsDB";
 
+
+const ppus = [
+    {
+        project_clone_url: 'http://git.example.com/some-project.git',
+        event_type: 'ProjectPreferencesUpdated',
+        command: 'UPDATE_WEIGHT_PROJECT_WIDE',
+        arguments: {
+            tool_id: 'tool:a',
+            amount: -1
+        }
+    },
+    {
+        project_clone_url: 'http://git.example.com/some-project.git',
+        event_type: 'ProjectPreferencesUpdated',
+        command: 'UPDATE_WEIGHT_PER_FILE',
+        arguments: {
+            tool_id: 'tool:a',
+            file_path: 'src/main/java/com/example/Main.java',
+            amount: -6
+        }
+    },
+    {
+        project_clone_url: 'http://git.example.com/some-project.git',
+        event_type: 'ProjectPreferencesUpdated',
+        command: 'UPDATE_WEIGHT_PER_FILE',
+        arguments: {
+            tool_id: 'tool:a',
+            file_path: 'src/main/java/com/example/Main.java',
+            amount: -4
+        }
+    },
+    {
+        project_clone_url: 'http://git.example.com/some-project.git',
+        event_type: 'ProjectPreferencesUpdated',
+        command: 'DISABLE_TOOL_PROJECT_WIDE',
+        arguments: {
+            tool_id: 'tool:b'
+        }
+    },
+    {
+        project_clone_url: 'http://git.example.com/some-project.git',
+        event_type: 'ProjectPreferencesUpdated',
+        command: 'DISABLE_TOOL_PER_FILE',
+        arguments: {
+            tool_id: 'tool:c',
+            file_path: 'src/main/java/com/example/Main.java'
+        }
+    }
+];
+
 describe('preferencesForProject', function () {
     let eventsDBMockManager;
     beforeEach('mock setup', () => eventsDBMockManager = ImportMock.mockStaticClass(eventsDBModule));
@@ -10,36 +60,34 @@ describe('preferencesForProject', function () {
 
 
     it('preferencesForProject', async function () {
-        eventsDBMockManager.mock('findAll', Promise.resolve(
-            [
-                {
-                    event_type: 'ProjectPreferencesUpdated'
-                }
-            ]
-        ));
+        eventsDBMockManager.mock('findAll', Promise.resolve(ppus));
 
         const expectedPrefs = {
             project_wide: {
                 'tool:a': {
-                    weight: -1
+                    weight_modification: -1,
+                    enabled: true
                 },
                 'tool:b': {
-                    disabled: true
+                    weight_modification: 0,
+                    enabled: false
                 }
             },
             per_file: {
                 'src/main/java/com/example/Main.java': {
                     'tool:a': {
-                        weight: -10
+                        weight_modification: -10,
+                        enabled: true
                     },
                     'tool:c': {
-                        disabled: true
+                        weight_modification: 0,
+                        enabled: false
                     }
                 }
             }
         };
         let actualPrefs = await preferencesForProject('http://git.example.com/some-project.git');
 
-        expect(actualPrefs).to.equal(expectedPrefs);
+        expect(actualPrefs).to.deep.equal(expectedPrefs);
     });
 });
--- src/application/preferencesForProject/preferencesForProject.ts
+++ src/application/preferencesForProject/preferencesForProject.ts
@@ -1,6 +1,73 @@
 import eventsDB from "../events/eventsDB";
 
-export default function preferencesForProject(cloneUrl: string) {
-    const ppus = eventsDB.findAll({event_type: 'ProjectPreferencesUpdated', 'payload.clone_url': cloneUrl});
-    return ppus;
+export interface ToolPreferences {
+    weight_modification: number;
+    enabled: boolean;
+}
+
+export interface ProjectPreferences {
+    project_wide: { [tool_id: string]: ToolPreferences };
+    per_file: { [file_path: string]: { [tool_id: string]: ToolPreferences } };
+}
+
+export default async function preferencesForProject(cloneUrl: string): Promise<ProjectPreferences> {
+    const ppus = await eventsDB.findAll({project_clone_url: cloneUrl, event_type: 'ProjectPreferencesUpdated'});
+
+    const prefs: ProjectPreferences = {
+        project_wide: {
+            // 'tool:a': {
+            //     weight_modification: -1
+            // },
+            // 'tool:b': {
+            //     enabled: false
+            // }
+        },
+        per_file: {
+            // 'src/main/java/com/example/Main.java': {
+            //     'tool:a': {
+            //         weight: -10
+            //     },
+            //     'tool:c': {
+            //         enabled: false
+            //     }
+            // }
+        }
+    };
+    return ppus.reduce((previousValue: ProjectPreferences, currentValue) => {
+        switch (currentValue.command) {
+            case 'UPDATE_WEIGHT_PROJECT_WIDE':
+                previousValue.project_wide[currentValue.arguments.tool_id] = previousValue.project_wide[currentValue.arguments.tool_id] || {weight_modification: 0, enabled: true};
+                previousValue.project_wide[currentValue.arguments.tool_id].weight_modification += currentValue.arguments.amount;
+                break;
+            case 'UPDATE_WEIGHT_PER_FILE':
+                previousValue.per_file[currentValue.arguments.file_path] = previousValue.per_file[currentValue.arguments.file_path] || {};
+                    previousValue.per_file[currentValue.arguments.file_path][currentValue.arguments.tool_id]
+                    = previousValue.per_file[currentValue.arguments.file_path][currentValue.arguments.tool_id] || {weight_modification: 0, enabled: true};
+                previousValue.per_file[currentValue.arguments.file_path][currentValue.arguments.tool_id].weight_modification += currentValue.arguments.amount;
+                break;
+            case 'DISABLE_TOOL_PROJECT_WIDE':
+                previousValue.project_wide[currentValue.arguments.tool_id] = previousValue.project_wide[currentValue.arguments.tool_id] || {weight_modification: 0, enabled: true};
+                previousValue.project_wide[currentValue.arguments.tool_id].enabled = false;
+                break;
+            case 'DISABLE_TOOL_PER_FILE':
+                previousValue.per_file[currentValue.arguments.file_path] = previousValue.per_file[currentValue.arguments.file_path] || {};
+                previousValue.per_file[currentValue.arguments.file_path][currentValue.arguments.tool_id]
+                    = previousValue.per_file[currentValue.arguments.file_path][currentValue.arguments.tool_id] || {weight_modification: 0, enabled: true};
+                previousValue.per_file[currentValue.arguments.file_path][currentValue.arguments.tool_id].enabled = false;
+                break;
+            case 'xENABLE_TOOL_PROJECT_WIDE':
+                previousValue.project_wide[currentValue.arguments.tool_id] = previousValue.project_wide[currentValue.arguments.tool_id] || {weight_modification: 0, enabled: true};
+                previousValue.project_wide[currentValue.arguments.tool_id].enabled = true;
+                break;
+            case 'xENABLE_TOOL_PER_FILE':
+                previousValue.per_file[currentValue.arguments.file_path] = previousValue.per_file[currentValue.arguments.file_path] || {};
+                previousValue.per_file[currentValue.arguments.file_path][currentValue.arguments.tool_id]
+                    = previousValue.per_file[currentValue.arguments.file_path][currentValue.arguments.tool_id] || {weight_modification: 0, enabled: true};
+                previousValue.per_file[currentValue.arguments.file_path][currentValue.arguments.tool_id].enabled = true;
+                break;
+            default:
+                throw Error('Unknown PPU command: '+currentValue.command);
+        }
+        return previousValue;
+    }, prefs)
 }
