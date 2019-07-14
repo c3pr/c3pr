d1a2f144e760f980acd092410988b12d8401aed3
Antonio Jr <acdcjunior@gmail.com>
2019-07-14 04:22:12 -0300

    feat(comment): ignore comments in MRs not created by the bot

--- src/application/handleComment/handleComment.ts
+++ src/application/handleComment/handleComment.ts
@@ -1,8 +1,14 @@
 import {GitLabNote} from "../../ports/outbound/types/GitLabNote/GitLabNote";
 import createPullRequestUpdatedFromNoteHook from "../PullRequestUpdated/createPullRequestUpdatedFromNoteHook";
 import emitPullRequestUpdated from "../PullRequestUpdated/emitPullRequestUpdated";
+import config from "../../config";
 
 export default async function handleComment(gitLabNote: GitLabNote, c3prLOG5) {
+    if (gitLabNote.merge_request.author_id !== config.c3pr.repoGitlab.gitlab.bot_user_id) {
+        c3prLOG5(`Note event is ignored. The PR is not authored by the bot.`, {meta: {gitLabNote, bot_user_id: config.c3pr.repoGitlab.gitlab.bot_user_id}});
+        return;
+    }
+
     c3prLOG5(`Handling comment "${gitLabNote.object_attributes.note}".`, {caller_name: 'handleComment', meta: {noteWebhook: gitLabNote}});
 
     const pullRequestUpdated = createPullRequestUpdatedFromNoteHook(gitLabNote);
