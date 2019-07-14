bd8906795a07ae708ae9676d0cc379204c6430dc
Antonio Jr <acdcjunior@gmail.com>
2019-07-14 04:34:50 -0300

    feat(sentry): better notify of unhandled errors

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,25 @@
+//////////// BEGIN ERROR TRACING CODE
 const Sentry = require('@sentry/node');
 Sentry.init({ dsn: 'https://ca35d507f8184162a6f3d3151f3de475@sentry.io/1494586' });
 
 process.on('unhandledRejection', function (err) {
+    Sentry.captureException(err);
     console.log('unhandledRejection >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>');
     const stack = new Error().stack;
     console.log(stack);
     console.log(err);
     console.log('unhandledRejection <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<');
 });
+
+process.on('uncaughtException', function (err) {
+    Sentry.captureException(err);
+    console.log('uncaughtException >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>');
+    const stack = new Error().stack;
+    console.log(stack);
+    console.log(err);
+    console.log('uncaughtException <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<');
+});
+//////////// END ERROR TRACING CODE
 
 require('./application/login/login');
 require('./web/express');
