dd2b3d34772f75de2b8d7b4f4d5ef2dbea4e6ae7
Antonio Jr <acdcjunior@gmail.com>
2019-07-14 04:42:32 -0300

    feat(pru): report invalid data

--- src/application/PullRequestUpdated/extractRevisionFromMrDescription.ts
+++ src/application/PullRequestUpdated/extractRevisionFromMrDescription.ts
@@ -1,4 +1,16 @@
+const Sentry = require('@sentry/node');
+
+function isNotGitSha(revision) {
+    return !/^[a-z0-9]{40}$/.test(revision);
+}
+
 export default function extractRevisionFromMrDescription(description: string) {
     // Expected message format: "dfasdfasdffd This fix was generated in response to the commit 5aeb86edb4a17cb985c13a4db14a4b66064ef94b.".match(/(\w+)\.$/)[1]
-    return description.match(/(\w+)\.$/)[1];
+    const revision = description.match(/(\w+)\.$/)[1];
+    if (isNotGitSha(revision)) {
+        const message = `Got invalid revision (${revision}) from MR description: ${description}`;
+        console.log(message);
+        Sentry.captureMessage(message);
+    }
+    return revision;
 }
--- src/application/PullRequestUpdated/getStatus.ts
+++ src/application/PullRequestUpdated/getStatus.ts
@@ -1,6 +1,15 @@
+const Sentry = require('@sentry/node');
+
 export function getStatus(state: "opened" | "closed" | "merged") {
     switch (state) {
         case 'opened': return 'open';
-        default: return state;
+        case 'closed': return state;
+        case 'merged': return state;
+
+        default:
+            const message = 'Unknown state: ' + state;
+            console.log(message);
+            Sentry.captureMessage(message);
+            return state;
     }
 }
