6c0b371863c8b01f4c31e38626ea130adb3f36d6
Antonio Jr <acdcjunior@gmail.com>
2019-07-14 07:52:51 -0300

    feat(events): add produceCommandEvent

---
+++ events/produceCommandEvent.ts
@@ -0,0 +1,55 @@
+import axios from 'axios';
+import axiosRetry from 'axios-retry/lib';
+import uuidv4 from "uuid/v4";
+
+export interface CommandEvent {
+    event_type: string; // TOPIC
+    project_clone_http_url: string; // KEY
+    // will be generated -> uuid: string;
+    // will be generated -> timestamp: string;
+    command: string;
+    args: {[s: string]: any};
+}
+
+export interface Config {
+    c3prHubUrl: string;
+    jwt: string;
+    retryWait?: number;
+}
+
+export default async function produceCommandEvent(event: CommandEvent, config: Config, c3prLOG5) {
+    const event_type = event.event_type;
+    const {c3prHubUrl, jwt, retryWait = 2000} = config;
+
+    const payload = {
+        event_type,
+        project_clone_http_url: event.project_clone_http_url,
+        uuid: uuid(),
+        timestamp: timestamp(),
+        command: event.command,
+        args: event.args
+    };
+
+    try {
+        c3prLOG5(`Registering new event for type '${event_type}'.`, {meta: {payload}});
+        const client = axios.create({ baseURL: c3prHubUrl });
+
+        axiosRetry(client, { retries: 3, retryDelay: retryCount => retryCount * retryWait, retryCondition() { return true; } });
+
+        const headers = {Authorization: `Bearer ${jwt}`};
+        await client.post(`/api/v1/events/${event_type}`, payload, {headers});
+    } catch (error) {
+        c3prLOG5(`Error while registering new event of type '${event_type}'.`, {error, meta: {payload}});
+        throw error;
+    }
+}
+
+
+
+function timestamp() {
+    return new Date().toISOString();
+}
+
+function uuid() {
+    return uuidv4();
+}
