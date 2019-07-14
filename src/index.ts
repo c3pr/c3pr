9e69bd728303e4b2b110ac583d51e1295b3cfae8
Antonio Jr <acdcjunior@gmail.com>
2019-07-14 04:34:25 -0300

    feat(sentry): notify of unhandled errors

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
+//////////// BEGIN ERROR TRACING CODE
 const Sentry = require('@sentry/node');
 Sentry.init({ dsn: 'https://ca35d507f8184162a6f3d3151f3de475@sentry.io/1494586' });
 
 process.on('unhandledRejection', function (err) {
+    Sentry.captureException(err);
     console.log('unhandledRejection >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>');
     const stack = new Error().stack;
     console.log(stack);
@@ -9,16 +11,19 @@ process.on('unhandledRejection', function (err) {
     console.log('unhandledRejection <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<');
 });
 
-require('./application/events/events').init.then(() => {
-    return require('./application/agentRegistry/agentRegistry').init;
-}).then(() => {
-    require('./web/express');
-});
-
 process.on('uncaughtException', function (err) {
+    Sentry.captureException(err);
     console.log('uncaughtException >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>');
     const stack = new Error().stack;
     console.log(stack);
     console.log(err);
     console.log('uncaughtException <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<');
+});
+//////////// END ERROR TRACING CODE
+
+
+require('./application/events/events').init.then(() => {
+    return require('./application/agentRegistry/agentRegistry').init;
+}).then(() => {
+    require('./web/express');
 });
