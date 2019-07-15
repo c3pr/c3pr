b66a1a130e4fd1992dd0784e011014d8ef77537f
Antonio Jr <acdcjunior@gmail.com>
2019-07-15 00:55:53 -0300

    feat(auth): lazily evaluate decoded jwt token

--- src/application/auth/authExpressMiddleware.ts
+++ src/application/auth/authExpressMiddleware.ts
@@ -6,7 +6,9 @@ function authExpressMiddleware(request, response, next) {
         response.status(401).send(`Please send a "Authorization: Bearer TOKEN-JWT" header.`);
     } else {
         try {
-            request.decodedJwtToken = decodeToken(request.headers.authorization.split(' ')[1]);
+            Object.defineProperty(request, 'decodedJwtToken', {
+                get: () => decodeToken(request.headers.authorization.split(' ')[1])
+            });
             next();
         } catch (e) {
             let errorStatus = (e.toString() === 'Error: Signature verification failed') ? 401 : 500;
