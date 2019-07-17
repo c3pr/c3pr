a2823a48c3b2eff7f6bb2f3ee6eb2b7158f1e7ec
Antonio 'acdc' Jr <acdcjunior@gmail.com>
2019-07-17 20:47:26 -0300

    feat(forkAndApplyPatch): improve logging

--- forkAndApplyPatch/index.js
+++ forkAndApplyPatch/index.js
@@ -3,36 +3,35 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const path = require("path");
 const rimraf = require("rimraf");
 const uuid_1 = require("uuid");
-const c3prLOG4_1 = require("node-c3pr-logger/c3prLOG4");
 const c3prSH3_1 = require("node-c3pr-git-client/src/c3prSH3");
 const applyGitPatchBase64_1 = require("node-c3pr-git-client/patch/applyGitPatchBase64");
 const os = require("os");
 const C3PR_CLONES_FOLDER = process.env.C3PR_CLONES_FOLDER || '/tmp/';
-async function forkAndApplyPatch({ createForkIfNotExists, addAuthenticationToCloneUrl, tokenReplacementForLogFunction, mainRepoOrgRepo, mainRepoBranch, mainRepoHash, gitUserName, gitUserEmail, prCommitMessage, patchContent, mainRepoCloneUrl, lcid, sha, euuid }) {
+async function forkAndApplyPatch({ createForkIfNotExists, addAuthenticationToCloneUrl, tokenReplacementForLogFunction, mainRepoOrgRepo, mainRepoBranch, mainRepoHash, gitUserName, gitUserEmail, prCommitMessage, patchContent, mainRepoCloneUrl }, c3prLOG5) {
+    c3prLOG5 = c3prLOG5({ caller_name: 'forkAndApplyPatch' });
     const stagingFolderName = `${mainRepoHash}_${uuid_1.v4()}`;
     const stagingFolder = path.resolve(`${C3PR_CLONES_FOLDER}/${stagingFolderName}`);
-    const forkInfo = await createForkIfNotExists(mainRepoOrgRepo, { lcid, sha, euuid });
+    const forkInfo = await createForkIfNotExists(mainRepoOrgRepo, c3prLOG5);
     const forkRepoOrg = forkInfo.organization;
     const forkRepoProject = forkInfo.forkName;
     const forkRepoCloneUrl = addAuthenticationToCloneUrl(forkInfo.cloneUrl);
     const forkRepoBranch = 'C3PR_' + mainRepoHash.substring(0, 8) + '_' + os.hostname().substring(0, 3) + Date.now().toString(16);
-    await c3prSH3_1.default(`git init ${stagingFolder}`, {}, { lcid, sha, euuid });
+    await c3prSH3_1.default(`git init ${stagingFolder}`, {}, {}, c3prLOG5);
     // create brand new orphan branch
-    await c3prSH3_1.default(`git checkout --orphan ${forkRepoBranch}`, { cwd: stagingFolder }, { lcid, sha, euuid });
+    await c3prSH3_1.default(`git checkout --orphan ${forkRepoBranch}`, { cwd: stagingFolder }, {}, c3prLOG5);
     // add main repo, fetch it and merge into recently created branch
-    await c3prSH3_1.default(`git remote add main ${mainRepoCloneUrl}`, { cwd: stagingFolder }, { replacements: [tokenReplacementForLogFunction], lcid, sha, euuid });
-    await c3prSH3_1.default(`git fetch main ${mainRepoBranch}`, { cwd: stagingFolder }, { lcid, sha, euuid });
-    await c3prSH3_1.default(`git merge main/${mainRepoBranch}`, { cwd: stagingFolder }, { lcid, sha, euuid });
-    await applyGitPatchBase64_1.default(stagingFolder, gitUserName, gitUserEmail, { hexBase64: patchContent, plain: '', header: '', footer: '' }, { lcid, sha, euuid });
+    await c3prSH3_1.default(`git remote add main ${mainRepoCloneUrl}`, { cwd: stagingFolder }, { replacements: [tokenReplacementForLogFunction] }, c3prLOG5);
+    await c3prSH3_1.default(`git fetch main ${mainRepoBranch}`, { cwd: stagingFolder }, {}, c3prLOG5);
+    await c3prSH3_1.default(`git merge main/${mainRepoBranch}`, { cwd: stagingFolder }, {}, c3prLOG5);
+    await applyGitPatchBase64_1.default(stagingFolder, gitUserName, gitUserEmail, { hexBase64: patchContent, plain: '', header: '', footer: '' }, c3prLOG5);
     // add fork repo
-    await c3prSH3_1.default(`git remote add fork ${forkRepoCloneUrl}`, { cwd: stagingFolder }, { replacements: [tokenReplacementForLogFunction], lcid, sha, euuid });
+    await c3prSH3_1.default(`git remote add fork ${forkRepoCloneUrl}`, { cwd: stagingFolder }, { replacements: [tokenReplacementForLogFunction] }, c3prLOG5);
     // push changes
-    await c3prSH3_1.default(`git push -u fork ${forkRepoBranch}`, { cwd: stagingFolder }, { lcid, sha, euuid });
+    await c3prSH3_1.default(`git push -u fork ${forkRepoBranch}`, { cwd: stagingFolder }, {}, c3prLOG5);
     rimraf(forkRepoBranch, () => {
-        c3prLOG4_1.default(`Clone/staging folder ${forkRepoBranch} removed.`, { lcid, sha, euuid });
+        c3prLOG5(`Clone/staging folder ${forkRepoBranch} removed.`);
     });
     return { forkRepoOrg, forkRepoProject, forkRepoBranch };
 }
-// noinspection JSUnusedGlobalSymbols
-exports.default = forkAndApplyPatch;
+exports.forkAndApplyPatch = forkAndApplyPatch;
 //# sourceMappingURL=index.js.map
--- forkAndApplyPatch/index.ts
+++ forkAndApplyPatch/index.ts
@@ -1,60 +1,74 @@
 import * as path from 'path';
 import * as rimraf from 'rimraf';
 import { v4 as uuidv4 } from 'uuid';
-import c3prLOG4 from "node-c3pr-logger/c3prLOG4";
 import c3prSH3 from "node-c3pr-git-client/src/c3prSH3";
 import applyGitPatchBase64 from "node-c3pr-git-client/patch/applyGitPatchBase64";
 import * as os from "os";
 
 const C3PR_CLONES_FOLDER = process.env.C3PR_CLONES_FOLDER || '/tmp/';
 
-async function forkAndApplyPatch({
-                            createForkIfNotExists,
-                            addAuthenticationToCloneUrl,
-                            tokenReplacementForLogFunction,
-                            mainRepoOrgRepo,
-                            mainRepoBranch,
-                            mainRepoHash,
-                            gitUserName,
-                            gitUserEmail,
-                            prCommitMessage,
-                            patchContent,
-                            mainRepoCloneUrl,
-                            lcid, sha, euuid
-                        }) {
+interface ForkAndApplyPatchArgs {
+    createForkIfNotExists: (mainRepoOrgRepo: string, c3prLOG5: any) => Promise<{organization: string, forkName: string, cloneUrl: string}>;
+    addAuthenticationToCloneUrl;
+    tokenReplacementForLogFunction;
+    mainRepoOrgRepo: string;
+    mainRepoBranch;
+    mainRepoHash;
+    gitUserName;
+    gitUserEmail;
+    prCommitMessage;
+    patchContent;
+    mainRepoCloneUrl;
+}
+
+export async function forkAndApplyPatch(
+    {
+        createForkIfNotExists,
+        addAuthenticationToCloneUrl,
+        tokenReplacementForLogFunction,
+        mainRepoOrgRepo,
+        mainRepoBranch,
+        mainRepoHash,
+        gitUserName,
+        gitUserEmail,
+        prCommitMessage,
+        patchContent,
+        mainRepoCloneUrl
+    }: ForkAndApplyPatchArgs,
+    c3prLOG5
+) {
+    c3prLOG5 = c3prLOG5({caller_name: 'forkAndApplyPatch'});
+
     const stagingFolderName = `${mainRepoHash}_${uuidv4()}`;
     const stagingFolder = path.resolve(`${C3PR_CLONES_FOLDER}/${stagingFolderName}`);
 
-    const forkInfo = await createForkIfNotExists(mainRepoOrgRepo, {lcid, sha, euuid});
+    const forkInfo = await createForkIfNotExists(mainRepoOrgRepo, c3prLOG5);
 
     const forkRepoOrg = forkInfo.organization;
     const forkRepoProject = forkInfo.forkName;
     const forkRepoCloneUrl = addAuthenticationToCloneUrl(forkInfo.cloneUrl);
     const forkRepoBranch = 'C3PR_' + mainRepoHash.substring(0, 8) + '_' + os.hostname().substring(0, 3) + Date.now().toString(16);
 
-    await c3prSH3(`git init ${stagingFolder}`, {}, {lcid, sha, euuid});
+    await c3prSH3(`git init ${stagingFolder}`, {}, {}, c3prLOG5);
 
     // create brand new orphan branch
-    await c3prSH3(`git checkout --orphan ${forkRepoBranch}`, {cwd: stagingFolder}, {lcid, sha, euuid});
+    await c3prSH3(`git checkout --orphan ${forkRepoBranch}`, {cwd: stagingFolder}, {}, c3prLOG5);
     // add main repo, fetch it and merge into recently created branch
 
-    await c3prSH3(`git remote add main ${mainRepoCloneUrl}`, {cwd: stagingFolder}, {replacements: [tokenReplacementForLogFunction], lcid, sha, euuid});
-    await c3prSH3(`git fetch main ${mainRepoBranch}`, {cwd: stagingFolder}, {lcid, sha, euuid});
-    await c3prSH3(`git merge main/${mainRepoBranch}`, {cwd: stagingFolder}, {lcid, sha, euuid});
+    await c3prSH3(`git remote add main ${mainRepoCloneUrl}`, {cwd: stagingFolder}, {replacements: [tokenReplacementForLogFunction]}, c3prLOG5);
+    await c3prSH3(`git fetch main ${mainRepoBranch}`, {cwd: stagingFolder}, {}, c3prLOG5);
+    await c3prSH3(`git merge main/${mainRepoBranch}`, {cwd: stagingFolder}, {}, c3prLOG5);
 
-    await applyGitPatchBase64(stagingFolder, gitUserName, gitUserEmail, {hexBase64: patchContent, plain: '', header: '', footer: ''}, {lcid, sha, euuid});
+    await applyGitPatchBase64(stagingFolder, gitUserName, gitUserEmail, {hexBase64: patchContent, plain: '', header: '', footer: ''}, c3prLOG5);
 
     // add fork repo
-    await c3prSH3(`git remote add fork ${forkRepoCloneUrl}`, {cwd: stagingFolder}, {replacements: [tokenReplacementForLogFunction], lcid, sha, euuid});
+    await c3prSH3(`git remote add fork ${forkRepoCloneUrl}`, {cwd: stagingFolder}, {replacements: [tokenReplacementForLogFunction]}, c3prLOG5);
     // push changes
-    await c3prSH3(`git push -u fork ${forkRepoBranch}`, {cwd: stagingFolder}, {lcid, sha, euuid});
+    await c3prSH3(`git push -u fork ${forkRepoBranch}`, {cwd: stagingFolder}, {}, c3prLOG5);
 
     rimraf(forkRepoBranch, () => {
-        c3prLOG4(`Clone/staging folder ${forkRepoBranch} removed.`, {lcid, sha, euuid});
+        c3prLOG5(`Clone/staging folder ${forkRepoBranch} removed.`);
     });
 
     return {forkRepoOrg, forkRepoProject, forkRepoBranch};
 }
-
-// noinspection JSUnusedGlobalSymbols
-export default forkAndApplyPatch;
