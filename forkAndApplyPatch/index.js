caf80ae119c6f90b295fff24e6dbf839cc6c3a7c
Antonio 'acdc' Jr <acdcjunior@gmail.com>
2019-07-19 19:23:21 -0300

    chore: update version and binaries

--- forkAndApplyPatch/index.js
+++ forkAndApplyPatch/index.js
@@ -7,6 +7,7 @@ const c3prSH3_1 = require("node-c3pr-git-client/src/c3prSH3");
 const applyGitPatchBase64_1 = require("node-c3pr-git-client/patch/applyGitPatchBase64");
 const os = require("os");
 const C3PR_CLONES_FOLDER = process.env.C3PR_CLONES_FOLDER || '/tmp/';
+let forkCount = 0;
 async function forkAndApplyPatch({ createForkIfNotExists, addAuthenticationToCloneUrl, tokenReplacementForLogFunction, mainRepoOrgRepo, mainRepoBranch, mainRepoHash, gitUserName, gitUserEmail, prCommitMessage, patchContent, mainRepoCloneUrl }, c3prLOG5) {
     c3prLOG5 = c3prLOG5({ caller_name: 'forkAndApplyPatch' });
     const stagingFolderName = `${mainRepoHash}_${uuid_1.v4()}`;
@@ -15,7 +16,7 @@ async function forkAndApplyPatch({ createForkIfNotExists, addAuthenticationToClo
     const forkRepoOrg = forkInfo.organization;
     const forkRepoProject = forkInfo.forkName;
     const forkRepoCloneUrl = addAuthenticationToCloneUrl(forkInfo.cloneUrl);
-    const forkRepoBranch = 'C3PR_' + mainRepoHash.substring(0, 8) + '_' + os.hostname().substring(0, 3) + Date.now().toString(16);
+    const forkRepoBranch = 'C3PR_' + mainRepoHash.substring(0, 8) + '_' + os.hostname().substring(0, 2) + (++forkCount % 36).toString(36) + Date.now().toString(36);
     await c3prSH3_1.default(`git init ${stagingFolder}`, {}, {}, c3prLOG5);
     // create brand new orphan branch
     await c3prSH3_1.default(`git checkout --orphan ${forkRepoBranch}`, { cwd: stagingFolder }, {}, c3prLOG5);
