e5b67d2569a1be842215dfcdd1dcb2e36e16ca58
Antonio Jr <acdcjunior@gmail.com>
2019-07-20 04:21:40 -0300

    refactor(invokeTool): use export directly in functions

--- src/application/invokeTool/extractExtensionsInChangeset.ts
+++ src/application/invokeTool/extractExtensionsInChangeset.ts
@@ -6,18 +6,14 @@ const path = require('path');
  * @param files e.g. ['src/main/a/b/c/Main.java', 'src/main/a/b/c/Main.js', 'src/main/a/b/c/Dockerfile']
  * @return Array<String> e.g. ['java', 'js', 'dockerfile']
  */
-function extractExtensionsInChangeset(files) {
+export function extractExtensionsInChangeset(files) {
     return distinct(files.map(extractExtension));
 }
 
-function extractExtension(file) {
+export function extractExtension(file) {
     return path.basename(file).split('.').pop().toLowerCase();
 }
 
 function distinct(a) {
     return Array.from(new Set(a));
 }
-
-export = {
-    extractExtensionsInChangeset, extractExtension
-};
