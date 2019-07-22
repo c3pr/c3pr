50be8e0ac2da56528ac3ea4dd2753bfb96cbb447
Antonio Jr <acdcjunior@gmail.com>
2019-07-22 06:38:38 -0300

    test: improve e2e script and gen

--- tests/e2e/gen-tools.js
+++ tests/e2e/gen-tools.js
@@ -1,12 +1,5 @@
 const fs = require('fs');
 
-for (let i = 1; i <= 100; i++) {
-    const template = template(i);
-
-    fs.mkdirSync(`${__dirname}/tools`, {recursive: true});
-    fs.writeFileSync(`${__dirname}/tools/txt-${i}.yml`, template)
-}
-
 function template(i) {
 
     return `tool_id: "agent_e2e:txt_${i}"
@@ -14,10 +7,19 @@ extensions: ["txt"]
 tags: ["Plaintext"]
 default_weight: 100
 
-command: 'node -e "if (Math.floor(Math.random() * 95) === 0) fs.writeFileSync(\`#{filename}\`, \`txt_${i}\`)"'
+command: 'node -e "if (Math.floor(Math.random()*100)< ${CHANCE_OF_TRANSFORMATION} ) fs.writeFileSync(\`#{filename}\`, \`txt_${i}\`)"'
 
 pr_title: TXT Rule ${i}
 pr_body: |
   This has been TXT Rule ${i}!`.replace(/\r?\n/g, '\r\n');
 
+}
+
+const CHANCE_OF_TRANSFORMATION = 50; // e.g. 40 means 40% chance
+
+// noinspection all
+fs.mkdirSync(`${__dirname}/tools`, {recursive: true});
+for (let i = 1; i <= 100; i++) {
+    // noinspection all
+    fs.writeFileSync(`${__dirname}/tools/txt-${i}.yml`, template(i))
 }
