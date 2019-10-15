4b5923f4d092cc154fe567b481aa2f96f1facc1b
Antonio 'acdc' Jr <acdcjunior@gmail.com>
2019-10-14 22:13:56 -0300

    feat(comment): add proper reply for "conflict"

--- src/application/comments/processComment.ts
+++ src/application/comments/processComment.ts
@@ -66,6 +66,18 @@ const MANUAL: Comment = {
         return `:robot: You will handle the issue manually. Got it, ${mention_handle}!`;
     }
 };
+const CONFLICT: Comment = {
+    matches(text: string): boolean {
+        return text.includes("conflict");
+    },
+    createCommand(files: string[], tool_id: string, timestamp: string): UpdatePrefsCommand[] {
+        // does not count this PR as bad
+        return modifyWeightOfToolForAllFiles(files, tool_id, timestamp, WEIGHT_MODIFICATION_PER_CLOSED_PR * -1);
+    },
+    createResponse(mention_handle: string): string {
+        return `:robot: Yeah, conflict is probably not worth it to fix. Got it, ${mention_handle}! I will not reduce the priority for this kind of suggestion for now.`;
+    }
+};
 const HELP: Comment = {
     matches(text: string): boolean {
         return text.includes("help");
@@ -76,15 +88,17 @@ const HELP: Comment = {
     createResponse(mention_handle: string): string {
         return `:robot: Hey, ${mention_handle}! You can send commands by mentioning me in comments. The ones available are:
         
-        - \`@c3pr\\-bot manual\`: lets me know you will performed yourself, manually, the change this PR suggested. This means I should not reduce the priority for
-        this kind o transformation when you close this PR.
-        - \`@c3pr\\-bot disable for file\`: disables this transformation for this file.
-        - \`@c3pr\\-bot disable for project\`: disables this transformation for all files of this project.
-        - \`@c3pr\\-bot disagree\`: indicates that you didn't agree with the proposed changes. The priority for this kind of suggestion will be reduced.
-        - \`@c3pr\\-bot bug\`: points out that this transformation generated a bug. I will disable the transformation for the file, and if it happens more times, for the whole project.
-        - \`@c3pr\\-bot help\`: I will reply with this message.
-        
-        Anyway, whenever you mention me, I will let you know if I understood the command or not. :v:
+- \`@c3pr\\-bot manual\`: lets me know you will perform yourself, manually, the change this PR suggested. This means I should not reduce the priority for
+this kind of transformation when you close this PR.
+- \`@c3pr\\-bot conflict\`: tells me you closed this PR due to a conflict that you don't feel like fixing. I will also not reduce the priority for
+this kind of transformation when you close this PR.
+- \`@c3pr\\-bot disagree\`: indicates that you didn't agree with the proposed changes. The priority for this kind of suggestion will be reduced.
+- \`@c3pr\\-bot disable for file\`: disables this transformation for this file.
+- \`@c3pr\\-bot disable for project\`: disables this transformation for all files of this project.
+- \`@c3pr\\-bot bug\`: points out that this transformation generated a bug. I will disable the transformation for the file, and if it happens more times, for the whole project.
+- \`@c3pr\\-bot help\`: I will reply with this message.
+
+Anyway, whenever you mention me, I will let you know if I understood the command or not. :v:
         `;
     }
 };
@@ -102,7 +116,7 @@ const UNRECOGNIZED_COMMENT: Comment = {
 };
 
 // the order here is important -- eg "disagree: disable for file" should be processed as "disabled for file" rather than "disagree"
-const COMMENT_TYPES = [DISABLE_FOR_PROJECT, DISABLE_FOR_FILE, DISAGREE, MANUAL, BUG, HELP, UNRECOGNIZED_COMMENT];
+const COMMENT_TYPES = [DISABLE_FOR_PROJECT, DISABLE_FOR_FILE, DISAGREE, CONFLICT, MANUAL, BUG, HELP, UNRECOGNIZED_COMMENT];
 
 export function generateCommandsFromComment(files: string[], tool_id: string, timestamp: string, text: string): UpdatePrefsCommand[] {
     return COMMENT_TYPES.find(ct => ct.matches(text)).createCommand(files, tool_id, timestamp);
